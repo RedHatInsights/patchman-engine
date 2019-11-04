f1e79f0eeb420101196c94ea0442167f27dc0feb
Josef Hak <jhak@redhat.com>
2019-11-04 18:09:22 +0100

    added kafka msgs sending after platform container started

--- scripts/send-kafka-requests.sh
+++ scripts/send-kafka-requests.sh
@@ -6,8 +6,7 @@ cd $DIR
 
 TOPIC="host.packages"
 
-for _ in $(seq 1 1000); do
-  input="/data/body/$(ls /data/body | shuf -n 1)"
+for _ in $(seq 1 20); do
+  input="./data/body/$(ls ./data/body | shuf -n 1)"
   ./kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic $TOPIC < $input &
 done
-
