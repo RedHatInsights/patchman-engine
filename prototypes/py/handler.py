86e7fdec17d3928b52ae08cf4d25896157869235
semtexzv <mhornick@redhat.com>
2019-11-07 11:46:35 +0100

    Basic rust prototype

--- prototypes/py/handler.py
+++ prototypes/py/handler.py
@@ -5,15 +5,12 @@ import hashlib
 
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
-
 def all_hosts():
     hosts = db.Host.select(
         db.Host.id,
         db.Host.request,
         db.Host.checksum)
-
     return [host for host in hosts.dicts()]
-
 
 def get_host(id):
     host = db.Host.get_or_none(db.Host.id == id)
--- scripts/send_kafka_requests.py
+++ scripts/send_kafka_requests.py
@@ -6,6 +6,7 @@ producer = KafkaProducer(bootstrap_servers=os.getenv("LISTENER_KAFKA_ADDRESS", N
 for i in range(1, int(os.getenv("BENCHMARK_MESSAGES", "30")) + 1):
     print(f"Sending message {i}")
     data = open(f"./data/body/{i}.json", 'rb').read()
+
     future = producer.send(os.getenv("LISTENER_KAFKA_TOPIC", None), data)
     future.get(timeout=60)
 
