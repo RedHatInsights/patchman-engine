f2ae4418cda032a067c6d4b0174701c1b9755952
Josef Hak <jhak@redhat.com>
2019-11-25 15:10:14 +0100

    removed useless endpoints from prototype

--- manager/routes/routes.go
+++ manager/routes/routes.go
@@ -10,10 +10,6 @@ func Init(app *gin.Engine) {
 	// public routes
 	app.GET("/health", controllers.HealthHandler)
 	app.GET("/db_health", controllers.HealthDBHandler)
-	app.GET("/samples", controllers.ListHandler)
-	app.GET("/hosts/:id", controllers.GetHostHandler)
-	app.GET("/create", controllers.CreateHandler)
-	app.GET("/delete", controllers.DeleteHandler)
 }
 
 func InitAPI(group *gin.RouterGroup) {
