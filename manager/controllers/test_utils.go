ac4943313556dc6a282ed46c8b095ced1c5ffc69
Josef Hak <jhak@redhat.com>
2019-11-25 15:10:14 +0100

    removed useless endpoints from prototype

--- manager/controllers/test_utils.go
+++ manager/controllers/test_utils.go
@@ -1,11 +1,8 @@
 package controllers
 
 import (
-	"app/base/database"
 	"app/manager/middlewares"
 	"github.com/gin-gonic/gin"
-
-	"app/base/structures"
 )
 
 func initRouter(handler gin.HandlerFunc) *gin.Engine {
@@ -17,13 +14,4 @@ func initRouterWithPath(handler gin.HandlerFunc, path string) *gin.Engine {
 	router.Use(middlewares.RequestResponseLogger())
 	router.GET(path, handler)
 	return router
-}
-
-func createTestingSample(id int) {
-	record := &structures.HostDAO{ID: id, Request: "r",
-		Checksum: "454349e422f05297191ead13e21d3db520e5abef52055e4964b82fb213f593a1"}
-	err := database.Db.Create(record).Error
-	if err != nil {
-		panic(err)
-	}
 }
