e8d4d0bde57ba13023497466a4a1259610f039b6
semtexzv <mhornick@redhat.com>
2019-12-02 11:13:57 +0100

    Implement proper handling of Kafka messages

--- scripts/generate-clients.sh
+++ scripts/generate-clients.sh
@@ -5,6 +5,8 @@
 # until the https://github.com/OpenAPITools/openapi-generator/pull/4664 is merged
 CONTAINER=semtexzv/openapi-generator-cli:latest
 
+# For now, we skip the dateTime parsing because of incompatible formats ( inventory does not produce fully compliant
+# RFC 3339 datetimes
 
 function generate_client() {
   NAME=$1
@@ -19,9 +21,10 @@ function generate_client() {
       --api-package $NAME \
       -p packageName=$NAME,isGoSubmodule=true \
       --git-host app --git-user-id _generated --git-repo-id cmsfr \
+      --type-mappings DateTime=string \
       -o /local/_generated/cmsfr/$NAME
 }
 
 generate_client inventory "https://ci.cloud.redhat.com/api/inventory/v1/openapi.json"
-generate_client remediations "https://ci.cloud.redhat.com/api/remediations/v1/openapi.json"
+#generate_client remediations "https://ci.cloud.redhat.com/api/remediations/v1/openapi.json"
 generate_client vmaas "https://webapp-vmaas-ci.5a9f.insights-dev.openshiftapps.com/api/openapi.json"
