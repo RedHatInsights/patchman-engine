99369178dd54cc325a705d1dbeccb93e9d5ca523
semtexzv <mhornick@redhat.com>
2019-12-02 11:08:46 +0100

    Implement API client generation
    Fix users in client generation script

---
+++ scripts/generate-clients.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#CONTAINER=openapitools/openapi-generator-cli
+# We temporarily use image built from https://github.com/semtexzv/openapi-generator
+# until the https://github.com/OpenAPITools/openapi-generator/pull/4664 is merged
+CONTAINER=semtexzv/openapi-generator-cli:latest
+
+
+function generate_client() {
+  NAME=$1
+  SPEC_URL=$2
+
+  curl $SPEC_URL > /tmp/openapi.json
+
+  HERE=$(pwd)
+  docker run --rm -v ${HERE}:/local:z -v /tmp:/tmp -v /etc/passwd:/etc/passwd -u `id -u`:`id -g` $CONTAINER generate \
+      -i /tmp/openapi.json \
+      -g go \
+      --api-package $NAME \
+      -p packageName=$NAME,isGoSubmodule=true \
+      --git-host app --git-user-id _generated --git-repo-id cmsfr \
+      -o /local/_generated/cmsfr/$NAME
+}
+
+generate_client inventory "https://ci.cloud.redhat.com/api/inventory/v1/openapi.json"
+generate_client remediations "https://ci.cloud.redhat.com/api/remediations/v1/openapi.json"
+generate_client vmaas "https://webapp-vmaas-ci.5a9f.insights-dev.openshiftapps.com/api/openapi.json"
