b7c42806b67623a9fbf203d915a8e5571390b6f2
semtexzv <mhornick@redhat.com>
2019-12-02 11:13:57 +0100

    Implement proper handling of Kafka messages

--- listener/listener.go
+++ listener/listener.go
@@ -1,16 +1,25 @@
 package listener
 
 import (
+	"app/_generated/cmsfr/inventory"
+	"app/_generated/cmsfr/vmaas"
 	"app/base/utils"
 	"context"
+	"encoding/json"
+	"github.com/antihax/optional"
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
+const INVENTORY_API_PREFIX = "/api/inventory/v1"
+const VMAAS_API_PREFIX = "/api"
+
 var (
-	uploadReader *kafka.Reader
-	eventsReader *kafka.Reader
+	uploadReader    *kafka.Reader
+	eventsReader    *kafka.Reader
+	inventoryClient *inventory.APIClient
+	vmaasClient     *vmaas.APIClient
 )
 
 func configure() {
@@ -23,11 +32,11 @@ func configure() {
 	utils.Log("KafkaAddress", kafkaAddress).Info("Connecting to kafka")
 
 	uploadConfig := kafka.ReaderConfig{
-		Brokers:        []string{kafkaAddress},
-		Topic:          uploadTopic,
-		GroupID:        kafkaGroup,
-		MinBytes:       1,
-		MaxBytes:       10e6, // 1MB
+		Brokers:  []string{kafkaAddress},
+		Topic:    uploadTopic,
+		GroupID:  kafkaGroup,
+		MinBytes: 1,
+		MaxBytes: 10e6, // 1MB
 	}
 
 	uploadReader = kafka.NewReader(uploadConfig)
@@ -37,35 +46,99 @@ func configure() {
 
 	eventsReader = kafka.NewReader(eventsConfig)
 
+	inventoryAddr := utils.GetenvOrFail("INVENTORY_ADDRESS")
+
+	config := inventory.NewConfiguration()
+	config.Debug = true
+	config.BasePath = inventoryAddr + INVENTORY_API_PREFIX
+
+	inventoryClient = inventory.NewAPIClient(config)
+
+	cfg := vmaas.NewConfiguration()
+	cfg.BasePath = utils.GetenvOrFail("VMAAS_ADDRESS") + VMAAS_API_PREFIX
+	cfg.Debug = true
+
+	vmaasClient = vmaas.NewAPIClient(cfg)
 }
 
-func shutdown() {
-	err := uploadReader.Close()
+func shutdown(reader *kafka.Reader) {
+	err := reader.Close()
 	if err != nil {
 		utils.Log("err", err.Error()).Error("unable to shutdown Kafka reader")
 	}
-	err = eventsReader.Close()
-	if err != nil {
-		utils.Log("err", err.Error()).Error("unable to shutdown Kafka reader")
-	}
-
 }
 
 func baseListener(reader *kafka.Reader, handler func(message kafka.Message)) {
+	defer shutdown(reader)
+
 	for {
 		m, err := reader.ReadMessage(context.Background())
 		if err != nil {
 			utils.Log("err", err.Error()).Error("unable to read message from Kafka reader")
 			panic(err)
-
 		}
-		handler(m)
-
+		// Spawn handler, not blocking the receiving thread
+		go handler(m)
 	}
 }
 
 func logHandler(m kafka.Message) {
-	utils.Log().Info("Received message [", m.Topic,"] ", string(m.Value))
+	utils.Log().Info("Received message [", m.Topic, "] ", string(m.Value))
+}
+
+func uploadHandler(m kafka.Message) {
+	var event PlatformEvent
+	utils.Log("msg", string(m.Value)).Info("Msg received")
+
+	err := json.Unmarshal(m.Value, &event)
+	if err != nil {
+		utils.Log("err", err.Error()).Error("Could not deserialize host event")
+		return
+	}
+	// We need the b64 identity in order to call the inventory
+	if event.B64Identity != nil && event.Url != nil {
+		identity, err := utils.ParseIdentity(*event.B64Identity)
+		if err != nil {
+			utils.Log("err", err.Error()).Error("Could not parse identity")
+			return
+		}
+
+
+		if identity.IsSmartEntitled() {
+			utils.Log("url", *event.Url).Error("Downloading system profile")
+			downloadSystemProfile(event.Id, *event.B64Identity)
+		} else {
+			utils.Log("account", identity.Identity.AccountNumber).Info("Is not smart entitled")
+		}
+	} else {
+		utils.Log("event", event).Info("Not a valid upload request")
+	}
+}
+
+func downloadSystemProfile(hostId string, identity string) {
+	utils.Log("hostId", hostId, "identity", identity).Error("Downloading system profile")
+
+	// Create new context, which has the apikey value set. This value is then used as a value for `x-rh-identity`
+	ctx := context.WithValue(context.Background(), inventory.ContextAPIKey, inventory.APIKey{Prefix: "", Key: identity})
+
+	data, res, err := inventoryClient.HostsApi.ApiHostGetHostSystemProfileById(ctx, []string{hostId}, nil)
+
+	if err != nil {
+		utils.Log("err", err.Error()).Error("Could not Download body")
+		return
+	}
+
+	utils.Log("data", data, "res", res).Error("Download complete")
+
+	vars := vmaas.AppUpdatesHandlerV2PostPostOpts{
+		UpdatesRequest: optional.NewInterface(vmaas.UpdatesRequest{
+			// TODO: Proper logic here. This is just a test
+			PackageList: data.Results[0].SystemProfile.InstalledPackages,
+		}),
+	}
+
+	vmaasData, resp, err := vmaasClient.UpdatesApi.AppUpdatesHandlerV2PostPost(ctx, &vars)
+	utils.Log("data", vmaasData, "res", resp).Error("VMAAS query complete")
 }
 
 func runMetrics() {
@@ -88,12 +161,11 @@ func RunListener() {
 	go runMetrics()
 
 	configure()
-	defer shutdown()
 
-	go baseListener(uploadReader, logHandler)
+	go baseListener(uploadReader, uploadHandler)
 	go baseListener(eventsReader, logHandler)
 
 	// Just block. Any error will panic and kill the process.
-	<- make(chan bool)
+	<-make(chan bool)
 
 }
--- listener/message.go
+++ listener/message.go
@@ -1,69 +1,18 @@
 package listener
 
-import (
-	"crypto/sha256"
-	"encoding/hex"
-	"encoding/json"
-	"app/base/utils"
-)
-
-type Message struct {
-	ID              int        `json:"id"`
-	Arch            string     `json:"arch"`
-	Packages        *[]string  `json:"packages"`
+type HostInfo struct {
+	InsightsId *string `json:"insights_id"`
 }
 
-func (msg *Message) FilterPackages() {
-	// filter packages in parallel using go-routines
-	nPkgs := len(*msg.Packages)
-	channel := make(chan int, nPkgs)
-	for i, pkg := range *msg.Packages {
-		filterNevra(i, pkg, msg.Arch, channel)
-	}
+type PlatformEvent struct {
+	Id        string `json:"id"`
+	Timestamp string `json:"timestamp"`
+	Account   string `json:"account"`
 
-	// create mask of filtered packages
-	packagesMask := make([]int, nPkgs)
-	for i := 0; i < nPkgs; i++ {
-		index, _ := <- channel
-		if index != -1 {
-			packagesMask[index] = 1
-		}
-	}
+	// Optional fields
+	Type        *string `json:"type"`
+	B64Identity *string `json:"b64_identity"`
+	Url         *string `json:"url"`
 
-	// select packages according to mask
-	filteredPackages := make([]string, 0, nPkgs)
-	for i, pkgMask := range packagesMask {
-		if pkgMask == 1 {
-			filteredPackages = append(filteredPackages, (*msg.Packages)[i])
-		}
-	}
-	msg.Packages = &filteredPackages
-}
-
-// parse nevra and check arch, send index to channel, or -1 to remove
-func filterNevra(index int, pkg, arch string, channel chan int) {
-	nevra, err := utils.ParseNevra(pkg)
-	if err != nil {
-		utils.Log("err", err.Error(), "nevra", pkg).Error("unable to parse nevra")
-	}
-
-	if nevra.Arch == arch {
-		channel <- index
-	} else {
-		channel <- -1
-	}
-}
-
-func (msg *Message) ToJSON() []byte {
-	bytes, err := json.Marshal(*msg)
-	if err != nil {
-		utils.Log("err", err.Error()).Error("unable to jsonify message")
-	}
-	return bytes
-}
-
-func (msg *Message) JSONChecksum() string {
-	js := msg.ToJSON()
-	bytes := sha256.Sum256(js)
-	return hex.EncodeToString(bytes[:])
+	HostInfo *HostInfo `json:"host_info"`
 }
