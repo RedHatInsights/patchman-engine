4a6ce4a9b16bb496bd4b74363bd4dd44e9632b5e
Josef Hak <jhak@redhat.com>
2019-12-03 09:18:51 +0100

    added parametrized urls counters unification

--- manager/middlewares/prometheus.go
+++ manager/middlewares/prometheus.go
@@ -1,9 +1,24 @@
 package middlewares
 
-import ginprometheus "github.com/zsais/go-gin-prometheus"
+import (
+	"github.com/gin-gonic/gin"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"strings"
+)
 
 // Create and configure Prometheus middleware to expose metrics
 func Prometheus() *ginprometheus.Prometheus{
 	prometheus := ginprometheus.NewPrometheus("patchman_engine")
+	unifyParametrizedUrlsCounters(prometheus)
 	return prometheus
+}
+
+func unifyParametrizedUrlsCounters(p *ginprometheus.Prometheus) {
+	p.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
+		url := c.Request.URL.Path
+		for _, p := range c.Params {
+			url = strings.Replace(url, "/" + p.Value, "/:" + p.Key, 1)
+		}
+		return url
+	}
 }
