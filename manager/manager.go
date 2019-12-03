6035bb0e4b7da4773bc06b5e4ef6eaa32050fcca
Josef Hak <jhak@redhat.com>
2019-12-03 09:23:39 +0100

    swagger setup moved to middlewares package

--- manager/manager.go
+++ manager/manager.go
@@ -6,10 +6,6 @@ import (
 	"app/manager/routes"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	"github.com/swaggo/gin-swagger"
-
-	_ "app/docs"
 )
 
 
@@ -22,10 +18,8 @@ func RunManager() {
 	middlewares.Prometheus().Use(app)
 	app.Use(middlewares.RequestResponseLogger())
 	app.Use(gzip.Gzip(gzip.DefaultCompression))
+	middlewares.SetSwagger(app)
 	app.HandleMethodNotAllowed = true
-
-	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
-	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	// routes
 	routes.Init(app)
