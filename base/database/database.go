5acc44715796ce57b6edf4d471e8dc412efa767f
semtexzv <mhornick@redhat.com>
2019-12-06 13:06:40 +0100

    Add data model classes

--- base/database/database.go
+++ base/database/database.go
@@ -3,11 +3,11 @@ package database
 import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"app/base/structures"
+	"app/base/models"
 )
 
 // database cleaning method
 func DeleteAllRhAccounts() error {
-	err := Db.Delete(structures.RhAccountDAO{}).Error
+	err := Db.Delete(models.RhAccount{}).Error
 	return err
 }
