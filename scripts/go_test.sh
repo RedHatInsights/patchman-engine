eddce3d5855928451349a16e2f4f8723604733cb
Josef Hak <jhak@redhat.com>
2020-01-08 14:59:54 +0100

    set "go test -p 1" to run tests sequentially

--- scripts/go_test.sh
+++ scripts/go_test.sh
@@ -3,4 +3,5 @@
 set -o pipefail
 
 # Run go test and colorize output (PASS - green, FAIL - red).
-go test -v -coverprofile=coverage.txt -covermode=atomic ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+# Set "-p 1" to run test sequentially to avoid parallel changes in testing database.
+go test -v -p 1 -coverprofile=coverage.txt -covermode=atomic ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
