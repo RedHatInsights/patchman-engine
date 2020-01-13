e40691936ca422f4c5fb2223c9138e482ae0fb9a
Josef Hak <jhak@redhat.com>
2020-01-13 12:12:08 +0100

    added metrics for advisories storing and vmaas calling

--- vmaas_sync/metrics.go
+++ vmaas_sync/metrics.go
@@ -8,4 +8,16 @@ var (
 		Subsystem: "vmaas_sync",
 		Name:      "websocket_msgs",
 	}, []string{"type"})
+
+	vmaasCallCnt = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "patchman_engine",
+		Subsystem: "vmaas_sync",
+		Name:      "vmaas_call",
+	}, []string{"type"})
+
+	storeAdvisoriesCnt = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "patchman_engine",
+		Subsystem: "vmaas_sync",
+		Name:      "store_advisories",
+	}, []string{"type"})
 )
