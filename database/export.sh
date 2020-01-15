c29c8a8de4f0878e0bad7bc978ed2838bc555a89
semtexzv <mhornick@redhat.com>
2020-01-15 13:02:32 +0100

    Add schema diffing test

--- database/export.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -u
-
-export PGPASSWORD=${DB_PASSWD}
-pg_dump -U ${DB_USER} -h ${DB_HOST} ${DB_NAME} -s -O -T schema_migrations
--- database/migrate.sh
+++ database/migrate.sh
@@ -3,6 +3,7 @@
 # Install the migrate tool
 #go get -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate
 
+# Allow running migrate either from system installation or local download
 PATH=.:$PATH
 
 set -u
