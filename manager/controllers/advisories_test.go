7f846e224be89cdad3824070dd4f8fb79561b8e1
Josef Hak <jhak@redhat.com>
2020-01-21 07:29:19 +0100

    updated ApplicableSystems in advisories controller

--- manager/controllers/advisories_test.go
+++ manager/controllers/advisories_test.go
@@ -27,6 +27,7 @@ func TestAdvisoriesDefault(t *testing.T) {
 	assert.Equal(t, "2016-09-22 16:00:00 +0000 UTC", output.Data[0].Attributes.PublicDate.String())
 	assert.Equal(t, "adv-1-des", output.Data[0].Attributes.Description)
 	assert.Equal(t, "adv-1-syn", output.Data[0].Attributes.Synopsis)
+	assert.Equal(t, 7, output.Data[0].Attributes.ApplicableSystems)
 
 	// links
 	assert.Equal(t, "/api/patch/v1/advisories?offset=0&limit=25&data_format=json", output.Links.First)
@@ -111,4 +112,5 @@ func TestAdvisoriesOrder(t *testing.T) {
 	assert.Equal(t, "2017-09-22 19:00:00 +0000 UTC", output.Data[0].Attributes.PublicDate.String())
 	assert.Equal(t, "adv-7-des", output.Data[0].Attributes.Description)
 	assert.Equal(t, "adv-7-syn", output.Data[0].Attributes.Synopsis)
+	assert.Equal(t, 1, output.Data[0].Attributes.ApplicableSystems)
 }
