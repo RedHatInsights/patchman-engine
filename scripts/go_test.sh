9b93a9f49979b2ef9ee1165d0e783885693afcfc
semtexzv <mhornick@redhat.com>
2020-01-22 18:24:36 +0100

    Make schema test run before unit tests

--- scripts/go_test.sh
+++ scripts/go_test.sh
@@ -2,17 +2,9 @@
 
 set -o pipefail
 
-TEST_DIRS=$(go list ./... | grep -v "app/database")
+TEST_DIRS=${1:-./...}
 
-
-function run_tests() {
-  # Run go test and colorize output (PASS - green, FAIL - red).
-  # Set "-p 1" to run test sequentially to avoid parallel changes in testing database.
-  go test -v -p 1 -coverprofile=coverage.txt -covermode=atomic $1 | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
-}
-
-# Run normal tests
-run_tests "$TEST_DIRS"
-
-# Run destructive schema compatiblity test
-run_tests app/database
+# Run go test and colorize output (PASS - green, FAIL - red).
+# Set "-p 1" to run test sequentially to avoid parallel changes in testing database.
+go test -v -p 1 -coverprofile=coverage.txt -covermode=atomic $TEST_DIRS \
+  | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
--- scripts/go_test_db.sh
+++ scripts/go_test_db.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 set -o pipefail
+set -u
 
 # wait untill database is ready
 ./scripts/wait-for-services.sh
+
+# Normal test run - everything except database schema test
+TEST_DIRS=$(go list ./... | grep -v "app/database")
+
+# Run database test, destroys and recreates database
+go test -v app/database
 
 # fill database with testing data
 echo $DB_PASSWD | psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
@@ -11,4 +18,4 @@ echo $DB_PASSWD | psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
                        -a -q -f ./scripts/test_data.sql
 
 # run tests
-./scripts/go_test.sh
+./scripts/go_test.sh "${TEST_DIRS}"
