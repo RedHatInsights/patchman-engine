06a852d4a7182c98e005a8c1e7f0ef4fcda8d4db
Josef Hak <jhak@redhat.com>
2020-01-22 17:20:06 +0100

    added advanced system advisories metrics to manager

--- manager/metrics/metrics_test.go
+++ manager/metrics/metrics_test.go
@@ -28,3 +28,15 @@ func TestAdvisoryCounts(t *testing.T) {
 	assert.Equal(t, 3, bug)
 	assert.Equal(t, 2, sec)
 }
+
+func TestSystemAdvisoriesStats(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	stats, err := getSystemAdvisorieStats()
+	assert.Nil(t, err)
+	assert.Equal(t, 8, stats.MaxAll)
+	assert.Equal(t, 3, stats.MaxEnh)
+	assert.Equal(t, 3, stats.MaxBug)
+	assert.Equal(t, 2, stats.MaxSec)
+}
