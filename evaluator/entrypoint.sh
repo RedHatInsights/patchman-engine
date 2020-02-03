e52053a9176022b0334908a17f97abd1e2d4b91c
Josef Hak <jhak@redhat.com>
2020-02-03 13:58:06 +0100

    added mountings to docker-compose.yml for easier debugging

--- evaluator/entrypoint.sh
+++ evaluator/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # This script is launched inside the /go/src/app working directory
-./wait-for-services.sh ./main evaluator
+if [ -n $GORUN ]; then
+  # Running using 'go run'
+  ./scripts/wait-for-services.sh go run main.go evaluator
+else
+  ./scripts/wait-for-services.sh ./main evaluator
+fi
--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # This script is launched inside the /go/src/app working directory
-./wait-for-services.sh ./main manager
+if [ -n $GORUN ]; then
+  # Running using 'go run'
+  ./scripts/wait-for-services.sh go run main.go manager
+else
+  ./scripts/wait-for-services.sh ./main manager
+fi
