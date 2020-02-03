761ca09bcd943d5d145c7816e4dc6025e0a7f7a3
Josef Hak <jhak@redhat.com>
2020-02-03 14:25:10 +0100

    fixed tests running

--- scripts/go_test_db.sh
+++ scripts/go_test_db.sh
@@ -15,7 +15,7 @@ go test -v app/database
 # fill database with testing data
 echo $DB_PASSWD | psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
                        -v ON_ERROR_STOP=1 \
-                       -a -q -f ./scripts/test_data.sql
+                       -a -q -f ./dev/test_data.sql
 
 # run tests
 ./scripts/go_test.sh "${TEST_DIRS}"
