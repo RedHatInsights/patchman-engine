ce4b487d1d0c77b38e6f18ef9958ea10f1d94015
Michal Hornicky <mhornick@redhat.com>
2020-02-05 10:29:55 +0100

    Simplify system update trigger

--- database/schema/create_schema.sql
+++ database/schema/create_schema.sql
@@ -20,8 +20,16 @@ $empty$
 BEGIN
     RETURN t ~ '^[[:space:]]*$';
 END;
-$empty$
-    LANGUAGE 'plpgsql';
+$empty$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION ternary(cond BOOL, iftrue ANYELEMENT, iffalse ANYELEMENT)
+    returns ANYELEMENT
+AS
+$$
+SELECT CASE WHEN cond = TRUE THEN iftrue else iffalse END;
+$$
+    LANGUAGE SQL IMMUTABLE;
+
 
 -- set_first_reported
 CREATE OR REPLACE FUNCTION set_first_reported()
@@ -68,98 +76,103 @@ END;
 $check_unchanged$
     LANGUAGE 'plpgsql';
 
--- opt_out_system_update_cache
-CREATE OR REPLACE FUNCTION opt_out_system_update_cache()
-    RETURNS TRIGGER AS
-$opt_out_system_update_cache$
+-- count system advisories according to advisory type
+CREATE OR REPLACE FUNCTION system_advisories_count(system_id_in INT, advisory_type_id_in INT DEFAULT NULL)
+    RETURNS INT AS
+$system_advisories_count$
+DECLARE
+    result_cnt INT;
 BEGIN
-    IF (TG_OP = 'UPDATE') AND NEW.last_evaluation IS NOT NULL THEN
-        -- system opted out
-        IF OLD.opt_out = FALSE AND NEW.opt_out = TRUE THEN
-            -- decrement affected advisory counts for system
-            WITH to_update_advisories AS (
-                SELECT ead.advisory_id, ead.status_id AS global_status_id, sa.status_id
-                FROM advisory_account_data ead
-                         INNER JOIN
-                     system_advisories sa ON ead.advisory_id = sa.advisory_id
-                WHERE ead.rh_account_id = NEW.rh_account_id
-                  AND sa.system_id = NEW.id
-                  AND sa.when_patched IS NULL
-                ORDER BY ead.advisory_id
-                    FOR UPDATE OF ead
-                -- decrement systems_affected and systems_status_divergent in case status is different
-            ),
-                 update_divergent AS (
-                     UPDATE advisory_account_data ead
-                         SET systems_affected = systems_affected - 1,
-                             systems_status_divergent = systems_status_divergent - 1
-                         FROM to_update_advisories
-                         WHERE ead.advisory_id = to_update_advisories.advisory_id AND
-                               ead.rh_account_id = NEW.rh_account_id AND
-                               to_update_advisories.global_status_id != to_update_advisories.status_id
-                 )
-                 -- decrement only systems_affected in case status is same
-            UPDATE advisory_account_data ead
-            SET systems_affected = systems_affected - 1
-            FROM to_update_advisories
-            WHERE ead.advisory_id = to_update_advisories.advisory_id
-              AND ead.rh_account_id = NEW.rh_account_id
-              AND to_update_advisories.global_status_id = to_update_advisories.status_id;
-            -- delete zero advisory counts
-            DELETE
-            FROM advisory_account_data
-            WHERE rh_account_id = NEW.rh_account_id
-              AND systems_affected = 0;
-
-            -- system opted in
-        ELSIF OLD.opt_out = TRUE AND NEW.opt_out = FALSE THEN
-            -- increment affected advisory counts for system
-            WITH to_update_advisories AS (
-                SELECT ead.advisory_id, ead.status_id AS global_status_id, sa.status_id
-                FROM advisory_account_data ead
-                         INNER JOIN
-                     system_advisories sa ON ead.advisory_id = sa.advisory_id
-                WHERE ead.rh_account_id = NEW.rh_account_id
-                  AND sa.system_id = NEW.id
-                  AND sa.when_patched IS NULL
-                ORDER BY ead.advisory_id
-                    FOR UPDATE OF ead
-                -- increment systems_affected and systems_status_divergent in case status is different
-            ),
-                 update_divergent AS (
-                     UPDATE advisory_account_data ead
-                         SET systems_affected = systems_affected + 1,
-                             systems_status_divergent = systems_status_divergent + 1
-                         FROM to_update_advisories
-                         WHERE ead.advisory_id = to_update_advisories.advisory_id AND
-                               ead.rh_account_id = NEW.rh_account_id AND
-                               to_update_advisories.global_status_id != to_update_advisories.status_id
-                 )
-                 -- increment only systems_affected in case status is same
-            UPDATE advisory_account_data ead
-            SET systems_affected = systems_affected + 1
-            FROM to_update_advisories
-            WHERE ead.advisory_id = to_update_advisories.advisory_id
-              AND ead.rh_account_id = NEW.rh_account_id
-              AND to_update_advisories.global_status_id = to_update_advisories.status_id;
-            -- insert cache if not exists
-            INSERT INTO advisory_account_data (advisory_id, rh_account_id, systems_affected)
-            SELECT sa.advisory_id, NEW.rh_account_id, 1
-            FROM system_advisories sa
-            WHERE sa.system_id = NEW.id
-              AND sa.when_patched IS NULL
-              AND NOT EXISTS(
-                    SELECT 1
-                    FROM advisory_account_data
-                    WHERE rh_account_id = NEW.rh_account_id
-                      AND advisory_id = sa.advisory_id
-                )
-            ON CONFLICT (advisory_id, rh_account_id) DO UPDATE SET systems_affected = advisory_account_data.systems_affected + EXCLUDED.systems_affected;
-        END IF;
-    END IF;
-    RETURN NEW;
+    SELECT COUNT(advisory_id)
+    FROM system_advisories sa
+             JOIN advisory_metadata am ON sa.advisory_id = am.id
+    WHERE (am.advisory_type_id = advisory_type_id_in OR advisory_type_id_in IS NULL)
+      AND sa.system_id = system_id_in
+      AND sa.when_patched IS NULL
+    INTO result_cnt;
+    RETURN result_cnt;
 END;
-$opt_out_system_update_cache$ LANGUAGE 'plpgsql';
+$system_advisories_count$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION on_system_update()
+    RETURNS TRIGGER
+AS
+$system_update$
+BEGIN
+    IF TG_OP != 'UPDATE' OR NEW.last_evaluation IS NULL THEN
+        RETURN NEW;
+    END IF;
+
+    -- Nothing changed
+    IF OLD.opt_out = NEW.opt_out THEN
+        RETURN NEW;
+    END IF;
+
+    IF NEW.opt_out = TRUE THEN
+
+        WITH to_update_advisories AS (
+            SELECT ead.advisory_id, ternary(ead.status_id != sa.status_id, 1, 0) as divergent
+            FROM advisory_account_data ead
+                     INNER JOIN
+                 system_advisories sa ON ead.advisory_id = sa.advisory_id
+            WHERE ead.rh_account_id = NEW.rh_account_id
+              AND sa.system_id = NEW.id
+              AND sa.when_patched IS NULL
+            ORDER BY ead.advisory_id
+                FOR UPDATE OF ead
+            -- decrement systems_affected and systems_status_divergent in case status is different
+        ),
+             update AS (
+                 UPDATE advisory_account_data ead
+                     SET systems_affected = systems_affected - 1,
+                         systems_status_divergent = systems_status_divergent - ta.divergent
+                     FROM to_update_advisories ta
+                     WHERE ead.advisory_id = to_update_advisories.advisory_id AND
+                           ead.rh_account_id = NEW.rh_account_id
+             )
+        DELETE
+        FROM advisory_account_data
+        WHERE rh_account_id = NEW.rh_account_id
+          AND systems_affected = 0;
+
+    ELSIF NEW.opt_out = FALSE THEN
+        -- increment affected advisory counts for system
+        WITH to_update_advisories AS (
+            SELECT ead.advisory_id, ternary(ead.status_id != sa.status_id, 1, 0) as divergent
+            FROM advisory_account_data ead
+                     INNER JOIN system_advisories sa
+                                ON ead.advisory_id = sa.advisory_id
+            WHERE ead.rh_account_id = NEW.rh_account_id
+              AND sa.system_id = NEW.id
+              AND sa.when_patched IS NULL
+            ORDER BY ead.advisory_id FOR
+                UPDATE OF ead
+            -- increment systems_affected and systems_status_divergent in case status is different
+        ),
+             update as (
+                 -- increment only systems_affected in case status is same
+                 UPDATE advisory_account_data ead
+                     SET systems_affected = systems_affected + 1,
+                         systems_status_divergent = systems_status_divergent + ta.divergent
+                     FROM to_update_advisories ta
+                     WHERE ead.advisory_id = to_update_advisories.advisory_id
+                         AND ead.rh_account_id = NEW.rh_account_id)
+        INSERT
+        INTO advisory_account_data (advisory_id, rh_account_id, systems_affected)
+        SELECT sa.advisory_id, NEW.rh_account_id, 1
+        FROM system_advisories sa
+        WHERE sa.system_id = NEW.id
+          AND sa.when_patched IS NULL
+          AND NOT EXISTS(
+                SELECT 1
+                FROM advisory_account_data
+                WHERE rh_account_id = NEW.rh_account_id
+                  AND advisory_id = sa.advisory_id
+            )
+        ON CONFLICT (advisory_id, rh_account_id) DO UPDATE SET systems_affected = advisory_account_data.systems_affected + EXCLUDED.systems_affected;
+    END IF;
+END;
+$system_update$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION refresh_advisory_caches(advisory_id_in INTEGER DEFAULT NULL,
                                                    rh_account_id_in INTEGER DEFAULT NULL)
@@ -231,26 +244,6 @@ END;
 $update_system_caches$
     LANGUAGE 'plpgsql';
 
--- count system advisories according to advisory type
-CREATE OR REPLACE FUNCTION system_advisories_count(system_id_in INT, advisory_type_id_in INT DEFAULT NULL)
-    RETURNS INT AS
-$system_advisories_count$
-DECLARE
-    result_cnt INT;
-BEGIN
-    SELECT COUNT(advisory_id)
-    FROM system_advisories sa
-             JOIN advisory_metadata am ON sa.advisory_id = am.id
-    WHERE (am.advisory_type_id = advisory_type_id_in OR advisory_type_id_in IS NULL)
-      AND sa.system_id = system_id_in
-      AND sa.when_patched IS NULL
-    INTO result_cnt;
-    RETURN result_cnt;
-END;
-$system_advisories_count$ LANGUAGE 'plpgsql';
-
-
-
 -- refresh_all_cached_counts
 -- WARNING: executing this procedure takes long time,
 --          use only when necessary, e.g. during upgrade to populate initial caches
@@ -263,7 +256,6 @@ BEGIN
 END;
 $refresh_all_cached_counts$
     LANGUAGE 'plpgsql';
-
 
 CREATE OR REPLACE FUNCTION refresh_account_cached_counts(rh_account_in varchar)
     RETURNS void AS
@@ -279,7 +271,6 @@ BEGIN
 END;
 $refresh_account_cached_counts$
     LANGUAGE 'plpgsql';
-
 
 CREATE OR REPLACE FUNCTION refresh_advisory_cached_counts(advisory_name varchar)
     RETURNS void AS
@@ -305,11 +296,11 @@ BEGIN
     -- update system count for ordered advisories
     SELECT id FROM advisory_metadata WHERE name = advisory_name INTO advisory_md_id;
     SELECT id FROM rh_account WHERE name = rh_account_name INTO rh_account_id_in;
+
     PERFORM refresh_advisory_caches(advisory_md_id, rh_account_id_in);
 END;
 $refresh_advisory_account_cached_counts$
     LANGUAGE 'plpgsql';
-
 
 CREATE OR REPLACE FUNCTION refresh_system_cached_counts(inventory_id_in varchar)
     RETURNS void AS
@@ -324,7 +315,6 @@ BEGIN
 END;
 $refresh_system_cached_counts$
     LANGUAGE 'plpgsql';
-
 
 CREATE OR REPLACE FUNCTION delete_system(inventory_id_in varchar)
     RETURNS TABLE
@@ -418,7 +408,7 @@ CREATE TRIGGER system_platform_opt_out_cache
     AFTER UPDATE OF opt_out
     ON system_platform
     FOR EACH ROW
-EXECUTE PROCEDURE opt_out_system_update_cache();
+EXECUTE PROCEDURE on_system_update();
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON system_platform TO listener;
 -- evaluator needs to update last_evaluation
