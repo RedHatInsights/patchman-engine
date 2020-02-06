509226f79f2ccd40ec2093cf38c0f2bc79d90481
Josef Hak <jhak@redhat.com>
2020-02-06 16:52:11 +0100

    added vmaas_sync /recalc private endpoint to allow manual recalc trigger

---
+++ scripts/re-calc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# script to manually call vmaas_sync - re-calc
+
+curl http://localhost:9999/re-calc
--- vmaas_sync/vmaas_sync.go
+++ vmaas_sync/vmaas_sync.go
@@ -1,11 +1,7 @@
 package vmaas_sync //nolint:golint,stylecheck
 
 import (
-	"app/base/database"
-	"app/base/models"
-	"app/base/mqueue"
 	"app/base/utils"
-	"context"
 	"github.com/gorilla/websocket"
 	"time"
 )
@@ -75,38 +71,6 @@ func websocketHandler(data []byte, conn *websocket.Conn) error {
 		}
 	}
 	return nil
-}
-
-func sendReevaluationMessages() error {
-	var inventoryIDs []string
-	err := database.Db.Model(&models.SystemPlatform{}).
-		Pluck("inventory_id", &inventoryIDs).Error
-	if err != nil {
-		return err
-	}
-
-	ctx := context.Background()
-
-	for _, inventoryID := range inventoryIDs {
-		sendOneMessage(ctx, inventoryID)
-	}
-	return nil
-}
-
-func sendOneMessage(ctx context.Context, inventoryID string) {
-	tStart := time.Now()
-	defer utils.ObserveSecondsSince(tStart, messageSendDuration)
-
-	utils.Log("inventoryID", inventoryID).Debug("Sending evaluation kafka message")
-	event := mqueue.PlatformEvent{
-		ID: inventoryID,
-	}
-
-	err := evalWriter.WriteEvent(ctx, event)
-	if err != nil {
-		utils.Log("err", err.Error(), "inventoryID", inventoryID).
-			Error("inventory id sending to re-evaluate failed")
-	}
 }
 
 func RunVmaasSync() {
