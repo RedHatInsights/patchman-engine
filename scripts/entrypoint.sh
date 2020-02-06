ec8c403462ddeb9cfce608fa1667e25f4fb174b7
Michal Hornicky <mhornick@redhat.com>
2020-02-06 14:14:17 +0100

    Fix GORUN checking in entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 COMPONENT=$1
 # This script is launched inside the /go/src/app working directory
-if [ -n $GORUN ]; then
+if [[ -n $GORUN ]]; then
   # Running using 'go run'
   ./scripts/wait-for-services.sh go run main.go $COMPONENT
 else
