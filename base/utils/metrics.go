00b2162d185f04ab65e5ee8ed7ff97a422860b29
Josef Hak <jhak@redhat.com>
2020-02-06 14:06:25 +0100

    fixed duration observing in duration metrics

---
+++ base/utils/metrics.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+)
+
+func ObserveSecondsSince(timeStart time.Time, observer prometheus.Observer) {
+	observer.Observe(time.Since(timeStart).Seconds())
+}
