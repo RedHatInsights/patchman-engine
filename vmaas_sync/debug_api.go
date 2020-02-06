da22fcd2cdc9c0b05a0a920d11c2fe6c20e943a5
Josef Hak <jhak@redhat.com>
2020-02-06 16:52:11 +0100

    added vmaas_sync /recalc private endpoint to allow manual recalc trigger

--- vmaas_sync/debug_api.go
+++ vmaas_sync/debug_api.go
@@ -12,11 +12,23 @@ func runDebugAPI() {
 		utils.Log().Info("manual syncing called...")
 		err := syncAdvisories()
 		if err != nil {
-			utils.Log("err", err.Error()).Error("manually called syncing failed")
+			utils.Log("err", err.Error()).Error("manual called syncing failed")
 			c.JSON(http.StatusInternalServerError, "error")
 			return
 		}
 		utils.Log().Info("manual syncing finished successfully")
+		c.JSON(http.StatusOK, "OK")
+	})
+
+	app.GET("/re-calc", func(c *gin.Context) {
+		utils.Log().Info("manual re-calc messages sending called...")
+		err := sendReevaluationMessages()
+		if err != nil {
+			utils.Log("err", err.Error()).Error("manual re-calc msgs sending failed")
+			c.JSON(http.StatusInternalServerError, "error")
+			return
+		}
+		utils.Log().Info("manual re-calc messages sent successfully")
 		c.JSON(http.StatusOK, "OK")
 	})
 
