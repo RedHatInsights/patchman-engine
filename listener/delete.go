54f7d876578e34f39630479065aa296ac7529be0
Josef Hak <jhak@redhat.com>
2020-02-10 13:27:10 +0100

    added inventoryID log info to listener delete part

--- listener/delete.go
+++ listener/delete.go
@@ -12,13 +12,13 @@ func deleteHandler(event mqueue.PlatformEvent) {
 	defer utils.ObserveSecondsSince(tStart, messageHandlingDuration.WithLabelValues(EventDelete))
 
 	if event.Type == nil {
-		utils.Log().Warn("empty event type received")
+		utils.Log("inventoryID", event.ID).Warn("empty event type received")
 		messagesReceivedCnt.WithLabelValues(EventDelete, ReceivedErrorOtherType)
 		return
 	}
 
 	if *event.Type != "delete" {
-		utils.Log("eventType", *event.Type).Warn("non-delete event type received")
+		utils.Log("inventoryID", event.ID, "eventType", *event.Type).Warn("non-delete event type received")
 		messagesReceivedCnt.WithLabelValues(EventDelete, ReceivedErrorOtherType)
 		return
 	}
@@ -26,11 +26,11 @@ func deleteHandler(event mqueue.PlatformEvent) {
 	query := database.Db.Exec("select deleted_inventory_id from delete_system(?)", event.ID)
 	err := query.Error
 	if err != nil {
-		utils.Log("id", event.ID, "err", err.Error()).Error("Could not delete system")
+		utils.Log("inventoryID", event.ID, "err", err.Error()).Error("Could not delete system")
 		messagesReceivedCnt.WithLabelValues(EventDelete, ReceivedErrorProcessing)
 		return
 	}
 
-	utils.Log("id", event.ID, "count", query.RowsAffected).Info("Systems deleted")
+	utils.Log("inventoryID", event.ID, "count", query.RowsAffected).Info("Systems deleted")
 	messagesReceivedCnt.WithLabelValues(EventDelete, ReceivedSuccess)
 }
