9165088a7aec273d030d2b20849d6575e5668cac
Michal Hornicky <mhornick@redhat.com>
2020-02-12 12:14:47 +0100

    Base RBAC implementation

--- base/base.go
+++ base/base.go
@@ -2,6 +2,7 @@ package base
 
 const InventoryAPIPrefix = "/api/inventory/v1"
 const VMaaSAPIPrefix = "/api"
+const RBACApiPrefix = "/api/rbac/v1"
 
 // Go datetime parser does not like slightly incorrect RFC 3339 which we are using (missing Z )
 const Rfc3339NoTz = "2006-01-02T15:04:05-07:00"
---
+++ manager/middlewares/rbac.go
@@ -0,0 +1,52 @@
+package middlewares
+
+import (
+	"app/base"
+	"app/base/utils"
+	"context"
+	"github.com/RedHatInsights/patchman-clients/inventory"
+	"github.com/RedHatInsights/patchman-clients/rbac"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var rbacClient *rbac.APIClient
+
+func init() {
+	traceAPI := utils.GetenvOrFail("LOG_LEVEL") == "trace"
+
+	rbacConfig := rbac.NewConfiguration()
+	rbacConfig.Debug = traceAPI
+
+	rbacConfig.BasePath = utils.GetenvOrFail("RBAC_ADDRESS") + base.RBACApiPrefix
+
+	rbacClient = rbac.NewAPIClient(rbacConfig)
+}
+
+func checkRbac(c *gin.Context) bool {
+	apiKey := rbac.APIKey{Prefix: "", Key: c.GetHeader("x-rh-identity")}
+	// Create new context, which has the apikey value set. This is then used as a value for `x-rh-identity`
+	ctx := context.WithValue(context.Background(), inventory.ContextAPIKey, apiKey)
+
+	access, _, err := rbacClient.AccessApi.GetPrincipalAccess(ctx, "patch", nil)
+	if err != nil {
+		return false
+	}
+
+	for _, a := range access.Data {
+		if a.Permission == "patch:*:*" {
+			return true
+		}
+	}
+	return false
+}
+
+
+func RBAC() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if !checkRbac(c) {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, utils.ErrorResponse{Error: "RBAC check failed"})
+			return
+		}
+	}
+}
