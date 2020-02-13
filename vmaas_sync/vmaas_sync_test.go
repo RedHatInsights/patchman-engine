c1fa5accc7fb5744350c806d9115c4ace4e2dbf5
Josef Hak <jhak@redhat.com>
2020-02-13 11:25:59 +0100

    added vmaas_sync test

---
+++ vmaas_sync/vmaas_sync_test.go
@@ -0,0 +1,38 @@
+package vmaas_sync //nolint:golint,stylecheck
+import (
+	"app/base/core"
+	"app/base/database"
+	"app/base/models"
+	"app/base/mqueue"
+	"app/base/utils"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var events []mqueue.PlatformEvent
+
+type mockKafkaWriter struct{}
+
+func (t mockKafkaWriter) WriteEvent(_ context.Context, ev mqueue.PlatformEvent) error {
+	events = append(events, ev)
+	return nil
+}
+
+func TestSync(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+	configure()
+
+	var mock mqueue.Writer = mockKafkaWriter{}
+	evalWriter = &mock
+
+	err := websocketHandler([]byte("webapps-refreshed"), nil)
+	assert.Nil(t, err)
+
+	expected := []string{"ER1", "ER2", "ER3"}
+	database.CheckAdvisoriesInDb(t, expected)
+	assert.Nil(t, database.Db.Unscoped().Where("name IN (?)", expected).Delete(&models.AdvisoryMetadata{}).Error)
+
+	assert.Equal(t, 12, len(events))
+}
