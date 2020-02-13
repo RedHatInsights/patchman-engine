870d62b8568d4b1c3165b78efe867124fa9986a3
Josef Hak <jhak@redhat.com>
2020-02-13 15:41:39 +0100

    added listener readers init test

---
+++ listener/listener_test.go
@@ -0,0 +1,33 @@
+package listener
+
+import (
+	"app/base/mqueue"
+	"github.com/bmizerany/assert"
+	"testing"
+	"time"
+)
+
+var mockReaders []*mqueue.Reader
+
+type mockReader struct {
+	Topic            string
+	HandleEventCalls int
+}
+
+func (t *mockReader) HandleEvents(_ mqueue.EventHandler) {
+	t.HandleEventCalls++
+}
+func (t *mockReader) Shutdown() {}
+
+func createMockReader(topic string) *mqueue.Reader {
+	var reader mqueue.Reader = &mockReader{Topic: topic}
+	mockReaders = append(mockReaders, &reader)
+	return &reader
+}
+
+func TestRunReaders(t *testing.T) {
+	runReaders(createMockReader)
+	time.Sleep(time.Millisecond * 100)
+	// it will create CONSUMER_COUNT (8) * topics (2) = readers (16)
+	assert.Equal(t, 16, len(mockReaders))
+}
