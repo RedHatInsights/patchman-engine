49b9e30db0a4277f6f3b8ee7588baecc1867f734
Josef Hak <jhak@redhat.com>
2020-02-28 15:34:01 +0100

    extended listener delete tests

--- listener/delete_test.go
+++ listener/delete_test.go
@@ -3,6 +3,8 @@ package listener
 import (
 	"app/base/core"
 	"app/base/utils"
+	"github.com/bmizerany/assert"
+	log "github.com/sirupsen/logrus"
 	"testing"
 )
 
@@ -19,4 +21,37 @@ func TestDeleteSystem(t *testing.T) {
 	deleteEvent := createTestDeleteEvent(id)
 	deleteHandler(deleteEvent)
 	assertSystemNotInDb(t)
+}
+
+func TestDeleteSystemWarn1(t *testing.T) {
+	logHook := utils.NewTestLogHook()
+	log.AddHook(logHook)
+	deleteEvent := createTestDeleteEvent(id)
+	deleteEvent.Type = nil
+	deleteHandler(deleteEvent)
+	assert.Equal(t, WarnEmptyEventType, logHook.LogEntries[len(logHook.LogEntries)-1].Message)
+}
+
+func TestDeleteSystemWarn2(t *testing.T) {
+	logHook := utils.NewTestLogHook()
+	log.AddHook(logHook)
+	deleteEvent := createTestDeleteEvent(id)
+	nonDeleteType := "no-delete"
+	deleteEvent.Type = &nonDeleteType
+	deleteHandler(deleteEvent)
+	assert.Equal(t, WarnNoDeleteType, logHook.LogEntries[len(logHook.LogEntries)-1].Message)
+}
+
+func TestDeleteSystemWarn3(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+	configure()
+
+	logHook := utils.NewTestLogHook()
+	log.AddHook(logHook)
+
+	deleteEvent := createTestDeleteEvent("not-existing-id")
+	deleteHandler(deleteEvent)
+
+	assert.Equal(t, WarnNoRowsModified, logHook.LogEntries[len(logHook.LogEntries)-1].Message)
 }
