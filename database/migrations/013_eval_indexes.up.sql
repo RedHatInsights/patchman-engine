022b91409bfefc9c049aa10beecd45d99bf5f84c
Michal Hornicky <mhornick@redhat.com>
2020-03-09 13:36:47 +0100

    Update system_platform and advisory-account_data tables for better performance

---
+++ database/migrations/013_eval_indexes.up.sql
@@ -0,0 +1,16 @@
+--CREATE UNIQUE INDEX advisory_account_data_rh_account_id_advisory_id_key ON advisory_account_data (rh_account_id, advisory_id);
+
+ALTER TABLE system_advisories
+    DROP CONSTRAINT system_advisories_system_id_advisory_id_key;
+
+ALTER TABLE system_advisories
+    DROP CONSTRAINT system_advisories_pkey;
+
+ALTER TABLE system_advisories
+    DROP COLUMN id;
+
+ALTER TABLE system_advisories
+    ADD PRIMARY KEY (system_id, advisory_id);
+
+ALTER TABLE advisory_account_data
+    ADD CONSTRAINT advisory_account_data_pkey PRIMARY KEY (rh_account_id, advisory_id);
