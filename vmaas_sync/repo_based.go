ba2aa031670be2ad646bf0127bf0eea5fe5b3910
Josef Hak <jhak@redhat.com>
2020-03-09 16:32:01 +0100

    added repo based re-evaluation messages sending

--- vmaas_sync/repo_based.go
+++ vmaas_sync/repo_based.go
@@ -10,6 +10,24 @@ import (
 
 const LastEvalRepoBased = "last_eval_repo_based"
 
+func getCurrentRepoBasedInventoryIDs() (*[]string, error) {
+	lastRepoBaseEval, err := getLastRepobasedEvalTms()
+	if err != nil {
+		return nil, err
+	}
+
+	updateRepos, err := getUpdatedRepos(*lastRepoBaseEval)
+	if err != nil {
+		return nil, err
+	}
+
+	inventoryIDs, err := getRepoBasedInventoryIDs(updateRepos)
+	if err != nil {
+		return nil, err
+	}
+	return inventoryIDs, nil
+}
+
 func getLastRepobasedEvalTms() (*time.Time, error) {
 	var timestamps []*time.Time
 	err := database.Db.Model(&models.TimestampKV{}).
@@ -25,12 +43,12 @@ func getLastRepobasedEvalTms() (*time.Time, error) {
 	return timestamps[0], nil
 }
 
-func getRepoBasedInventoryIDs(repos []string) (*[]string, error) {
+func getRepoBasedInventoryIDs(repos *[]string) (*[]string, error) {
 	var intentoryIDs []string
 	err := database.Db.Table("system_repo sr").
 		Joins("JOIN repo ON repo.id = sr.repo_id").
 		Joins("JOIN system_platform sp ON sp.id = sr.system_id").
-		Where("repo.name IN (?)", repos).
+		Where("repo.name IN (?)", *repos).
 		Order("inventory_id ASC").
 		Pluck("inventory_id", &intentoryIDs).Error
 	if err != nil {
