efa648d60fb54d6fcd6c54660675619f612bd806
Josef Hak <jhak@redhat.com>
2020-03-12 17:51:32 +0100

    well arranged go_test_on_ci.sh script

--- scripts/go_test_db.sh
+++ scripts/go_test_db.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-# wait untill database is ready
+# Wait untill database is ready
 ./scripts/wait-for-services.sh
-
-# Normal test run - everything except database schema test
-TEST_DIRS=$(go list ./... | grep -v "app/database")
 
 # Run database test, destroys and recreates database
 go test -v app/database
 
-# fill database with testing data
+# Fill database with testing data
 ./scripts/feed_db.sh
 
-# run tests
+# Normal test run - everything except database schema test
+TEST_DIRS=$(go list ./... | grep -v "app/database")
 ./scripts/go_test.sh "${TEST_DIRS}"
