1efae91b6eda0c20dcc35011b1348bc6bb17dcff
Josef Hak <jhak@redhat.com>
2020-03-13 16:33:12 +0100

    updated tests running script

--- scripts/feed_db.sh
+++ scripts/feed_db.sh
@@ -4,6 +4,6 @@
 ./scripts/wait-for-services.sh
 
 # fill database with testing data
-echo $DB_PASSWD | psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
+PGPASSWORD=$DB_PASSWD psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
                        -v ON_ERROR_STOP=1 \
                        -a -q -f ./dev/test_data.sql
