253b7a262cad6a02daa3f0510ffb63c757f6a899
Josef Hak <jhak@redhat.com>
2020-03-13 15:49:01 +0100

    added database update

--- database/migrate.sh
+++ database/migrate.sh
@@ -2,14 +2,4 @@
 
 set -o pipefail
 
-export PGHOST=$DB_HOST
-export PGUSER=$DB_USER
-export PGDATABASE=$DB_NAME
-export PGPORT=$DB_PORT
-export PGPASSWORD=$DB_PASSWD
-
-psql -a -f /database/schema/create_users.sql
-
 /migrate -source file://database/migrations -database postgres://$DB_HOST/$DB_NAME$MIGRATE_DB_URL_PARAMS up
-
-sleep infinity
