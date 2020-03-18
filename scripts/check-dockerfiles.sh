02d43a56fae2ab2ab015dac74df5ca12674c384e
Josef Hak <jhak@redhat.com>
2020-03-18 11:00:19 +0100

    added database dockerfiles

--- scripts/check-dockerfiles.sh
+++ scripts/check-dockerfiles.sh
@@ -3,7 +3,7 @@
 rc=0
 
 # Check consistent testing (centos) and production (redhat) dockerfiles.
-for dockerfile in "Dockerfile"
+for dockerfile in "Dockerfile" "database/Dockerfile"
 do
     if [ ! -f "$dockerfile.centos" ]; then
         echo "Dockerfile '$dockerfile.centos' doesn't exist" >&2
