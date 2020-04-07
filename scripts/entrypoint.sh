a6b90d98769f93128a894d5cf820b0643dc1a434
Josef Hak <jhak@redhat.com>
2020-04-07 16:12:21 +0200

    added exec to entrypoints

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ COMPONENT=$1
 # This script is launched inside the /go/src/app working directory
 if [[ -n $GORUN ]]; then
   # Running using 'go run'
-  ./scripts/wait-for-services.sh go run main.go $COMPONENT
+  exec ./scripts/wait-for-services.sh go run main.go $COMPONENT
 else
-  ./scripts/wait-for-services.sh ./main $COMPONENT
+  exec ./scripts/wait-for-services.sh ./main $COMPONENT
 fi
