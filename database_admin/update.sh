8b1d534cb6b1da948869b8ca006dfd05d89fc74a
Michal Hornicky <mhornick@redhat.com>
2020-04-15 12:12:04 +0200

    Log database statements, fix crashing migrations

--- database_admin/update.sh
+++ database_admin/update.sh
@@ -30,3 +30,6 @@ psql -c "ALTER USER listener WITH PASSWORD '${LISTENER_PASSWORD}'"
 psql -c "ALTER USER evaluator WITH PASSWORD '${EVALUATOR_PASSWORD}'"
 psql -c "ALTER USER manager WITH PASSWORD '${MANAGER_PASSWORD}'"
 psql -c "ALTER USER vmaas_sync WITH PASSWORD '${VMAAS_SYNC_PASSWORD}'"
+
+echo "Setting database config"
+psql -f './database_admin/config.sql'
