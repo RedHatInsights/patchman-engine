957718b2aa3bfab1542e67c5baa739aaf3b75a14
Michal Hornicky <mhornick@redhat.com>
2020-04-15 14:11:53 +0200

    Better error handling for system culling

--- vmaas_sync/system_culling.go
+++ vmaas_sync/system_culling.go
@@ -4,23 +4,41 @@ import (
 	"app/base"
 	"app/base/database"
 	"app/base/utils"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 	"time"
 )
+
+// Need to run code within a function, because defer can't be used in loops
+func withTx(do func(db *gorm.DB) error) error {
+	tx := database.Db.BeginTx(base.Context, nil)
+	defer tx.RollbackUnlessCommitted()
+	if err := do(tx); err != nil {
+		return err
+	}
+	return errors.Wrap(tx.Commit().Error, "Commit")
+}
 
 func RunSystemCulling() {
 	ticker := time.NewTicker(time.Minute * 10)
 
 	for {
 		<-ticker.C
-		tx := database.Db.BeginTx(base.Context, nil)
-		tx.Exec("select delete_culled_systems()")
-		tx.Exec("select mark_stale_systems()")
 
-		if err := tx.Commit().Error; err != nil {
-			utils.Log("err", err.Error()).Info("Commit of system culling failed")
-			tx.RollbackUnlessCommitted()
-			return
+		err := withTx(func(tx *gorm.DB) error {
+			if err := tx.Exec("select delete_culled_systems()").Error; err != nil {
+				return errors.Wrap(err, "Delete culled")
+			}
+			if err := tx.Exec("select mark_stale_systems()").Error; err != nil {
+				return errors.Wrap(err, "Mark stale")
+			}
+			return nil
+		})
+
+		if err != nil {
+			utils.Log("err", err.Error()).Error("System culling")
+		} else {
+			utils.Log().Info("System culling tasks performed successfully")
 		}
-		utils.Log().Info("System culling tasks performed successfully")
 	}
 }
