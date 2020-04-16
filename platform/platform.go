b7895477da1258ec1bc8337fa96696cf2880602b
Josef Hak <jhak@redhat.com>
2020-04-16 10:50:36 +0200

    added option to testing upload in loop

--- platform/platform.go
+++ platform/platform.go
@@ -4,12 +4,15 @@ import (
 	"app/base"
 	"app/base/utils"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
 	"net/http"
+	"time"
 )
 
 var websockets []chan string
+var runUploadLoop = false
 
 func AddWebsocket() chan string {
 	ws := make(chan string, 100)
@@ -27,6 +30,7 @@ func platformMock() {
 	app.POST("/control/upload", MockUploadHandler)
 	app.POST("/control/delete", MockDeleteHandler)
 	app.POST("/control/sync", MockSyncHandler)
+	app.POST("/control/toggle_upload", MockToggleUpload)
 
 	err := utils.RunServer(base.Context, app, ":9001")
 	if err != nil {
@@ -67,6 +71,22 @@ func mockIdentity() string {
 	return identity
 }
 
+func upload(randomPkgs bool) {
+	event := map[string]interface{}{
+		"type": "created",
+		"host": map[string]interface{}{
+			"id":             "TEST-0000",
+			"account":        "TEST-0000",
+			"system_profile": makeSystemProfile("TEST-0000", randomPkgs),
+		},
+	}
+	msg, err := json.Marshal(event)
+	if err != nil {
+		panic(err)
+	}
+	sendMessageToTopic("platform.inventory.host-egress", string(msg))
+}
+
 func sendMessageToTopic(topic, message string) {
 	writer := mockKafkaWriter(topic)
 
@@ -79,22 +99,10 @@ func sendMessageToTopic(topic, message string) {
 		panic(err)
 	}
 }
+
 func MockUploadHandler(c *gin.Context) {
 	utils.Log().Info("Mocking platform upload event")
-
-	event := map[string]interface{}{
-		"type": "created",
-		"host": map[string]interface{}{
-			"id":             "TEST-0000",
-			"account":        "TEST-0000",
-			"system_profile": makeSystemProfile("TEST-0000"),
-		},
-	}
-	msg, err := json.Marshal(event)
-	if err != nil {
-		panic(err)
-	}
-	sendMessageToTopic("platform.inventory.host-egress", string(msg))
+	upload(false)
 	c.Status(http.StatusOK)
 }
 
@@ -115,6 +123,27 @@ func MockDeleteHandler(c *gin.Context) {
 	c.Status(http.StatusOK)
 }
 
+func MockToggleUpload(c *gin.Context) {
+	runUploadLoop = !runUploadLoop
+	c.JSON(http.StatusOK, fmt.Sprintf("%v", runUploadLoop))
+}
+
+func uploader() {
+	var i int
+	for {
+		if runUploadLoop {
+			upload(true)
+			i++
+			utils.Log("iteration", i).Info("upload loop running")
+			time.Sleep(time.Millisecond * 10)
+		} else {
+			i = 0
+			time.Sleep(time.Second)
+		}
+	}
+}
+
 func main() {
+	go uploader()
 	platformMock()
 }
