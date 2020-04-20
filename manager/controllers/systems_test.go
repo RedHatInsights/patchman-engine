8b66fca013aa0d8dfab6245ace85a1a2c7f083d7
Michal Hornicky <mhornick@redhat.com>
2020-04-20 10:42:16 +0200

    Change default sort key for systems to -last_upload

--- manager/controllers/systems_test.go
+++ manager/controllers/systems_test.go
@@ -32,8 +32,8 @@ func TestSystemsDefault(t *testing.T) {
 	assert.Equal(t, true, output.Data[0].Attributes.Enabled)
 
 	// links
-	assert.Equal(t, "/api/patch/v1/systems?offset=0&limit=25&filter[stale]=eq:false&sort=id", output.Links.First)
-	assert.Equal(t, "/api/patch/v1/systems?offset=0&limit=25&filter[stale]=eq:false&sort=id", output.Links.Last)
+	assert.Equal(t, "/api/patch/v1/systems?offset=0&limit=25&filter[stale]=eq:false&sort=-last_upload", output.Links.First)
+	assert.Equal(t, "/api/patch/v1/systems?offset=0&limit=25&filter[stale]=eq:false&sort=-last_upload", output.Links.Last)
 	assert.Nil(t, output.Links.Next)
 	assert.Nil(t, output.Links.Previous)
 
