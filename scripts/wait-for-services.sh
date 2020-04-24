0a5dbadc4cf2561fb3ca925571359891edb058df
Josef Hak <jhak@redhat.com>
2020-04-24 10:09:44 +0200

    removed not working Kafka check from wait-for-services.sh

--- scripts/wait-for-services.sh
+++ scripts/wait-for-services.sh
@@ -19,15 +19,5 @@ else
   >&2 echo "Skipping PostgreSQL check"
 fi
 
-if [ ! -z "$KAFKA_ADDRESS" ] && echo "from kafka import KafkaConsumer" | python3 &> /dev/null; then
-  >&2 echo "Checking if Kafka server is up"
-  until echo "from kafka import KafkaConsumer;c=KafkaConsumer(bootstrap_servers=[\"$KAFKA_ADDRESS\"]);c.close()" | python3 &> /dev/null; do
-    >&2 echo "Kafka server is unavailable - sleeping"
-    sleep 1
-  done
-else
-  >&2 echo "Skipping Kafka server check"
-fi
-
 >&2 echo "Everything is up - executing command"
 exec $cmd
