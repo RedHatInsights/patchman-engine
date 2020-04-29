8beea8ab9ef65db67822f87cc95793ff751d3719
Michael Mraka <michael.mraka@redhat.com>
2020-04-29 13:41:05 +0200

    added indexes to speed up queries
    SELECT MAX(advisory_count_cache) as max_all, MAX(advisory_enh_count_cache) as max_enh, MAX(advisory_bug_count_cache) as max_bug, MAX(advisory_sec_count_cache) as max_sec from system_platform

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -466,6 +466,10 @@ CREATE TABLE IF NOT EXISTS system_platform
 
 CREATE INDEX ON system_platform (rh_account_id);
 CREATE INDEX ON system_platform (last_upload DESC NULLS LAST);
+CREATE INDEX ON system_platform (advisory_count_cache DESC NULLS LAST);
+CREATE INDEX ON system_platform (advisory_enh_count_cache DESC NULLS LAST);
+CREATE INDEX ON system_platform (advisory_bug_count_cache DESC NULLS LAST);
+CREATE INDEX ON system_platform (advisory_sec_count_cache DESC NULLS LAST);
 
 CREATE TRIGGER system_platform_set_first_reported
     BEFORE INSERT
