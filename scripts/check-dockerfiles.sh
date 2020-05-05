456709f4925162e03bdfd75a70781964049fd34b
Josef Hak <jhak@redhat.com>
2020-05-05 10:49:05 +0200

    upgraded database from postgres 10 to 12

--- scripts/check-dockerfiles.sh
+++ scripts/check-dockerfiles.sh
@@ -14,7 +14,7 @@ do
       if [ -f "$dockerfile.$suffix" ]; then
         sed \
             -e "s/centos:7/registry.access.redhat.com\/rhel7/" \
-            -e "s/centos\/postgresql-10-centos7/registry.access.redhat.com\/rhscl\/postgresql-10-rhel7/" \
+            -e "s/centos\/postgresql-12-centos7/registry.redhat.io\/rhscl\/postgresql-12-rhel7/" \
             -e "s/yum -y install centos-release-scl/yum-config-manager --enable rhel-server-rhscl-7-rpms/" \
             -e "s/centos:8/registry.access.redhat.com\/ubi8/" \
             -e "s/RUN rpm --import \/etc\/pki\/rpm-gpg\/RPM-GPG-KEY-centosofficial//" \
