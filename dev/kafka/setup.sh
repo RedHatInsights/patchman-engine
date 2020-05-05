9fecc44d5bc38878cea55da20151156641ab024c
Michal Hornicky <mhornick@redhat.com>
2020-05-05 10:26:39 +0200

    Handle 'updated' events

--- dev/kafka/setup.sh
+++ dev/kafka/setup.sh
@@ -4,7 +4,7 @@
 for topic in "platform.inventory.host-egress" "platform.inventory.events" "patchman.evaluator.upload" \
              "patchman.evaluator.recalc" "test"
 do
-    until /usr/bin/kafka-topics --create --topic $topic --partitions 1 --zookeeper zookeeper:2181 \
+    until /usr/bin/kafka-topics --create --if-not-exists --topic $topic --partitions 1 --zookeeper zookeeper:2181 \
     --replication-factor 1; do
       echo "Unable to create topic $topic"
       sleep 1
