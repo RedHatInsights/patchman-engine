30430f00a9e30f167c810896a36461993fa0c0ab
Josef Hak <jhak@redhat.com>
2020-08-17 12:48:05 +0200

    added openapi.json file checker, added to test pipeline

---
+++ scripts/check-openapi-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp docs/openapi.json docs/openapi1.json
+./scripts/generate_docs.sh
+diff docs/openapi.json docs/openapi1.json
+rc=$?
+if [ $rc -gt 0 ]; then
+  echo "docs/openapi.json different from file generated with './scripts/generate_docs.sh'!"
+else
+  echo "docs/openapi.json consistent with generated file."
+fi
+
+exit $rc
