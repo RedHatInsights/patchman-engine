d9662c4c13de109a02e26f51c6069223401fe420
Ales Kasparek <akaspare@redhat.com>
2020-09-08 08:55:52 +0200

    feat (vmaas_sync): Cyndi metrics
    PR changes
    
    PR change
    
    PR changes

---
+++ vmaas_sync/metrics_cyndi.go
@@ -0,0 +1,120 @@
+package vmaas_sync //nolint:golint,stylecheck
+
+import (
+	"app/base/database"
+	"app/base/utils"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"time"
+)
+
+const (
+	allSystemCount        = "allSystem"
+	uniqueTagsCount       = "uniqueTags"
+	systemsSapSystemCount = "systemsSapSystem"
+	systemsWithTagsCount  = "systemsWithTag"
+)
+
+var (
+	cyndiSystemsCnt = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Help:      "How many systems are stored and how up-to-date they are",
+		Namespace: "patchman_engine",
+		Subsystem: "vmaas_sync",
+		Name:      "cyndi_systems",
+	}, []string{"type"})
+
+	cyndiTagsCnt = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Help:      "How many inventory hosts and which tags are there",
+		Namespace: "patchman_engine",
+		Subsystem: "vmaas_sync",
+		Name:      "cyndi_tags_count",
+	}, []string{"type"})
+)
+
+type InventoryHostsStats struct {
+	SystemsCount    int
+	UniqueTags      int
+	SapCount        int
+	SystemsWithTags int
+}
+
+func updateCyndiData() {
+	stats, err := getCyndiData()
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi metrics")
+		stats = InventoryHostsStats{}
+	}
+	cyndiTagsCnt.WithLabelValues(allSystemCount).Set(float64(stats.SystemsCount))
+	cyndiTagsCnt.WithLabelValues(uniqueTagsCount).Set(float64(stats.UniqueTags))
+	cyndiTagsCnt.WithLabelValues(systemsSapSystemCount).Set(float64(stats.SapCount))
+	cyndiTagsCnt.WithLabelValues(systemsWithTagsCount).Set(float64(stats.SystemsWithTags))
+}
+
+func getCyndiData() (stats InventoryHostsStats, err error) {
+	err = database.Db.Table("inventory.hosts").Count(&stats.SystemsCount).Error
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi metrics")
+		return stats, err
+	}
+
+	err = database.Db.Table("(SELECT DISTINCT jsonb_array_elements(tags) FROM inventory.hosts) as t").
+		Count(&stats.UniqueTags).Error
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi metrics")
+		return stats, err
+	}
+
+	err = database.Db.Table("inventory.hosts").Where("system_profile -> 'sap_system' = 'true'").
+		Count(&stats.SapCount).Error
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi metrics")
+		return stats, err
+	}
+
+	err = database.Db.Table("inventory.hosts").Select("tags").Where("jsonb_array_length(tags) > 0").
+		Count(&stats.SystemsWithTags).Error
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi metrics")
+		return stats, err
+	}
+
+	return stats, nil
+}
+
+func updateCyndiSystemMetrics() {
+	counts, err := getCyndiCounts(time.Now())
+	if err != nil {
+		utils.Log("err", err.Error()).Error("unable to update cyndi system metrics")
+		return
+	}
+
+	for labels, count := range counts {
+		cyndiSystemsCnt.WithLabelValues(labels).Set(float64(count))
+	}
+}
+
+func getCyndiCounts(refTime time.Time) (map[string]int, error) {
+	systemsQuery := database.Db.Table("inventory.hosts")
+	lastUploadKV := map[string]int{lastUploadLast1D: 1, lastUploadLast7D: 7, lastUploadLast30D: 30, lastUploadAll: -1}
+	counts := map[string]int{}
+	for lastUploadK, lastUploadV := range lastUploadKV {
+		systemsQueryOptOutLastUpload := updateCyndiQueryLastUpload(systemsQuery, refTime, lastUploadV)
+		var nSystems int
+		err := systemsQueryOptOutLastUpload.Count(&nSystems).Error
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to load systems counts: "+
+				fmt.Sprintf("last_upload_before_days: %v", lastUploadV))
+		}
+		counts[lastUploadK] = nSystems
+	}
+	return counts, nil
+}
+
+func updateCyndiQueryLastUpload(systemsQuery *gorm.DB, refTime time.Time, lastNDays int) *gorm.DB {
+	if lastNDays >= 0 {
+		return systemsQuery.Where("updated > ?", refTime.AddDate(0, 0, -lastNDays))
+	}
+	return systemsQuery
+}
---
+++ vmaas_sync/metrics_cyndi_test.go
@@ -0,0 +1,88 @@
+package vmaas_sync //nolint:golint,stylecheck
+
+import (
+	"app/base/core"
+	"app/base/database"
+	"app/base/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestCyndiSystemsCounts(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	counts, err := getCyndiCounts(refTime())
+	assert.Nil(t, err)
+	assert.Equal(t, 16, counts[lastUploadLast1D])
+	assert.Equal(t, 16, counts[lastUploadAll])
+}
+
+func TestUploadedCyndiSystemsCounts1D(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	systemsQuery := database.Db.Table("inventory.hosts")
+	systemsQuery = updateCyndiQueryLastUpload(systemsQuery, refTime(), 1)
+	var nSystems int
+	assert.Nil(t, systemsQuery.Count(&nSystems).Error)
+	assert.Equal(t, 16, nSystems)
+}
+
+func TestUploadedCyndiSystemsCounts7D(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	systemsQuery := database.Db.Table("inventory.hosts")
+	systemsQuery = updateCyndiQueryLastUpload(systemsQuery, refTime(), 7)
+	var nSystems int
+	assert.Nil(t, systemsQuery.Count(&nSystems).Error)
+	assert.Equal(t, 16, nSystems)
+}
+
+func TestUploadedCyndiSystemsCounts30D(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	systemsQuery := database.Db.Table("inventory.hosts")
+	systemsQuery = updateCyndiQueryLastUpload(systemsQuery, refTime(), 30)
+	var nSystems int
+	assert.Nil(t, systemsQuery.Count(&nSystems).Error)
+	assert.Equal(t, 16, nSystems)
+}
+
+func TestUploadedCyndiSystemsCountsNoSystems(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	systemsQuery := database.Db.Table("inventory.hosts")
+	refTime := time.Date(2020, 9, 23, 10, 0, 0, 0, time.UTC)
+	systemsQuery = updateCyndiQueryLastUpload(systemsQuery, refTime, 30)
+	var nSystems int
+	assert.Nil(t, systemsQuery.Count(&nSystems).Error)
+	assert.Equal(t, 0, nSystems)
+}
+
+func TestUploadedCyndiSystemsCountsAllSystems(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	systemsQuery := database.Db.Table("inventory.hosts")
+	systemsQuery = updateCyndiQueryLastUpload(systemsQuery, refTime(), -1)
+	var nSystems int
+	assert.Nil(t, systemsQuery.Count(&nSystems).Error)
+	assert.Equal(t, 16, nSystems)
+}
+
+func TestCyndiTags(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	cyndiData, err := getCyndiData()
+	assert.Nil(t, err)
+	assert.Equal(t, 16, cyndiData.SystemsCount)
+	assert.Equal(t, 3, cyndiData.UniqueTags)
+	assert.Equal(t, 14, cyndiData.SapCount)
+	assert.Equal(t, 15, cyndiData.SystemsWithTags)
+}
