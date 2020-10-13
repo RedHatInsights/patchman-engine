78c4163f51e765d72f8ac7591d844ce7d5b1eba2
Josef Hak <jhak@redhat.com>
2020-10-13 16:04:06 +0200

    moved systems export endpoint to separate file

--- manager/controllers/systems.go
+++ manager/controllers/systems.go
@@ -3,11 +3,9 @@ package controllers
 import (
 	"app/base/database"
 	"app/manager/middlewares"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"net/http"
-	"strings"
 	"time"
 )
 
@@ -111,62 +109,6 @@ func SystemsListHandler(c *gin.Context) {
 		Meta:  *meta,
 	}
 	c.JSON(http.StatusOK, &resp)
-}
-
-// nolint: gocritic
-// @Summary Export systems for my account
-// @Description  Export systems for my account
-// @ID exportSystems
-// @Security RhIdentity
-// @Accept   json
-// @Produce  json,text/csv
-// @Param    filter[id]              query   string  false "Filter"
-// @Param    filter[display_name]    query   string  false "Filter"
-// @Param    filter[last_evaluation] query   string  false "Filter"
-// @Param    filter[last_upload]     query   string  false "Filter"
-// @Param    filter[rhsa_count]      query   string  false "Filter"
-// @Param    filter[rhba_count]      query   string  false "Filter"
-// @Param    filter[rhea_count]      query   string  false "Filter"
-// @Param    filter[enabled]         query   string  false "Filter"
-// @Param    filter[stale]           query   string  false "Filter"
-// @Success 200 {array} SystemInlineItem
-// @Router /api/patch/v1/export/systems [get]
-func SystemsExportHandler(c *gin.Context) {
-	account := c.GetInt(middlewares.KeyAccount)
-	query := querySystems(account)
-	query = ApplySearch(c, query, "system_platform.display_name")
-	query, _ = ApplyTagsFilter(c, query, "system_platform.inventory_id")
-
-	var systems []SystemDBLookup
-
-	query = query.Order("id")
-	query, err := ExportListCommon(query, c, SystemOpts)
-	if err != nil {
-		// Error handling and setting of result code & content is done in ListCommon
-		return
-	}
-
-	err = query.Find(&systems).Error
-	if err != nil {
-		LogAndRespError(c, err, "db error")
-		return
-	}
-
-	data := make([]SystemInlineItem, len(systems))
-
-	for i, v := range systems {
-		data[i] = SystemInlineItem(v)
-	}
-
-	accept := c.GetHeader("Accept")
-	if strings.Contains(accept, "application/json") {
-		c.JSON(http.StatusOK, data)
-	} else if strings.Contains(accept, "text/csv") {
-		Csv(c, 200, data)
-	} else {
-		LogWarnAndResp(c, http.StatusUnsupportedMediaType,
-			fmt.Sprintf("Invalid content type '%s', use 'application/json' or 'text/csv'", accept))
-	}
 }
 
 func querySystems(account int) *gorm.DB {
---
+++ manager/controllers/systems_export_test.go
@@ -0,0 +1,122 @@
+package controllers
+
+import (
+	"app/base/core"
+	"app/base/utils"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestSystemsExportJSON(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("Accept", "application/json")
+	core.InitRouterWithPath(SystemsExportHandler, "/").ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	var output []SystemInlineItem
+
+	ParseReponseBody(t, w.Body.Bytes(), &output)
+	assert.Equal(t, 8, len(output))
+	assert.Equal(t, "00000000-0000-0000-0000-000000000001", output[0].ID)
+}
+
+// nolint: lll
+func TestSystemsExportCSV(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("Accept", "text/csv")
+	core.InitRouter(SystemsExportHandler).ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	body := w.Body.String()
+	lines := strings.Split(body, "\n")
+
+	assert.Equal(t, 10, len(lines))
+	assert.Equal(t,
+		"id,display_name,last_evaluation,last_upload,rhsa_count,rhba_count,rhea_count,stale,"+
+			"packages_installed,packages_updatable",
+		lines[0])
+
+	assert.Equal(t, "00000000-0000-0000-0000-000000000001,00000000-0000-0000-0000-000000000001,2018-09-22T16:00:00Z,2020-09-22T16:00:00Z,2,3,3,false,0,0", lines[1])
+}
+
+func TestSystemsExportWrongFormat(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("Accept", "test-format")
+	core.InitRouter(SystemsExportHandler).ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnsupportedMediaType, w.Code)
+	body := w.Body.String()
+	exp := `{"error":"Invalid content type 'test-format', use 'application/json' or 'text/csv'"}`
+	assert.Equal(t, exp, body)
+}
+
+func TestSystemsExportCSVFilter(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/?filter[display_name]=nonexistant", nil)
+	req.Header.Add("Accept", "text/csv")
+	core.InitRouter(SystemsExportHandler).ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	body := w.Body.String()
+	lines := strings.Split(body, "\n")
+
+	assert.Equal(t, 2, len(lines))
+	assert.Equal(t,
+		"id,display_name,last_evaluation,last_upload,rhsa_count,rhba_count,rhea_count,stale,"+
+			"packages_installed,packages_updatable",
+		lines[0])
+	assert.Equal(t, "", lines[1])
+}
+
+func TestExportSystemsTags(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/?tags=ns1/k1=val1", nil)
+	req.Header.Add("Accept", "application/json")
+	core.InitRouterWithPath(SystemsExportHandler, "/").ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	var output []SystemInlineItem
+
+	ParseReponseBody(t, w.Body.Bytes(), &output)
+	assert.Equal(t, 8, len(output))
+	assert.Equal(t, "00000000-0000-0000-0000-000000000001", output[0].ID)
+}
+
+func TestSystemsExportWorkloads(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET",
+		"/?filter[system_profile][sap_system]=true&filter[system_profile][sap_sids][in][]=ABC", nil)
+	req.Header.Add("Accept", "application/json")
+	core.InitRouterWithPath(SystemsExportHandler, "/").ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	var output []SystemInlineItem
+
+	ParseReponseBody(t, w.Body.Bytes(), &output)
+	assert.Equal(t, 2, len(output))
+	assert.Equal(t, "00000000-0000-0000-0000-000000000001", output[0].ID)
+}
