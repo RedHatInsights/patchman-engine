811ff48aadc290935c8b7f6d5c2662fa6edd2932
Michal Hornicky <mhornick@redhat.com>
2020-10-15 09:03:07 +0200

    Remove duplicate params

--- manager/controllers/systems.go
+++ manager/controllers/systems.go
@@ -82,7 +82,6 @@ type SystemsResponse struct {
 // @Param    filter[packages_installed] query string false "Filter"
 // @Param    filter[packages_updatable] query string false "Filter"
 // @Param    tags                    query   []string  false "Tag filter"
-// @Param    tags                    query   []string  false "Tag filter"
 // @Param    filter[system_profile][sap_system]   query string   false "Filter only SAP systems"
 // @Param    filter[system_profile][sap_sids][in] query []string false "Filter systems by their SAP SIDs"
 // @Success 200 {object} SystemsResponse
