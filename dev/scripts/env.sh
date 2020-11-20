dda17d2dc5e00e35b8c06c9cf7d332c716ee5601
Josef Hak <jhak@redhat.com>
2020-11-20 15:29:27 +0100

    fixed setting up testing account

--- dev/scripts/env.sh
+++ dev/scripts/env.sh
@@ -1,5 +1,5 @@
 # This identity contains account_name = "0"
 
-ACCOUNT_NUMER=${1:-"1"}
+ACCOUNT_NUMER=${1:-"acc-1"}
 JSON='{"entitlements":{"smart_management":{"is_entitled":true}},"identity":{"account_number":"'$ACCOUNT_NUMER'","type":"User"}}'
 IDENTITY=$(echo "$JSON" | base64 -w 0 -)
