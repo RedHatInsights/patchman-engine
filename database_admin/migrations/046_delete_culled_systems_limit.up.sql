65f7e5df0f0eb6829981cd0d6908583cdaf59745
Josef Hak <jhak@redhat.com>
2020-11-27 17:37:22 +0100

    fixed "delte_culled_systems", and added "delted_limit" argument

---
+++ database_admin/migrations/046_delete_culled_systems_limit.up.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION delete_culled_systems(delete_limit INTEGER)
+    RETURNS INTEGER
+AS
+$fun$
+DECLARE
+    n_deleted INTEGER;
+BEGIN
+    SELECT count(*) INTO n_deleted FROM (
+        SELECT delete_system(inventory_id)
+        FROM system_platform sp
+        WHERE culled_timestamp < now()
+        ORDER BY id
+        LIMIT delete_limit) x;
+    RETURN n_deleted;
+END;
+$fun$ LANGUAGE plpgsql;
+
+DROP FUNCTION delete_culled_systems();
