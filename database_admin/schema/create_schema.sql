4ceedce05f028643d5843f2a19c1f476d5061662
Josef Hak <jhak@redhat.com>
2020-11-27 12:24:20 +0100

    Revert "added migration 46 to delete yupana systems"
    This reverts commit 5c9252430fe3564fe1bb4858d5d85af9c5c084af.

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (46, false);
+VALUES (45, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
