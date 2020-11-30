569525ee754d91a6104227e3cc5bb7e5b99d44e3
Michael Mraka <michael.mraka@redhat.com>
2020-11-30 13:05:52 +0100

    log slow statements in test/ci/qa
    fixing
    psql:./database_admin/config.sql:2: ERROR:  database "patchman" does not exist

--- database_admin/config.sql
+++ database_admin/config.sql
@@ -1,2 +1,13 @@
+-- In test/CI/QA environment
 -- Log statements which take more than 2s
-ALTER DATABASE patchman SET log_min_duration_statement = 2000;
+DO
+$$
+    DECLARE
+      dbname text;
+    BEGIN
+      SELECT current_database() into dbname;
+      IF dbname = 'patchman' THEN
+        EXECUTE 'ALTER DATABASE ' || dbname || ' SET log_min_duration_statement = 2000;';
+      END IF;
+    END
+$$ LANGUAGE plpgsql;
