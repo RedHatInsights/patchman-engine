88b767d49d228aa216af4cd40421ee968dd45ff1
Michal Hornicky <mhornick@redhat.com>
2020-12-04 09:06:21 +0100

    Add missing params to system_packages

--- manager/controllers/system_packages.go
+++ manager/controllers/system_packages.go
@@ -69,6 +69,8 @@ func systemPackageQuery(account int, inventoryID string) *gorm.DB {
 // @Accept   json
 // @Produce  json
 // @Param    inventory_id    path    string   true "Inventory ID"
+// @Param    limit          query   int     false   "Limit for paging, set -1 to return all"
+// @Param    offset         query   int     false   "Offset for paging"
 // @Param    search          query   string  false   "Find matching text"
 // @Param    filter[name]            query   string  false "Filter"
 // @Param    filter[description]     query   string  false "Filter"
