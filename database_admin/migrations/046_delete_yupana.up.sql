570ec038b6ed430284f9fd70af5424f4a69c0d04
Michal Hornicky <semtexzv@gmail.com>
2021-01-12 11:46:37 +0100

    Manually delete yupana systems

---
+++ database_admin/migrations/046_delete_yupana.up.sql
@@ -0,0 +1,18 @@
+DO
+$$
+    DECLARE
+        ids     int[];
+        deleted int = 1;
+    BEGIN
+        WHILE deleted > 0
+            LOOP
+                ids := ARRAY(SELECT id
+                             FROM system_platform
+                             WHERE reporter_id = (select id from reporter where name = 'yupana')
+                             LIMIT 5000
+                    );
+                deleted := delete_systems(ids);
+                RAISE NOTICE 'Deleted yupana systems: %', text(deleted);
+            END LOOP;
+    END;
+$$ LANGUAGE plpgsql;
