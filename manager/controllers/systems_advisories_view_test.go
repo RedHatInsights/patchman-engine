4c547c7bf6d54d28d81aa7b1dae136be61a948f6
AlesKas <akaspare@redhat.com>
2021-01-27 11:16:08 +0100

    TestAdvisoriesSystemsViewRaw

--- manager/controllers/systems_advisories_view_test.go
+++ manager/controllers/systems_advisories_view_test.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"app/base/core"
+	"app/base/database"
 	"app/base/utils"
 	"bytes"
 	"encoding/json"
@@ -31,6 +32,29 @@ func doTestView(t *testing.T, handler gin.HandlerFunc, checker func(w *httptest.
 	core.InitRouterWithParams(handler, 1, "POST", "/").
 		ServeHTTP(w, req)
 	checker(w)
+}
+
+//nolint: gofmt
+func TestAdvisoriesSystemsViewRaw(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	var data []systemsAdvisoriesDBLoad
+	query := database.Db.Raw("SELECT sp.inventory_id as system_id, am.name as advisory_id" +
+	" FROM system_advisories sa join system_platform sp on sp.rh_account_id = 1 and sp.id = sa.system_id" +
+	" join advisory_metadata am on am.id = sa.advisory_id JOIN inventory.hosts ih ON ih.id = sp.inventory_id" +
+	" WHERE sp.rh_account_id = 1 AND (sp.inventory_id in" +
+	" ('00000000-0000-0000-0000-000000000001','00000000-0000-0000-0000-000000000002'))" +
+	" AND am.name in ('RH-1','RH-2') ORDER BY sp.inventory_id, am.id;")
+	err := query.Find(&data).Error;
+
+	assert.NoError(t, err)
+	assert.Equal(t, data[0].SystemID, SystemID("00000000-0000-0000-0000-000000000001"))
+	assert.Equal(t, data[0].AdvisoryID, AdvisoryName("RH-1"))
+	assert.Equal(t, data[1].SystemID, SystemID("00000000-0000-0000-0000-000000000001"))
+	assert.Equal(t, data[1].AdvisoryID, AdvisoryName("RH-2"))
+	assert.Equal(t, data[2].SystemID, SystemID("00000000-0000-0000-0000-000000000002"))
+	assert.Equal(t, data[2].AdvisoryID, AdvisoryName("RH-1"))
 }
 
 func TestSystemsAdvisoriesView(t *testing.T) {
