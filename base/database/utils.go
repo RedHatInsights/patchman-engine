a05cb01225136caa0c8fe8f96b24802524fea965
Michael Mraka <michael.mraka@redhat.com>
2021-01-29 10:55:52 +0100

    SPM-631: performance improvements

--- base/database/utils.go
+++ base/database/utils.go
@@ -41,7 +41,6 @@ func SystemAdvisoriesBySystemID(tx *gorm.DB, accountID, systemID int) *gorm.DB {
 
 func systemAdvisoriesQuery(tx *gorm.DB, accountID int) *gorm.DB {
 	query := tx.Table("system_advisories sa").Select("sa.*").
-		Joins("join advisory_metadata am ON am.id=sa.advisory_id").
 		Joins("join system_platform sp ON sa.rh_account_id = sp.rh_account_id AND sa.system_id = sp.id").
 		Where("sa.rh_account_id = ? AND sp.rh_account_id = ?", accountID, accountID)
 	return query
