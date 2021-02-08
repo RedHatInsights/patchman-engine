828484e30f5c06dc0e52ce824900420a2a811f21
Josef Hak <jhak@redhat.com>
2021-02-08 15:18:27 +0100

    fixed waiting for finished migration in "wait-for-services.sh"

--- scripts/feed_db.sh
+++ scripts/feed_db.sh
@@ -3,8 +3,6 @@
 # wait untill database is ready
 ./scripts/wait-for-services.sh
 
-sleep 1 # wait to ensure all migrations to be done
-
 # fill database with testing data
 PGPASSWORD=$DB_PASSWD psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
                        -v ON_ERROR_STOP=1 \
--- scripts/wait-for-services.sh
+++ scripts/wait-for-services.sh
@@ -9,7 +9,8 @@ if [ ! -z "$DB_HOST" ]; then
   if [ ! -z "$WAIT_FOR_EMPTY_DB" ]; then
     CHECK_QUERY="\q" # Wait only for empty database.
   else
-    CHECK_QUERY="SELECT * FROM schema_migrations;" # Wait even for database schema initialization.
+    # Wait even for database schema initialization.
+    CHECK_QUERY="SELECT 1/count(*) FROM schema_migrations WHERE dirty='f';"
   fi
   until PGPASSWORD="$DB_PASSWD" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "${CHECK_QUERY}" -q 2>/dev/null; do
     >&2 echo "PostgreSQL is unavailable - sleeping"
