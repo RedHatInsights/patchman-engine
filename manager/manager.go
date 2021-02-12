f47ed69288f3210c25616d89f09fcdfb7a1b424b
Josef Hak <jhak@redhat.com>
2021-02-12 11:56:35 +0100

    bump version

--- manager/manager.go
+++ manager/manager.go
@@ -12,7 +12,7 @@ import (
 
 // @title Patchman-engine API
 // DO NOT EDIT version MANUALLY - this variable is modified by generate_docs.sh
-// @version  v1.7.0
+// @version  v1.7.3
 // @description Description here
 
 // @license.name GPLv3
