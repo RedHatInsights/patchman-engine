57c817ab5967b6e62fa7748791dc7279001c5689
Josef Hak <jhak@redhat.com>
2021-02-17 15:47:46 +0100

    add Haberdasher logging command wrapper support

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ COMPONENT=$1
 # This script is launched inside the /go/src/app working directory
 if [[ -n $GORUN ]]; then
   # Running using 'go run'
-  exec ./scripts/wait-for-services.sh go run main.go $COMPONENT
+  exec $CMD_WRAPPER ./scripts/wait-for-services.sh go run main.go $COMPONENT
 else
-  exec ./scripts/wait-for-services.sh ./main $COMPONENT
+  exec $CMD_WRAPPER ./scripts/wait-for-services.sh ./main $COMPONENT
 fi
