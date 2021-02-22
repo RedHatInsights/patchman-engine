e6719c3e5fbd6a4c453d44483a0f2686d0363f06
Josef Hak <jhak@redhat.com>
2021-02-22 17:50:08 +0100

    extended system_package testing data

--- manager/controllers/system_packages_test.go
+++ manager/controllers/system_packages_test.go
@@ -14,16 +14,22 @@ func TestSystemPackages(t *testing.T) {
 	core.SetupTestEnvironment()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/00000000-0000-0000-0000-000000000012/packages", nil)
+	req, _ := http.NewRequest("GET", "/00000000-0000-0000-0000-000000000013/packages", nil)
 	core.InitRouterWithParams(SystemPackagesHandler, 3, "GET", "/:inventory_id/packages").
 		ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
 	var output SystemPackageResponse
 	ParseReponseBody(t, w.Body.Bytes(), &output)
-	assert.Len(t, output.Data, 2)
-	assert.Equal(t, output.Data[0].Name, "firefox")
-	assert.Equal(t, output.Data[1].Name, "kernel")
+	assert.Len(t, output.Data, 4)
+	assert.Equal(t, output.Data[0].Name, "bash")
+	assert.Equal(t, len(output.Data[0].Updates), 0)
+	assert.Equal(t, output.Data[1].Name, "curl")
+	assert.Equal(t, len(output.Data[1].Updates), 0)
+	assert.Equal(t, output.Data[2].Name, "firefox")
+	assert.Equal(t, len(output.Data[2].Updates), 2)
+	assert.Equal(t, output.Data[3].Name, "kernel")
+	assert.Equal(t, len(output.Data[3].Updates), 0)
 }
 
 func TestPackagesSearch(t *testing.T) {
@@ -60,16 +66,15 @@ func TestSystemPackagesUpdatableOnly(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET",
-		"/00000000-0000-0000-0000-000000000012/packages?filter[updatable]=true", nil)
+		"/00000000-0000-0000-0000-000000000013/packages?filter[updatable]=true", nil)
 	core.InitRouterWithParams(SystemPackagesHandler, 3, "GET", "/:inventory_id/packages").
 		ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
 	var output SystemPackageResponse
 	ParseReponseBody(t, w.Body.Bytes(), &output)
-	assert.Len(t, output.Data, 2)
+	assert.Len(t, output.Data, 1)
 	assert.Equal(t, output.Data[0].Name, "firefox")
-	assert.Equal(t, output.Data[1].Name, "kernel")
 }
 
 func TestSystemPackagesWrongOffset(t *testing.T) {
