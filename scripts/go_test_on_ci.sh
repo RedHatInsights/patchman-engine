b7a8272c744e431a891ab55a3b9ebf4ae2416c64
Josef Hak <jhak@redhat.com>
2021-03-11 17:34:39 +0100

    add codecov analysis to github workflow, removed Travis

--- scripts/go_test_on_ci.sh
+++ scripts/go_test_on_ci.sh
@@ -14,7 +14,3 @@ echo "Go code analysed successfully."
 
 # Run project tests
 ./scripts/go_test_db.sh | ./scripts/colorize.sh
-
-if [ -n "$TRAVIS" ]; then
-  bash <(curl -s https://codecov.io/bash)
-fi
