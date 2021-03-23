a995c6fa8697c522503739c70cace0c10e20ea28
mkholjur <mkholjur@redhat.com>
2021-03-23 10:21:23 +0100

    SPM-782: add additional info to systems endpoint

--- manager/controllers/systems_export_test.go
+++ manager/controllers/systems_export_test.go
@@ -33,6 +33,10 @@ func TestSystemsExportJSON(t *testing.T) {
 	assert.Equal(t, "8", output[0].SystemItemAttributes.OSMajor)
 	assert.Equal(t, "1", output[0].SystemItemAttributes.OSMinor)
 	assert.Equal(t, "8.1", output[0].SystemItemAttributes.Rhsm)
+	assert.Equal(t, "2018-08-26 16:00:00 +0000 UTC", output[0].SystemItemAttributes.StaleTimestamp.String())
+	assert.Equal(t, "2018-09-02 16:00:00 +0000 UTC", output[0].SystemItemAttributes.StaleWarningTimestamp.String())
+	assert.Equal(t, "2018-09-09 16:00:00 +0000 UTC", output[0].SystemItemAttributes.CulledTimestamp.String())
+	assert.Equal(t, "2018-08-26 16:00:00 +0000 UTC", output[0].SystemItemAttributes.Created.String())
 }
 
 // nolint: lll
@@ -52,11 +56,13 @@ func TestSystemsExportCSV(t *testing.T) {
 	assert.Equal(t, 10, len(lines))
 	assert.Equal(t,
 		"id,display_name,last_evaluation,last_upload,rhsa_count,rhba_count,rhea_count,stale,"+
-			"third_party,packages_installed,packages_updatable,os_name,os_major,os_minor,rhsm",
+			"third_party,packages_installed,packages_updatable,os_name,os_major,os_minor,rhsm,"+
+			"stale_timestamp,stale_warning_timestamp,culled_timestamp,created",
 		lines[0])
 
 	assert.Equal(t, "00000000-0000-0000-0000-000000000001,00000000-0000-0000-0000-000000000001,"+
-		"2018-09-22T16:00:00Z,2020-09-22T16:00:00Z,2,3,3,false,true,0,0,RHEL,8,1,8.1", lines[1])
+		"2018-09-22T16:00:00Z,2020-09-22T16:00:00Z,2,3,3,false,true,0,0,RHEL,8,1,8.1,"+
+		"2018-08-26T16:00:00Z,2018-09-02T16:00:00Z,2018-09-09T16:00:00Z,2018-08-26T16:00:00Z", lines[1])
 }
 
 func TestSystemsExportWrongFormat(t *testing.T) {
@@ -90,7 +96,8 @@ func TestSystemsExportCSVFilter(t *testing.T) {
 	assert.Equal(t, 2, len(lines))
 	assert.Equal(t,
 		"id,display_name,last_evaluation,last_upload,rhsa_count,rhba_count,rhea_count,stale,"+
-			"third_party,packages_installed,packages_updatable,os_name,os_major,os_minor,rhsm",
+			"third_party,packages_installed,packages_updatable,os_name,os_major,os_minor,rhsm,"+
+			"stale_timestamp,stale_warning_timestamp,culled_timestamp,created",
 		lines[0])
 	assert.Equal(t, "", lines[1])
 }
