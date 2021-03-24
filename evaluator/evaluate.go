5fea36a580cf8cd2cdd085507112e8cebe37a79f
Michael Mraka <michael.mraka@redhat.com>
2021-03-24 11:10:35 +0100

    fixup! SPM-736: mark system with thirdparty repo as thirdparty system

--- evaluator/evaluate.go
+++ evaluator/evaluate.go
@@ -216,20 +216,19 @@ func analyzeRepos(tx *gorm.DB, system *models.SystemPlatform) (
 
 	// if system has associated at least one third party repo
 	// it's marked as third party system
-	var thirdPartyCount int
 	err = tx.Table("system_repo sr").
 		Joins("join repo r on r.id = sr.repo_id").
 		Where("sr.rh_account_id = ?", system.RhAccountID).
 		Where("sr.system_id = ?", system.ID).
 		Where("r.third_party = true").
-		Count(&thirdPartyCount).Error
+		Count(&thirdParty).Error
 	if err != nil {
 		utils.Log("err", err.Error(), "accountID", system.RhAccountID, "systemID", system.ID).
 			Warn("counting third party repos")
 		return false, err
 	}
 
-	return (thirdPartyCount > 0), nil
+	return thirdParty, nil
 }
 
 func deleteOldSystemPackages(tx *gorm.DB, accountID, systemID int, packagesByNEVRA map[utils.Nevra]namedPackage) error {
