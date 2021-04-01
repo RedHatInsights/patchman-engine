3423eae67753b5c94626046bee3528540b361d7b
Josef Hak <jhak@redhat.com>
2021-04-01 10:54:11 +0200

    bump version 1.8.0

--- base/metrics/metrics.go
+++ base/metrics/metrics.go
@@ -22,7 +22,7 @@ var (
 	}, []string{"version"})
 
 	// ENGINEVERSION - DO NOT EDIT this variable MANUALLY - it is modified by generate_docs.sh
-	ENGINEVERSION = "v1.7.9"
+	ENGINEVERSION = "v1.8.0"
 )
 
 func init() {
