ae0fb7cccd13a96ac2635a3847cd94b6b2629db9
Michael Mraka <michael.mraka@redhat.com>
2021-04-08 09:03:47 +0200

    keep git repo tidy from temporary files

--- scripts/check-openapi-docs.sh
+++ scripts/check-openapi-docs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cp docs/openapi.json docs/openapi1.json
+OPENAPI_COPY=$(mktemp -t openapi.json.XXX)
+cp docs/openapi.json $OPENAPI_COPY
 ./scripts/generate_docs.sh --keep
-diff docs/openapi.json docs/openapi1.json
+diff docs/openapi.json $OPENAPI_COPY
 rc=$?
 if [ $rc -gt 0 ]; then
   echo "docs/openapi.json different from file generated with './scripts/generate_docs.sh'!"
@@ -10,4 +11,5 @@ else
   echo "docs/openapi.json consistent with generated file."
 fi
 
+rm $OPENAPI_COPY
 exit $rc
