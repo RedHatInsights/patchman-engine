62b86754a115fd6b2983eda5ac265d95b4d24973
Josef Hak <jhak@redhat.com>
2021-04-16 15:40:07 +0200

    SPM-728 Remove randomly failing test "TestRunReaders"

--- listener/listener_test.go
+++ listener/listener_test.go
@@ -1,23 +1,10 @@
 package listener
 
 import (
-	"app/base/mqueue"
 	"app/base/utils"
 	"github.com/stretchr/testify/assert"
-	"sync"
 	"testing"
 )
-
-func TestRunReaders(t *testing.T) {
-	nReaders := 0
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	runReaders(&wg, mqueue.CreateCountedMockReader(&nReaders))
-	nReadersExpected := 8
-	utils.AssertEqualWait(t, 10, func() (exp, act interface{}) {
-		return nReadersExpected, nReaders
-	})
-}
 
 func TestLoadValidReporters(t *testing.T) {
 	utils.SkipWithoutDB(t)
