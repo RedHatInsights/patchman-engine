4ae654b3aaab6bc3ebfe05c4b440bb244839c67a
Josef Hak <jhak@redhat.com>
2021-04-23 15:11:53 +0200

    SPM-853 updated vmaas_sync tests

---
+++ vmaas_sync/package_sync_test.go
@@ -0,0 +1,20 @@
+package vmaas_sync //nolint:golint,stylecheck
+import (
+	"app/base/core"
+	"app/base/database"
+	"app/base/utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSyncPackages(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+	configure()
+
+	err := syncPackages()
+	assert.NoError(t, err)
+
+	database.CheckPackagesNamesInDB(t, "bash", "curl")
+	database.DeleteNewlyAddedPackages(t)
+}
