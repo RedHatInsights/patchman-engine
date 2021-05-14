183203a084e92112e8768e52ecae2ef453a49b1d
Michael Mraka <michael.mraka@redhat.com>
2021-05-14 12:27:16 +0200

    don't try to remove system uuid_* functions

--- database_admin/schema/clear_db.sql
+++ database_admin/schema/clear_db.sql
@@ -8,6 +8,7 @@ $$
                      FROM pg_proc
                               INNER JOIN pg_namespace ns ON (pg_proc.pronamespace = ns.oid)
                      WHERE ns.nspname = 'public'
+                       AND proname not like 'uuid_%'
                      ORDER BY proname)
             LOOP
                 EXECUTE stmt;
