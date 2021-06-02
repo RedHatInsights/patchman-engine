ac149e46cf78a359a586b2547ca6dd5c1679f963
Nikhil Dhandre <ndhandre@redhat.com>
2021-06-02 17:17:14 +0530

    SPM-955: collecting smoke test with CJI

--- pr_check.sh
+++ pr_check.sh
@@ -3,24 +3,26 @@
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="patchman"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="patchman"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/patchman-engine-app"
-DOCKERFILE="Dockerfile.rhel8"
-COMPONENTS_W_RESOURCES="vmaas"
+export APP_NAME="patchman"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="patchman"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/patchman-engine-app"
+export DOCKERFILE="Dockerfile.rhel8"
+export COMPONENTS_W_RESOURCES="vmaas"
 
-IQE_PLUGINS="patchman"
-IQE_MARKER_EXPRESSION=""
-IQE_FILTER_EXPRESSION=""
+export IQE_PLUGINS="patchman"
+export IQE_MARKER_EXPRESSION="patch_smoke"
+export IQE_FILTER_EXPRESSION=""
+export IQE_CJI_TIMEOUT="30m"
+
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
-source $CICD_ROOT/deploy_ephemeral_env.sh
-# source $CICD_ROOT/smoke_test.sh # TODO add working smoke tests
 
-# create empty test results as a workaround for disabled tests
-mkdir -p $WORKSPACE/artifacts
-echo '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="1" time="1.0" timestamp="2020-11-19T17:23:32.254980" hostname="localhost.localdomain"><testcase classname="patch.dummy" name="test_deploy" time="1.0"><properties><property name="polarion-testcase-id" value="test_deploy" /></properties></testcase></testsuite></testsuites>' > $WORKSPACE/artifacts/junit-patchman-sequential.xml
+# Deploy patchman to an ephemeral namespace for testing
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Run iqe-patchman somke tests with ClowdJobInvocation
+source $CICD_ROOT/cji_smoke_test.sh
