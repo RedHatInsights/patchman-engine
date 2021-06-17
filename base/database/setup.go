aa4f5ddc8e62b710f440570c44a506a054f0ffb6
Michael Mraka <michael.mraka@redhat.com>
2021-06-17 15:43:02 +0200

    SPM-984: use postgresql sslrootcert if available

--- base/database/setup.go
+++ base/database/setup.go
@@ -3,11 +3,12 @@ package database
 import (
 	"app/base/utils"
 	"fmt"
+	"strconv"
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"strconv"
-	"time"
 )
 
 var (
@@ -23,13 +24,14 @@ func Configure() {
 
 // PostgreSQLConfig PostgreSQL database config
 type PostgreSQLConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Database string
-	Passwd   string
-	SSLMode  string
-	Debug    bool
+	Host        string
+	Port        int
+	User        string
+	Database    string
+	Passwd      string
+	SSLMode     string
+	SSLRootCert string
+	Debug       bool
 
 	// Additional params.
 	StatementTimeoutMs     int // https://www.postgresql.org/docs/10/runtime-config-client.html
@@ -89,13 +91,14 @@ func loadEnvPostgreSQLConfig() *PostgreSQLConfig {
 	}
 
 	config := PostgreSQLConfig{
-		User:     utils.Getenv("DB_USER", "FILL"),
-		Host:     utils.Getenv("DB_HOST", "FILL"),
-		Port:     port,
-		Database: utils.Getenv("DB_NAME", "FILL"),
-		Passwd:   utils.Getenv("DB_PASSWD", "FILL"),
-		SSLMode:  utils.Getenv("DB_SSLMODE", "FILL"),
-		Debug:    utils.GetBoolEnvOrDefault("DB_DEBUG", false),
+		User:        utils.Getenv("DB_USER", "FILL"),
+		Host:        utils.Getenv("DB_HOST", "FILL"),
+		Port:        port,
+		Database:    utils.Getenv("DB_NAME", "FILL"),
+		Passwd:      utils.Getenv("DB_PASSWD", "FILL"),
+		SSLMode:     utils.Getenv("DB_SSLMODE", "FILL"),
+		SSLRootCert: utils.Getenv("DB_SSLROOTCERT", ""),
+		Debug:       utils.GetBoolEnvOrDefault("DB_DEBUG", false),
 
 		StatementTimeoutMs:     utils.GetIntEnvOrDefault("DB_STATEMENT_TIMEOUT_MS", 0),
 		MaxConnections:         utils.GetIntEnvOrDefault("DB_MAX_CONNECTIONS", 250),
@@ -107,7 +110,11 @@ func loadEnvPostgreSQLConfig() *PostgreSQLConfig {
 
 // create "data source" config string needed for database connection opening
 func dataSourceName(dbConfig *PostgreSQLConfig) string {
-	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s statement_timeout=%d",
+	dbsource := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s statement_timeout=%d",
 		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Database, dbConfig.Passwd, dbConfig.SSLMode,
 		dbConfig.StatementTimeoutMs)
+	if dbConfig.SSLRootCert != "" {
+		dbsource = fmt.Sprintf("%s sslrootcert=%s", dbsource, dbConfig.SSLRootCert)
+	}
+	return dbsource
 }
--- database_admin/migrate.sh
+++ database_admin/migrate.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 MIGRATION_FILES=file://./database_admin/migrations
-DATABASE_URL=postgres://$DB_HOST/$DB_NAME?sslmode=$DB_SSLMODE
+DATABASE_URL="postgres://$DB_HOST/$DB_NAME?sslmode=$DB_SSLMODE${DB_SSLROOTCERT:+&sslrootcert=$DB_SSLROOTCERT}"
 
 if [[ -n $GORUN ]]; then
   go run main.go migrate $MIGRATION_FILES $DATABASE_URL
