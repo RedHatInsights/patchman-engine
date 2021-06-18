d568d3c1a6dc8bfee967e8960dd9f2e68ae41edf
Josef Hak <jhak@redhat.com>
2021-06-18 20:05:57 +0200

    SPM-989: Added "package_systems_export" and test

---
+++ manager/controllers/package_systems_export.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"app/base/utils"
+	"app/manager/middlewares"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+// @Summary Show me all my systems which have a package installed
+// @Description  Show me all my systems which have a package installed
+// @ID exportPackageSystems
+// @Security RhIdentity
+// @Accept   json
+// @Produce  json
+// @Param    limit          query   int     false   "Limit for paging, set -1 to return all"
+// @Param    offset         query   int     false   "Offset for paging"
+// @Param    package_name    path    string    true  "Package name"
+// @Param    tags            query   []string  false "Tag filter"
+// @Param    filter[system_profile][sap_system]   query  string  false "Filter only SAP systems"
+// @Param    filter[system_profile][sap_sids][in] query []string  false "Filter systems by their SAP SIDs"
+// @Success 200 {array} PackageSystemItem
+// @Router /api/patch/v1/export/packages/{package_name}/systems [get]
+func PackageSystemsExportHandler(c *gin.Context) {
+	account := c.GetInt(middlewares.KeyAccount)
+
+	packageName := c.Param("package_name")
+	if packageName == "" {
+		c.JSON(http.StatusBadRequest, utils.ErrorResponse{Error: "package_name param not found"})
+		return
+	}
+
+	var packageIDs []int
+	if err := packagesByNameQuery(packageName).Pluck("p.id", &packageIDs).Error; err != nil {
+		LogAndRespError(c, err, "database error")
+		return
+	}
+
+	query := packageSystemsQuery(account, packageIDs)
+	query, _, err := ApplyTagsFilter(c, query, "sp.inventory_id")
+	if err != nil {
+		return
+	} // Error handled in method itself
+	query, err = ExportListCommon(query, c, PackageSystemsOpts)
+	if err != nil {
+		return
+	} // Error handled in method itself
+
+	var systems []PackageSystemItem
+	err = query.Find(&systems).Error
+	if err != nil {
+		LogAndRespError(c, err, "database error")
+		return
+	}
+
+	accept := c.GetHeader("Accept")
+	if strings.Contains(accept, "application/json") { // nolint: gocritic
+		c.JSON(http.StatusOK, systems)
+	} else if strings.Contains(accept, "text/csv") {
+		Csv(c, 200, systems)
+	} else {
+		LogWarnAndResp(c, http.StatusUnsupportedMediaType,
+			fmt.Sprintf("Invalid content type '%s', use 'application/json' or 'text/csv'", accept))
+	}
+}
---
+++ manager/controllers/package_systems_export_test.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"app/base/core"
+	"app/base/utils"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestPackageSystemsExportHandlerJSON(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/kernel/systems?sort=id", nil)
+	req.Header.Add("Accept", "application/json")
+	core.InitRouterWithParams(PackageSystemsExportHandler, 3, "GET", "/:package_name/systems").
+		ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	var output []PackageSystemItem
+	ParseReponseBody(t, w.Body.Bytes(), &output)
+	assert.Equal(t, 2, len(output))
+	assert.Equal(t, output[0].ID, "00000000-0000-0000-0000-000000000012")
+	assert.Equal(t, output[0].InstalledEVRA, "5.6.13-200.fc31.x86_64")
+	assert.Equal(t, output[0].AvailableEVRA, "5.10.13-200.fc31.x86_64")
+}
+
+func TestPackageSystemsExportHandlerCSV(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	core.SetupTestEnvironment()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/kernel/systems?sort=id", nil)
+	req.Header.Add("Accept", "text/csv")
+	core.InitRouterWithParams(PackageSystemsExportHandler, 3, "GET", "/:package_name/systems").
+		ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	body := w.Body.String()
+	lines := strings.Split(body, "\n")
+
+	assert.Equal(t, 4, len(lines))
+	assert.Equal(t, "id,installed_evra,available_evra,updatable", lines[0])
+	assert.Equal(t, "00000000-0000-0000-0000-000000000012,5.6.13-200.fc31.x86_64,5.10.13-200.fc31.x86_64,true",
+		lines[1])
+	assert.Equal(t, "00000000-0000-0000-0000-000000000013,5.6.13-200.fc31.x86_64,,false", lines[2])
+}
