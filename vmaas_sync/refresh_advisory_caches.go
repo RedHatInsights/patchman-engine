9d4ce27a6f334b3c5ce134238ad4630bdc42fab0
Josef Hak <jhak@redhat.com>
2021-06-21 21:07:46 +0200

    SPM-831: Added advisory caches refresh job to vmaas-sync
    - ADVISORY_CACHES_WAIT_SECONDS
    - ENABLE_REFRESH_ADVISORY_CACHES

---
+++ vmaas_sync/refresh_advisory_caches.go
@@ -0,0 +1,41 @@
+package vmaas_sync //nolint:golint,stylecheck
+
+import (
+	"app/base/database"
+	"app/base/utils"
+	"gorm.io/gorm"
+	"time"
+)
+
+func refreshAdvisoryCaches() {
+	if !enableRefreshAdvisoryCaches {
+		return
+	}
+
+	nSecondsWait := utils.GetIntEnvOrDefault("ADVISORY_CACHES_WAIT_SECONDS", 60*15) // set 15 min
+	for {
+		refreshAdvisoryCachesPerAccounts(nSecondsWait)
+	}
+}
+
+func refreshAdvisoryCachesPerAccounts(nSecondsWait int) {
+	var rhAccountIDs []int
+	err := database.Db.Table("rh_account").Pluck("id", &rhAccountIDs).Error
+	if err != nil {
+		utils.Log("err", err.Error()).Error("Unable to load rh_account table ids to refresh caches")
+		return
+	}
+
+	for _, rhAccountID := range rhAccountIDs {
+		time.Sleep(time.Second * time.Duration(nSecondsWait))
+		err = withTx(func(tx *gorm.DB) error {
+			return tx.Exec("select refresh_advisory_caches(NULL, ?)", rhAccountID).Error
+		})
+		if err != nil {
+			utils.Log("err", err.Error(), "rh_account_id", rhAccountID).
+				Error("Refreshed account advisory caches")
+		} else {
+			utils.Log("rh_account_id", rhAccountID).Info("Refreshed account advisory caches")
+		}
+	}
+}
