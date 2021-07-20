e82fddee8ced65c77f4ed8b412ce38556aeef9f0
Josef Hak <jhak@redhat.com>
2021-07-20 16:05:57 +0200

    SPM-1034: Use epoch 0 for package analysis test

--- evaluator/evaluate_packages_test.go
+++ evaluator/evaluate_packages_test.go
@@ -22,12 +22,12 @@ func TestAnalyzePackages(t *testing.T) {
 	database.CheckSystemPackages(t, system.ID, 0)
 	database.CheckEVRAsInDB(t, 0, "12.0.1-1.fc31.x86_64") // lazy added package
 	vmaasData := vmaas.UpdatesV2Response{UpdateList: &map[string]vmaas.UpdatesV2ResponseUpdateList{
-		"kernel-5.6.13-200.fc31.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{}},
-		"firefox-12.0.1-1.fc31.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{{
+		"kernel-0:5.6.13-200.fc31.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{}},
+		"firefox-0:12.0.1-1.fc31.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{{
 			Package: utils.PtrString("firefox-77.0.1-1.fc31.x86_64"),
 		}}},
 		// this custom-package will be ignored
-		"custom-package-1.2.3-1.fc33.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{{}}}}}
+		"custom-package-0:1.2.3-1.fc33.x86_64": {AvailableUpdates: &[]vmaas.UpdatesV2ResponseAvailableUpdates{{}}}}}
 
 	installed, updatable, err := analyzePackages(database.Db, &system, &vmaasData)
 	assert.Nil(t, err)
