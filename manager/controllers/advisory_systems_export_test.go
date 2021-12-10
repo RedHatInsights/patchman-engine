c0245878138597fdf0ddcb39fd01d3cdd5c015eb
Josef Hak <jhak@redhat.com>
2021-12-10 11:16:18 +0100

    SPM-1259: Update tests after hiding baseline API changes

--- manager/controllers/advisory_systems_export_test.go
+++ manager/controllers/advisory_systems_export_test.go
@@ -43,11 +43,11 @@ func TestAdvisorySystemsExportCSV(t *testing.T) {
 	assert.Equal(t,
 		"id,display_name,last_evaluation,last_upload,rhsa_count,rhba_count,rhea_count,other_count,stale,third_party,"+
 			"insights_id,packages_installed,packages_updatable,os_name,os_major,os_minor,os,rhsm,stale_timestamp,"+
-			"stale_warning_timestamp,culled_timestamp,created,tags,baseline_name,baseline_uptodate", lines[0])
+			"stale_warning_timestamp,culled_timestamp,created,tags", lines[0]) // TODO: ,baseline_name,baseline_uptodate
 
 	assert.Equal(t, "00000000-0000-0000-0000-000000000001,00000000-0000-0000-0000-000000000001,"+
 		"2018-09-22T16:00:00Z,2020-09-22T16:00:00Z,2,3,3,0,false,true,00000000-0000-0000-0001-000000000001,0,0,"+
 		"RHEL,8,10,RHEL 8.10,8.10,2018-08-26T16:00:00Z,2018-09-02T16:00:00Z,2018-09-09T16:00:00Z,2018-08-26T16:00:00Z,"+
-		"\"[{'key':'k1','namespace':'ns1','value':'val1'},{'key':'k2','namespace':'ns1','value':'val2'}]\","+
-		"baseline_1-1,true", lines[1])
+		"\"[{'key':'k1','namespace':'ns1','value':'val1'},{'key':'k2','namespace':'ns1','value':'val2'}]\"",
+		lines[1]) // TODO: ,baseline_1-1,true
 }
