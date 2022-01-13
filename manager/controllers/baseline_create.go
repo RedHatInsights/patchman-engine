68aa90691dc8011fd63422de20955dde46fafa15
Josef Hak <jhak@redhat.com>
2022-01-13 15:29:41 +0100

    SPM-1261: Updated baseline create endpoint

--- manager/controllers/baseline_create.go
+++ manager/controllers/baseline_create.go
@@ -6,27 +6,21 @@ import (
 	"app/base/models"
 	"app/manager/middlewares"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 )
 
-type CreateBaselineRequst struct {
-	Name         string     `json:"name"`
-	InventoryIDs []SystemID `json:"inventoryIDs"`
-	ToTime       string     `json:"toTime"`
+type CreateBaselineRequest struct {
+	Name         *string         `json:"name"`
+	InventoryIDs []string        `json:"inventory_ids"`
+	Config       *BaselineConfig `json:"config"`
 }
 
-type BaselineConfig struct {
-	ToTime string `json:"to_time"`
-}
-
-type AccountID struct {
-	RhAccountID int
-}
-type CreateBaselineResponse struct {
-	BaselineID int
-}
+type CreateBaselineResponse UpdateBaselineResponse
 
 // @Summary Create a baseline for my set of systems
 // @Description Create a baseline for my set of systems
@@ -34,35 +28,37 @@ type CreateBaselineResponse struct {
 // @Security RhIdentity
 // @Accept   json
 // @Produce  json
-// @Param    body    body    CreateBaselineRequst true "Request body"
+// @Param    body    body    CreateBaselineRequest true "Request body"
 // @Success 200 {object} CreateBaselineResponse
 // @Router /api/patch/v1/baselines [put]
 func CreateBaselineHandler(c *gin.Context) {
-	account := AccountID{
-		RhAccountID: c.GetInt(middlewares.KeyAccount),
+	accountID := c.GetInt(middlewares.KeyAccount)
+
+	var request CreateBaselineRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		LogAndRespBadRequest(c, err, "Invalid request body: "+err.Error())
+		return
 	}
 
-	var req CreateBaselineRequst
-	if err := c.ShouldBindJSON(&req); err != nil {
-		LogAndRespBadRequest(c, err, "Invalid request body")
+	if request.Name == nil {
+		msg := "missing required parameter 'name'"
+		LogAndRespBadRequest(c, errors.New(msg), msg)
+		return
 	}
 
-	baselineConfig := BaselineConfig{
-		ToTime: req.ToTime,
-	}
-
-	config, err := json.Marshal(&baselineConfig)
+	missingIDs, err := checkInventoryIDs(accountID, request.InventoryIDs)
 	if err != nil {
-		LogAndRespError(c, err, "Invalid config")
+		LogAndRespError(c, err, "Database error")
+		return
 	}
 
-	baseline := models.Baseline{
-		Name:        req.Name,
-		Config:      config,
-		RhAccountID: account.RhAccountID,
+	if len(missingIDs) > 0 {
+		msg := fmt.Sprintf("Missing inventory_ids: %v", missingIDs)
+		LogAndRespNotFound(c, errors.New(msg), msg)
+		return
 	}
 
-	baselineID, err := buildCreateBaselineQuery(baseline, req.InventoryIDs, account)
+	baselineID, err := buildCreateBaselineQuery(request, accountID)
 	if err != nil {
 		LogAndRespError(c, err, "Database error")
 	}
@@ -70,23 +66,62 @@ func CreateBaselineHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, baselineID)
 }
 
-func buildCreateBaselineQuery(baseline models.Baseline, inventoryIDs []SystemID,
-	account AccountID) (int, error) {
+func buildCreateBaselineQuery(request CreateBaselineRequest, accountID int) (int, error) {
 	tx := database.Db.WithContext(base.Context).Begin()
 	defer tx.Rollback()
+
+	baseline := models.Baseline{
+		RhAccountID: accountID,
+		Name:        *request.Name,
+	}
+
+	if request.Config != nil {
+		config, err := json.Marshal(request.Config)
+		if err != nil {
+			return 0, err
+		}
+		baseline.Config = config
+	}
 
 	if err := tx.Model(models.Baseline{}).Create(&baseline).Error; err != nil {
 		return baseline.ID, err
 	}
 
-	if err := tx.Model(models.SystemPlatform{}).
-		Joins("JOIN inventory.hosts ih ON ih.id = sp.inventory_id").
-		Where("rh_account_id = (?) AND inventory_id::text IN (?)", account.RhAccountID, inventoryIDs).
-		Update("baseline_id", baseline.ID).Error; err != nil {
-		return baseline.ID, err
+	if len(request.InventoryIDs) > 0 {
+		err := updateSystemsBaselineID(tx, accountID, request.InventoryIDs, baseline.ID)
+		if err != nil {
+			return baseline.ID, err
+		}
 	}
 
-	query := tx.Commit()
+	err := tx.Commit().Error
+	return baseline.ID, err
+}
 
-	return baseline.ID, query.Error
+func checkInventoryIDs(accountID int, inventoryIDs []string) (missingIDs []string, err error) {
+	var containingIDs []string
+	err = database.Db.Table("system_platform sp").
+		Joins("JOIN inventory.hosts ih ON ih.id = sp.inventory_id").
+		Where("rh_account_id = (?) AND inventory_id::text IN (?)", accountID, inventoryIDs).
+		Pluck("sp.inventory_id", &containingIDs).Error
+	if err != nil {
+		return nil, err
+	}
+
+	if len(inventoryIDs) == len(containingIDs) {
+		return []string{}, nil // all inventoryIDs found in database
+	}
+
+	containingIDsMap := map[string]bool{}
+	for _, containingID := range containingIDs {
+		containingIDsMap[containingID] = true
+	}
+
+	for _, inventoryID := range inventoryIDs {
+		if _, ok := containingIDsMap[inventoryID]; !ok {
+			missingIDs = append(missingIDs, inventoryID)
+		}
+	}
+	sort.Strings(missingIDs)
+	return missingIDs, nil
 }
