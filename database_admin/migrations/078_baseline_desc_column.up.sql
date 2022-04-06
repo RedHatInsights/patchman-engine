826e9c4c24394fcf8619ed1d28740d05cfc5fc42
Michael Mraka <michael.mraka@redhat.com>
2022-04-06 13:22:23 +0200

    SPM-1416: make sure there are no duplicate before creating unique constraint

--- database_admin/migrations/078_baseline_desc_column.up.sql
+++ database_admin/migrations/078_baseline_desc_column.up.sql
@@ -2,4 +2,6 @@ ALTER TABLE baseline ADD CHECK (not empty(name));
 
 ALTER TABLE baseline ADD COLUMN description TEXT;
 
+UPDATE baseline SET name = name || ' ' || id
+ WHERE (rh_account_id, name) in (SELECT rh_account_id, name FROM baseline GROUP BY rh_account_id, name HAVING count(*) > 1);
 ALTER TABLE baseline ADD CONSTRAINT baseline_rh_account_id_name_key UNIQUE(rh_account_id, name);
