85fc0e46bb4a2bbce889ab1617cb581e4061e4c1
Michael Mraka <michael.mraka@redhat.com>
2022-04-22 16:34:05 +0200

    SPM-1455: remove redundant messages which mess clowder param output
    grep is not in ubi-micro image

--- scripts/try_export_clowder_params.sh
+++ scripts/try_export_clowder_params.sh
@@ -10,16 +10,15 @@ function print_clowder_params() {
   fi
 }
 
-# Detect params, it should be printed in 'PARAM=VALUE' format
-CLOWDER_PARAMS=$(print_clowder_params | grep '=' || true) # use '|| true' not to stop on empty match
+if [[ -n $ACG_CONFIG ]] ; then
+  # clowder is enabled
+  CLOWDER_PARAMS=$(print_clowder_params)
 
-# Enable to show Clowder vars in logs
-if [[ -n $SHOW_CLOWDER_VARS ]]; then
-  echo $CLOWDER_PARAMS
-fi
+  # Enable to show Clowder vars in logs
+  if [[ -n $SHOW_CLOWDER_VARS ]]; then
+    echo $CLOWDER_PARAMS
+  fi
 
-# Export Clowder params if any found
-if [[ ! -z $CLOWDER_PARAMS ]]; then
   echo "Clowder params found, setting..."
   export $CLOWDER_PARAMS
 else
