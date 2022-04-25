7674c0a9159b915360d41b1e8e9435c6712f077f
Michael Mraka <michael.mraka@redhat.com>
2022-04-25 10:45:49 +0200

    SPM-1455: remove redundant websocket test from vmaas_sync

--- vmaas_sync/entrypoint.sh
+++ vmaas_sync/entrypoint.sh
@@ -6,21 +6,5 @@ if [ -z "$VMAAS_WS_ADDRESS" ]; then
   exit 1
 fi
 
-# Replace websocket prefixes for curl
-search1="ws://"
-replace1="http://"
-search2="wss://"
-replace2="https://"
-
-TEST_ADDR=${VMAAS_WS_ADDRESS/$search1/$replace1}
-TEST_ADDR=${TEST_ADDR/$search2/$replace2}
-
-until curl -v $TEST_ADDR > /dev/null 2> /dev/null; do
-  echo "Waiting for VMaaS websocket to be available"
-  sleep 1
-done
-
-echo "VMaaS websocket is up"
-
 echo "Running in $(pwd) as $(id)"
 exec ./scripts/entrypoint.sh vmaas_sync
