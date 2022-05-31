63ec6e75a22ab354c022aeafc619472912a71ab7
michalslomczynski <mslomczy@redhat.com>
2022-05-31 13:11:41 +0200

    SPM-1546: Read Advisory Types to memory at DB setup

--- base/database/setup_test.go
+++ base/database/setup_test.go
@@ -2,10 +2,18 @@ package database
 
 import (
 	"app/base/utils"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestDBCheck(t *testing.T) {
 	utils.SkipWithoutDB(t)
 	Configure()
+}
+
+func TestAdditionalParams(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	Configure()
+
+	assert.True(t, len(AdvisoryTypes) == 5)
 }
