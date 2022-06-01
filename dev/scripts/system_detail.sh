4ba49cea6ea24e03a84312d184230fb97169d742
Michael Mraka <michael.mraka@redhat.com>
2022-06-01 13:54:52 +0200

    make identity script usable in interactive shell

--- dev/scripts/system_detail.sh
+++ dev/scripts/system_detail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source "$(dirname $(realpath "$0"))/env.sh"
+IDENTITY="$($(dirname "$0")/identity.sh)"
 
 curl -v -H "x-rh-identity: $IDENTITY" -XGET http://localhost:8080/api/patch/v1/systems/00000000-0000-0000-0000-000000000001 | python3 -m json.tool
