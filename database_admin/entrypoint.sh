9252c46a03a89e094cb5c93a1363cfd7d6edd5de
Michael Mraka <michael.mraka@redhat.com>
2022-06-06 17:28:09 +0200

    SPM-1537: make a oneliner from go run command

--- database_admin/entrypoint.sh
+++ database_admin/entrypoint.sh
@@ -5,10 +5,6 @@ set -e -o pipefail # stop on error
 MIGRATION_FILES=file://./database_admin/migrations
 
 echo "Running in $(pwd) as $(id)"
-if [[ -n $GORUN ]]; then
-  go run main.go migrate $MIGRATION_FILES
-else
-  ./main migrate $MIGRATION_FILES
-fi
+${GORUN:+go run} ./main${GORUN:+.go} migrate $MIGRATION_FILES
 
 exec sleep infinity
