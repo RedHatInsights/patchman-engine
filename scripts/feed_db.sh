8a44df1e3ad3b5b13fe548fc2f3d1a45a0416531
Patrik Segedy <psegedy@redhat.com>
2022-06-08 15:10:56 +0200

    SPM-1537: remove unused scripts and move psql scripts to dev/scripts

--- scripts/feed_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wait for all migrations applied
-WAIT_FOR_FULL_DB=1 ./scripts/wait-for-services.sh
-
-# fill database with testing data
-PGPASSWORD=$DB_PASSWD psql -h $DB_HOST -d $DB_NAME -U $DB_USER -p $DB_PORT \
-                       -v ON_ERROR_STOP=1 \
-                       -a -f ./dev/test_data.sql
