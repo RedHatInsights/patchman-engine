b473991631dd7a432a1439c4a70766a5e6b28049
Michael Mraka <michael.mraka@redhat.com>
2022-06-10 13:52:12 +0200

    SPM-1537: db_feed have to wait for end of migration

--- scripts/feed_db.go
+++ scripts/feed_db.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	database.InitDB()
-	database.DBWait("empty")
+	database.DBWait("full")
 	query, err := ioutil.ReadFile("./dev/test_data.sql")
 	if err != nil {
 		panic(err)
 	}
-	database.Db.Exec(string(query))
+	err = database.Db.Exec(string(query)).Error
+	if err != nil {
+		panic(err)
+	}
 }
--- scripts/go_test_db.sh
+++ scripts/go_test_db.sh
@@ -10,7 +10,7 @@ go run main.go migrate $MIGRATION_FILES
 go test -v app/database_admin
 
 # Fill database with testing data
-go run ./scripts/feed_db.go
+WAIT_FOR_DB=full go run ./scripts/feed_db.go
 
 # Normal test run - everything except database schema test
 TEST_DIRS=$(go list ./... | grep -v "app/database_admin")
