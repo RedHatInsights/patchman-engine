fd23b57354b50ac62ea1725ef0369f743e43e23b
Michael Mraka <michael.mraka@redhat.com>
2022-06-13 11:34:15 +0200

    devel setup: improve scripts
    save the planet by running one sed instead of cat, sed, printf, sed, printf

---
+++ dev/scripts/epemeral-port-forward.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#PODS="$(oc get services -o name)"
+
+fwd() {
+    service=$1
+    ports=$2
+
+    oc port-forward "$service" $ports >/dev/null &
+    echo $service forwarded to $ports
+}
+
+fwd service/ingress-service 8000:8000
+fwd service/patchman-db 5432:5432
+fwd service/patchman-manager 8080:8000
+
+wait
--- scripts/colorize.sh
+++ scripts/colorize.sh
@@ -3,6 +3,5 @@
 # Colorize some keywords (e.g. PASS - green, FAIL - red) in stdin.
 # Example: echo This PASS, this FAIL | ./scripts/colorize.sh
 
-cat /dev/stdin | \
-sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | \
-sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+sed "s/PASS/\x1b[32mPASS\x1b[0m/
+     s/FAIL/\x1b[31mFAIL\x1b[0m/"
