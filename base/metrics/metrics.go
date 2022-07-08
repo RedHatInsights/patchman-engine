1ddcbd107a8aebd447d3674fea6e1864ad7a6bb1
Michael Mraka <michael.mraka@redhat.com>
2022-07-08 11:19:35 +0200

    Revert "v2.0.8"
    This reverts commit c9911bc5df256af4d047f21aa6c5495b7b8ef432.

--- base/metrics/metrics.go
+++ base/metrics/metrics.go
@@ -23,7 +23,7 @@ var (
 	}, []string{"version"})
 
 	// ENGINEVERSION - DO NOT EDIT this variable MANUALLY - it is modified by generate_docs.sh
-	ENGINEVERSION = "v2.0.8"
+	ENGINEVERSION = "v2.0.7"
 )
 
 func init() {
