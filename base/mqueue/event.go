0736139c6ec8042ae20c11dec7cdebc6af1dcc58
Michael Mraka <michael.mraka@redhat.com>
2022-07-08 11:19:35 +0200

    Revert "Make BatchSize configurable"
    This reverts commit 9437cff37cae6f2449a4b2812c53af696d621f9f.

--- base/mqueue/event.go
+++ base/mqueue/event.go
@@ -9,7 +9,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-var BatchSize = utils.GetIntEnvOrDefault("MSG_BATCH_SIZE", 4000)
+const BatchSize = 4000
 
 var policy = backoff.NewExponential(
 	backoff.WithInterval(time.Second),
