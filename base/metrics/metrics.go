9070c2c31a81071c3fe8d45cd8ccfcce26f53bd9
Michael Mraka <michael.mraka@redhat.com>
2022-07-08 11:19:35 +0200

    Revert "v2.0.22"
    This reverts commit dac129ddad4e89533cee18b1a6164e0ef1985239.

--- base/metrics/metrics.go
+++ base/metrics/metrics.go
@@ -23,7 +23,7 @@ var (
 	}, []string{"version"})
 
 	// ENGINEVERSION - DO NOT EDIT this variable MANUALLY - it is modified by generate_docs.sh
-	ENGINEVERSION = "v2.0.22"
+	ENGINEVERSION = "v2.0.21"
 )
 
 func init() {
