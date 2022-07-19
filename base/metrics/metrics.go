130c2294372a14d37ecf30d4a087212909f2fa6f
michalslomczynski <mslomczy@redhat.com>
2022-07-19 12:44:56 +0200

    Revert "v2.0.30"
    This reverts commit 50c81bf179d7df3781d456ab0484919665a1abc0.

--- base/metrics/metrics.go
+++ base/metrics/metrics.go
@@ -23,7 +23,7 @@ var (
 	}, []string{"version"})
 
 	// ENGINEVERSION - DO NOT EDIT this variable MANUALLY - it is modified by generate_docs.sh
-	ENGINEVERSION = "v2.0.30"
+	ENGINEVERSION = "v2.0.29"
 )
 
 func init() {
