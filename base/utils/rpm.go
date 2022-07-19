3cc3810a6a48e61a450d9f9df304923633b28e89
michalslomczynski <mslomczy@redhat.com>
2022-07-19 12:45:38 +0200

    Revert "SPM-1326: function to compare NEVRAs"
    This reverts commit 4b135fa8775a6761eb4108a8770e654e1d788f02.

--- base/utils/rpm.go
+++ base/utils/rpm.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 	"strings"
 
-	rpm "github.com/ezamriy/gorpm"
 	"github.com/pkg/errors"
 )
 
@@ -126,12 +125,4 @@ func (n Nevra) IsLessVersion(other *Nevra) (bool, error) {
 	}
 
 	return false, nil
-}
-
-func (n Nevra) Cmp(other *Nevra) int {
-	ret := strings.Compare(n.Name, other.Name)
-	if ret != 0 {
-		return ret
-	}
-	return rpm.Vercmp(n.EVRAStringE(true), other.EVRAStringE(true))
 }
