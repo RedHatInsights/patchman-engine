d0dd52087aff8042dc915667208d88782e4e7ea7
michalslomczynski <mslomczy@redhat.com>
2022-07-19 12:44:56 +0200

    Revert "SPM-1610: add extension to run checksums in SQL"
    This reverts commit b78384caaa197b40523cfac52bca9ab15c17c499.

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,13 +7,11 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (83, false);
+VALUES (82, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
 -- ---------------------------------------------------------------------------
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- empty
 CREATE OR REPLACE FUNCTION empty(t TEXT)
