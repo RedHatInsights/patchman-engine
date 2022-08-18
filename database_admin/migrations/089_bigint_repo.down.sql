1fddcb296e0b6a38f2ca7295fa7e60b0be2fb247
Michael Mraka <michael.mraka@redhat.com>
2022-08-18 11:18:40 +0200

    SPM-1647: convert repo id to bigint

---
+++ database_admin/migrations/089_bigint_repo.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE repo ALTER COLUMN id TYPE INT;
+
+ALTER TABLE system_repo ALTER COLUMN repo_id TYPE INT,
+                        ALTER COLUMN system_id TYPE INT;
---
+++ database_admin/migrations/089_bigint_repo.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE repo ALTER COLUMN id TYPE BIGINT;
+
+ALTER TABLE system_repo ALTER COLUMN repo_id TYPE BIGINT,
+                        ALTER COLUMN system_id TYPE BIGINT;
