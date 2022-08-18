8c6f004d168c85f833dee998142eb90d54c09d31
Patrik Segedy <psegedy@redhat.com>
2022-08-18 17:47:06 +0200

    try to fix NodeExistsException on kafka start

--- dev/kafka/entrypoint.sh
+++ dev/kafka/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://github.com/wurstmeister/kafka-docker/issues/389#issuecomment-800814529
+sleep 20
 /app/setup.sh 2>&1 | grep -v '^WARNING: Due to limitations in metric names' &
 
 exec /etc/confluent/docker/run 2>&1 \
