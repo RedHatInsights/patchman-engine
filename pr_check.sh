e267ccec5003ddea3084ee7f40d715d70dca6d22
Tomáš Kašpárek <tkasparek@redhat.com>
2022-09-01 15:39:24 +0200

    support for skipping runs/deploy in pr_check in ephemeral

--- pr_check.sh
+++ pr_check.sh
@@ -14,14 +14,128 @@ export IQE_MARKER_EXPRESSION="patch_smoke"
 export IQE_FILTER_EXPRESSION=""
 export IQE_CJI_TIMEOUT="30m"
 
-# Install bonfire repo/initialize
-CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+# Heavily inspired by project-koku pr_check
+# https://github.com/project-koku/koku/blob/main/pr_check.sh
+LABELS_DIR="$WORKSPACE/github_labels"
+export ARTIFACTS_DIR="$WORKSPACE/artifacts"
 
-source $CICD_ROOT/build.sh
+mkdir -p $LABELS_DIR
+exit_code=0
+task_arr=([1]="Build" [2]="Deploy" [3]="Smoke Tests")
+error_arr=([1]="The PR is labeled to not build the test image" \
+           [2]="The PR is labeled to not deploy to ephemeral" \
+           [3]="The PR is labeled to not run smoke tests")
 
-# Deploy patchman to an ephemeral namespace for testing
-source $CICD_ROOT/deploy_ephemeral_env.sh
+function check_for_labels() {
+    if [ -f $LABELS_DIR/github_labels.txt ]; then
+        egrep "$1" $LABELS_DIR/github_labels.txt &>/dev/null
+    fi
+}
 
-# Run iqe-patchman somke tests with ClowdJobInvocation
-source $CICD_ROOT/cji_smoke_test.sh
+function _process_requirements_labels() {
+    # $1 env var to export
+    # $@ labels to process
+    env_var_name=$1; shift;
+    labels=$@
+    if [ -n "$labels" ]; then
+        labels=$(echo "$labels" | sed -e 's/"//g')
+        # contents of labels is different in different shells
+        # it can be one of
+        #   labels="REQ1\nREQ2"
+        #   labels="REQ1 REQ2"
+        lines=$(echo "$labels" | wc -l | xargs)
+        words=$(echo "$labels" | wc -w | xargs)
+        if [[ "$lines" == "1" ]] && [[ "$words" == "1" ]]; then
+            export "$env_var_name=$labels"
+            return
+        fi
+        processed=""
+        for req in $labels; do
+            processed="$processed$req,"
+        done
+        # delete extra comma
+        processed="${processed%?}"
+        export "$env_var_name=$processed"
+    fi
+}
+
+function process_requirements_labels() {
+    if [ -f $LABELS_DIR/github_labels.txt ]; then
+        requirements=$(egrep "^\"[A-Z]+-[A-Z-]*\"$" $LABELS_DIR/github_labels.txt)
+        requirements_priority=$(egrep "^\"(critical|high|medium|low)-requirements\"$" $LABELS_DIR/github_labels.txt | sed -e 's/-requirements//g')
+        _process_requirements_labels IQE_REQUIREMENTS $requirements
+        _process_requirements_labels IQE_REQUIREMENTS_PRIORITY $requirements_priority
+    fi
+}
+
+function build_image() {
+    source $CICD_ROOT/build.sh
+}
+
+function deploy_ephemeral() {
+    source $CICD_ROOT/deploy_ephemeral_env.sh
+}
+
+function run_smoke_tests() {
+    source $CICD_ROOT/cji_smoke_test.sh
+}
+
+function make_results_xml() {
+cat << EOF > $WORKSPACE/artifacts/junit-pr_check.xml
+<?xml version="1.0" encoding="UTF-8" ?>
+<testsuite id="pr_check" name="PR Check" tests="1" failures="0">
+    <testcase id="pr_check.${task_arr[$exit_code]}" name="${task_arr[$exit_code]}">
+    </testcase>
+</testsuite>
+EOF
+}
+
+# Save PR labels into a file
+set -ex
+# GH_API_URL="${GITHUB_API_URL:-https://api.github.com/}"  # don't use app-sre github api mirror for now
+curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/search/issues\?q\=sha:$ghprbActualCommit | jq '.items[].labels[].name' > $LABELS_DIR/github_labels.txt
+
+if check_for_labels "keep-namespace"; then
+    export RELEASE_NAMESPACE=false
+fi
+
+if check_for_labels "skip-build"; then
+    echo "PR check skipped"
+    exit_code=1
+else
+    # Install bonfire repo/initialize
+    CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+    curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+    echo "creating PR image"
+    build_image
+fi
+
+if [[ $exit_code == 0 ]]; then
+    if check_for_labels "skip-deploy"; then
+        echo "deployment to ephemeral skipped"
+        exit_code=2
+    else
+        echo "deploying to ephemeral"
+        deploy_ephemeral
+        if check_for_labels "skip-tests"; then
+            echo "PR smoke tests skipped"
+            exit_code=3
+        else
+            echo "running PR smoke tests"
+            set +e
+            process_requirements_labels
+            set -e
+            run_smoke_tests
+        fi
+    fi
+fi
+
+mkdir -p $ARTIFACTS_DIR
+cp $LABELS_DIR/github_labels.txt $ARTIFACTS_DIR/github_labels.txt
+
+if [[ $exit_code != 0 ]]
+then
+    echo "PR check failed"
+    make_results_xml
+fi
+
