bf7b3e3352846ae383924e00c5f0300ed829cfdf
Patrik Segedy <psegedy@redhat.com>
2022-09-06 15:49:37 +0200

    SPM-1707: fix pagination on views endpoints

--- manager/controllers/systems_advisories_view.go
+++ manager/controllers/systems_advisories_view.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"app/base/database"
 	"app/manager/middlewares"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -33,32 +34,86 @@ type systemsAdvisoriesDBLoad struct {
 
 var systemsAdvisoriesSelect = database.MustGetSelect(&systemsAdvisoriesDBLoad{})
 
-func systemsAdvisoriesQuery(acc int, systems []SystemID, advisories []AdvisoryName) *gorm.DB {
-	query := database.SystemAdvisories(database.Db, acc).
-		Select(systemsAdvisoriesSelect).
-		Joins("join advisory_metadata am on am.id = sa.advisory_id").
-		Order("sp.inventory_id, am.id")
+func systemsAdvisoriesQuery(acc int, systems []SystemID, advisories []AdvisoryName,
+	limit, offset *int) (*gorm.DB, error) {
+	sysq := database.Systems(database.Db, acc).
+		Distinct("sp.rh_account_id, sp.id, sp.inventory_id").
+		// we need to join system_advisories to make `limit` work properly
+		// without this join it can happen that we display less items on some pages
+		Joins(`JOIN system_advisories sa ON sa.system_id = sp.id
+			AND sa.rh_account_id = sp.rh_account_id AND sa.rh_account_id = ?`, acc).
+		Where("when_patched IS NULL").
+		Order("sp.inventory_id")
 	if len(systems) > 0 {
-		query = query.Where("sp.inventory_id::text in (?)", systems)
+		sysq = sysq.Where("sp.inventory_id::text in (?)", systems)
 	}
+
+	err := Paginate(sysq, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	query := database.Db.Table("(?) as sp", sysq).
+		Select(systemsAdvisoriesSelect).
+		Joins(`JOIN system_advisories sa ON sa.system_id = sp.id
+			AND sa.rh_account_id = sp.rh_account_id AND sa.rh_account_id = ?`, acc).
+		Joins("JOIN advisory_metadata am ON am.id = sa.advisory_id").
+		Where("when_patched IS NULL").
+		Order("sp.inventory_id, am.id")
 	if len(advisories) > 0 {
 		query = query.Where("am.name in (?)", advisories)
 	}
-	return query
+
+	return query, nil
 }
 
-func systemsAdvisories(c *gin.Context) ([]systemsAdvisoriesDBLoad, error) {
+func advisoriesSystemsQuery(acc int, systems []SystemID, advisories []AdvisoryName,
+	limit, offset *int) (*gorm.DB, error) {
+	advq := database.Db.Table("advisory_metadata am").
+		Distinct("am.id, am.name").
+		// we need to join system_advisories to make `limit` work properly
+		// without this join it can happen that we display less items on some pages
+		Joins("JOIN system_advisories sa ON am.id = sa.advisory_id AND sa.rh_account_id = ?", acc).
+		Where("when_patched IS NULL").
+		Order("am.id")
+	if len(advisories) > 0 {
+		advq = advq.Where("am.name in (?)", advisories)
+	}
+
+	err := Paginate(advq, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	query := database.Db.Table("(?) as am", advq).
+		Select(systemsAdvisoriesSelect).
+		Joins("JOIN system_advisories sa ON am.id = sa.advisory_id AND sa.rh_account_id = ?", acc).
+		Joins("JOIN system_platform sp ON sp.id = sa.system_id AND sa.rh_account_id = sp.rh_account_id").
+		Where("when_patched IS NULL").
+		Order("am.id, sp.inventory_id")
+	if len(systems) > 0 {
+		query = query.Where("sp.inventory_id::text in (?)", systems)
+	}
+	return query, nil
+}
+
+func queryDB(c *gin.Context, endpoint string) ([]systemsAdvisoriesDBLoad, error) {
 	var req SystemsAdvisoriesRequest
+	var q *gorm.DB
+	var err error
 	if err := c.ShouldBindJSON(&req); err != nil {
 		LogAndRespBadRequest(c, err, "Invalid request body")
 		return nil, err
 	}
 	acc := c.GetInt(middlewares.KeyAccount)
-	q := systemsAdvisoriesQuery(acc, req.Systems, req.Advisories)
-	q, err := Paginate(q, req.Limit, req.Offset)
+	switch endpoint {
+	case "SystemsAdvisories":
+		q, err = systemsAdvisoriesQuery(acc, req.Systems, req.Advisories, req.Limit, req.Offset)
+	case "AdvisoriesSystems":
+		q, err = advisoriesSystemsQuery(acc, req.Systems, req.Advisories, req.Limit, req.Offset)
+	default:
+		return nil, fmt.Errorf("unknown endpoint '%s'", endpoint)
+	}
 	if err != nil {
 		LogAndRespBadRequest(c, err, err.Error())
-		return nil, err
 	}
 
 	var data []systemsAdvisoriesDBLoad
@@ -81,7 +136,7 @@ func systemsAdvisories(c *gin.Context) ([]systemsAdvisoriesDBLoad, error) {
 // @Failure 500 {object} utils.ErrorResponse
 // @Router /views/systems/advisories [post]
 func PostSystemsAdvisories(c *gin.Context) {
-	data, err := systemsAdvisories(c)
+	data, err := queryDB(c, "SystemsAdvisories")
 	if err != nil {
 		return
 	}
@@ -108,7 +163,7 @@ func PostSystemsAdvisories(c *gin.Context) {
 // @Failure 500 {object} utils.ErrorResponse
 // @Router /views/advisories/systems [post]
 func PostAdvisoriesSystems(c *gin.Context) {
-	data, err := systemsAdvisories(c)
+	data, err := queryDB(c, "AdvisoriesSystems")
 	if err != nil {
 		return
 	}
