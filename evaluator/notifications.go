ebd39181d835ec54d490c9267f54b1f9a1bfd950
michalslomczynski <mslomczy@redhat.com>
2022-09-09 14:27:38 +0200

    SPM-1732: Sort advisory notification payload by errata name

--- evaluator/notifications.go
+++ evaluator/notifications.go
@@ -36,6 +36,7 @@ func getUnnotifiedAdvisories(tx *gorm.DB, accountID int, newAdvs SystemAdvisoryM
 		Joins("inner join advisory_metadata am on am.id = acd.advisory_id").
 		Where("acd.rh_account_id = ? AND acd.advisory_id IN (?)"+
 			"AND acd.notified IS NULL AND acd.systems_affected > 0", accountID, advIDs).
+		Order("am.name ASC").
 		Scan(&advNames).Error
 	if err != nil {
 		return nil, errors.Wrap(err, "querying unnotified advisories from DB failed")
