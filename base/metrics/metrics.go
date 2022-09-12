5173cc75a082e6ab3736282cb40344b61fd7208d
semantic-release <>
2022-09-12 08:42:51 +0000

    v2.3.5

--- base/metrics/metrics.go
+++ base/metrics/metrics.go
@@ -23,7 +23,7 @@ var (
 	}, []string{"version"})
 
 	// ENGINEVERSION - DO NOT EDIT this variable MANUALLY - it is modified by generate_docs.sh
-	ENGINEVERSION = "v2.3.4"
+	ENGINEVERSION = "v2.3.5"
 )
 
 func init() {
