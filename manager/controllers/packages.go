a335ca3d373540321883451dcd3fb8452325ffee
Michael Mraka <michael.mraka@redhat.com>
2022-09-27 14:00:54 +0200

    Revert "SPM-1745: use cached counts only when cache is valid"
    This reverts commit a82e63245fb23d9bf14a936d5ad2c553296840f7.

--- manager/controllers/packages.go
+++ manager/controllers/packages.go
@@ -47,12 +47,7 @@ type queryItem struct {
 var queryItemSelect = database.MustGetSelect(&queryItem{})
 
 func packagesQuery(filters map[string]FilterData, acc int) *gorm.DB {
-	var validCache bool
-	err := database.Db.Table("rh_account").
-		Select("valid_package_cache").
-		Where("id = ?", acc).
-		Scan(&validCache).Error
-	if err == nil && validCache && len(filters) == 0 && enabledPackageCache {
+	if len(filters) == 0 && enabledPackageCache {
 		// use cache only when tag filter is not used
 		q := database.Db.Table("package_account_data res").
 			Select(PackagesSelect).
