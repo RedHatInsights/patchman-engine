ee74de899b7900314dc046cb3774a50c9b4e8aa7
Patrik Segedy <psegedy@redhat.com>
2022-10-03 15:29:29 +0200

    SPM-1756: add granular rbac

--- manager/middlewares/rbac.go
+++ manager/middlewares/rbac.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +20,23 @@ var (
 )
 
 const xRHIdentity = "x-rh-identity"
+
+var allPerms = "patch:*:*"
+var readPerms = map[string]bool{allPerms: true, "patch:*:read": true}
+var writePerms = map[string]bool{allPerms: true, "patch:*:write": true}
+
+// handlerName to permissions mapping
+var granularPerms = map[string]struct {
+	Permission string
+	Read       bool
+	Write      bool
+}{
+	"CreateBaselineHandler":        {"patch:template:write", false, true},
+	"BaselineUpdateHandler":        {"patch:template:write", false, true},
+	"BaselineDeleteHandler":        {"patch:template:write", false, true},
+	"BaselineSystemsRemoveHandler": {"patch:template:write", false, true},
+	"SystemDeleteHandler":          {"patch:system:write", false, true},
+}
 
 // Make RBAC client on demand, with specified identity
 func makeClient(identity string) *api.Client {
@@ -33,12 +51,46 @@ func makeClient(identity string) *api.Client {
 	return &client
 }
 
-type rbacPerms struct {
-	Read  bool
-	Write bool
+func checkPermissions(access *rbac.AccessPagination, handlerName, method string) bool {
+	granted := false
+	for _, a := range access.Data {
+		if granted {
+			return true
+		}
+		if p, has := granularPerms[handlerName]; has {
+			// API handler requires granular permissions
+			if a.Permission == p.Permission {
+				// the required permission is present, e.g. patch:template:write
+				return true
+			}
+			if p.Read && !p.Write && readPerms[a.Permission] {
+				// required permission is read permission
+				// check whether we have either patch:*:read or patch:*:*
+				return true
+			}
+			if p.Write && !p.Read && writePerms[a.Permission] {
+				// required permission is write permission
+				// check whether we have either patch:*:write or patch:*:*
+				return true
+			}
+			// we need both read and write permissions - patch:*:*
+			granted = (a.Permission == allPerms)
+		} else {
+			// not granular
+			// require read permissions for GET and POST
+			// require write permissions for PUT and DELETE
+			switch method {
+			case "GET", "POST":
+				granted = readPerms[a.Permission]
+			case "PUT", "DELETE":
+				granted = writePerms[a.Permission]
+			}
+		}
+	}
+	return granted
 }
 
-func isAccessGranted(c *gin.Context) rbacPerms {
+func isAccessGranted(c *gin.Context) bool {
 	client := makeClient(c.GetHeader("x-rh-identity"))
 	access := rbac.AccessPagination{}
 	res, err := client.Request(&base.Context, http.MethodGet, rbacURL, nil, &access)
@@ -53,23 +105,12 @@ func isAccessGranted(c *gin.Context) rbacPerms {
 			status = res.StatusCode
 		}
 		serviceErrorCnt.WithLabelValues("rbac", strconv.Itoa(status)).Inc()
-		return rbacPerms{Read: false, Write: false}
+		return false
 	}
+	nameSplitted := strings.Split(c.HandlerName(), ".")
+	handlerName := nameSplitted[len(nameSplitted)-1]
 
-	perms := rbacPerms{Read: false, Write: false}
-	for _, a := range access.Data {
-		switch a.Permission {
-		case "patch:*:*", "patch:system:*":
-			perms.Read = true
-			perms.Write = true
-		case "patch:*:read", "patch:system:read":
-			perms.Read = true
-		case "patch:*:write", "patch:system:write":
-			perms.Write = true
-		default:
-		}
-	}
-	return perms
+	return checkPermissions(&access, handlerName, c.Request.Method)
 }
 
 func RBAC() gin.HandlerFunc {
@@ -79,17 +120,8 @@ func RBAC() gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
-		grantedPerms := isAccessGranted(c)
-
-		switch c.Request.Method {
-		case "GET", "POST":
-			if grantedPerms.Read {
-				return
-			}
-		case "DELETE", "PUT":
-			if grantedPerms.Write {
-				return
-			}
+		if isAccessGranted(c) {
+			return
 		}
 		c.AbortWithStatusJSON(http.StatusUnauthorized,
 			utils.ErrorResponse{Error: "You don't have access to this application"})
--- manager/middlewares/rbac_test.go
+++ manager/middlewares/rbac_test.go
@@ -1,11 +1,13 @@
 package middlewares
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
+	"app/base/rbac"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func okHandler(c *gin.Context) {
@@ -36,4 +38,189 @@ func TestRBACDelete(t *testing.T) {
 
 func TestRBACPut(t *testing.T) {
 	testRBAC(t, "PUT", http.StatusUnauthorized)
+}
+
+func TestPermissionsSingleWrite(t *testing.T) {
+	// handler needs `patch:template:write`
+	handler := "CreateBaselineHandler"
+	access := rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:*"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:write"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:template:write"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+}
+
+func TestPermissionsSingleRead(t *testing.T) {
+	// handler needs `patch:single:read`
+	handler := "SingleRead"
+	granularPerms = map[string]struct {
+		Permission string
+		Read       bool
+		Write      bool
+	}{"SingleRead": {"patch:single:read", true, false}}
+	access := rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:*"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:read"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:single:read"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "GET"))
+}
+
+func TestPermissionsSingleReadWrite(t *testing.T) {
+	// handler needs `patch:single:read`
+	handler := "SingleReadWrite"
+	granularPerms = map[string]struct {
+		Permission string
+		Read       bool
+		Write      bool
+	}{"SingleReadWrite": {"patch:single:*", true, true}}
+	access := rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:*"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:single:*"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:single:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:read"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:asdf:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "PUT"))
+}
+
+func TestPermissionsRead(t *testing.T) {
+	// handler needs `patch:single:read`
+	handler := "Read"
+	granularPerms = map[string]struct {
+		Permission string
+		Read       bool
+		Write      bool
+	}{"Read": {"patch:*:read", true, false}}
+	access := rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:*"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:read"},
+		},
+	}
+	assert.True(t, checkPermissions(&access, handler, "GET"))
+
+	access = rbac.AccessPagination{
+		Data: []rbac.Access{
+			{Permission: "patch:*:write"},
+		},
+	}
+	assert.False(t, checkPermissions(&access, handler, "GET"))
 }
