f7bbe5879e4bb975be74ebe66c71451630b552b9
Michael Mraka <michael.mraka@redhat.com>
2022-11-02 11:38:42 +0100

    Revert "SPM-1756: add granular rbac"
    This reverts commit ee74de899b7900314dc046cb3774a50c9b4e8aa7.

--- manager/middlewares/rbac.go
+++ manager/middlewares/rbac.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,23 +19,6 @@ var (
 )
 
 const xRHIdentity = "x-rh-identity"
-
-var allPerms = "patch:*:*"
-var readPerms = map[string]bool{allPerms: true, "patch:*:read": true}
-var writePerms = map[string]bool{allPerms: true, "patch:*:write": true}
-
-// handlerName to permissions mapping
-var granularPerms = map[string]struct {
-	Permission string
-	Read       bool
-	Write      bool
-}{
-	"CreateBaselineHandler":        {"patch:template:write", false, true},
-	"BaselineUpdateHandler":        {"patch:template:write", false, true},
-	"BaselineDeleteHandler":        {"patch:template:write", false, true},
-	"BaselineSystemsRemoveHandler": {"patch:template:write", false, true},
-	"SystemDeleteHandler":          {"patch:system:write", false, true},
-}
 
 // Make RBAC client on demand, with specified identity
 func makeClient(identity string) *api.Client {
@@ -51,46 +33,12 @@ func makeClient(identity string) *api.Client {
 	return &client
 }
 
-func checkPermissions(access *rbac.AccessPagination, handlerName, method string) bool {
-	granted := false
-	for _, a := range access.Data {
-		if granted {
-			return true
-		}
-		if p, has := granularPerms[handlerName]; has {
-			// API handler requires granular permissions
-			if a.Permission == p.Permission {
-				// the required permission is present, e.g. patch:template:write
-				return true
-			}
-			if p.Read && !p.Write && readPerms[a.Permission] {
-				// required permission is read permission
-				// check whether we have either patch:*:read or patch:*:*
-				return true
-			}
-			if p.Write && !p.Read && writePerms[a.Permission] {
-				// required permission is write permission
-				// check whether we have either patch:*:write or patch:*:*
-				return true
-			}
-			// we need both read and write permissions - patch:*:*
-			granted = (a.Permission == allPerms)
-		} else {
-			// not granular
-			// require read permissions for GET and POST
-			// require write permissions for PUT and DELETE
-			switch method {
-			case "GET", "POST":
-				granted = readPerms[a.Permission]
-			case "PUT", "DELETE":
-				granted = writePerms[a.Permission]
-			}
-		}
-	}
-	return granted
+type rbacPerms struct {
+	Read  bool
+	Write bool
 }
 
-func isAccessGranted(c *gin.Context) bool {
+func isAccessGranted(c *gin.Context) rbacPerms {
 	client := makeClient(c.GetHeader("x-rh-identity"))
 	access := rbac.AccessPagination{}
 	res, err := client.Request(&base.Context, http.MethodGet, rbacURL, nil, &access)
@@ -105,12 +53,23 @@ func isAccessGranted(c *gin.Context) bool {
 			status = res.StatusCode
 		}
 		serviceErrorCnt.WithLabelValues("rbac", strconv.Itoa(status)).Inc()
-		return false
+		return rbacPerms{Read: false, Write: false}
 	}
-	nameSplitted := strings.Split(c.HandlerName(), ".")
-	handlerName := nameSplitted[len(nameSplitted)-1]
 
-	return checkPermissions(&access, handlerName, c.Request.Method)
+	perms := rbacPerms{Read: false, Write: false}
+	for _, a := range access.Data {
+		switch a.Permission {
+		case "patch:*:*", "patch:system:*":
+			perms.Read = true
+			perms.Write = true
+		case "patch:*:read", "patch:system:read":
+			perms.Read = true
+		case "patch:*:write", "patch:system:write":
+			perms.Write = true
+		default:
+		}
+	}
+	return perms
 }
 
 func RBAC() gin.HandlerFunc {
@@ -120,8 +79,17 @@ func RBAC() gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
-		if isAccessGranted(c) {
-			return
+		grantedPerms := isAccessGranted(c)
+
+		switch c.Request.Method {
+		case "GET", "POST":
+			if grantedPerms.Read {
+				return
+			}
+		case "DELETE", "PUT":
+			if grantedPerms.Write {
+				return
+			}
 		}
 		c.AbortWithStatusJSON(http.StatusUnauthorized,
 			utils.ErrorResponse{Error: "You don't have access to this application"})
