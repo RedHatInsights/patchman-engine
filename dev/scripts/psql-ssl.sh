8fa5d74e68eee89a7f941f9baf7c5c78f34046d2
Michael Mraka <michael.mraka@redhat.com>
2022-12-14 14:04:12 +0000

    SPM-1836: merge psql script together

--- dev/scripts/psql-ssl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Connect to database for manual administration
-PGSSLMODE=$DB_SSLMODE PGSSLROOTCERT=$DB_SSLROOTCERT PGPASSWORD=$DB_ADMIN_PASSWD psql -d $DB_NAME -h $DB_HOST -U $DB_ADMIN_USER -p $DB_PORT
--- dev/scripts/psql.sh
+++ dev/scripts/psql.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ -n $DB_SSLROOTCERT ]] ; then
+    export PGSSLROOTCERT=$DB_SSLROOTCERT
+    export PGSSLMODE=$DB_SSLMODE
+fi
 # Connect to database for manual administration
 PGPASSWORD=$DB_ADMIN_PASSWD psql -d $DB_NAME -h $DB_HOST -U $DB_ADMIN_USER -p $DB_PORT
