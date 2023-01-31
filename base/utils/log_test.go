38b94b4d3130e335456448034fd8bb89e93f207b
Michael Mraka <michael.mraka@redhat.com>
2023-01-31 12:30:11 +0100

    SPM-1876: update logger

--- base/utils/log_test.go
+++ base/utils/log_test.go
@@ -1,10 +1,11 @@
 package utils
 
 import (
-	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestInitLogging(t *testing.T) {
@@ -14,27 +15,28 @@ func TestInitLogging(t *testing.T) {
 	var hook = NewTestLogHook()
 	log.AddHook(hook)
 
-	Log("num", 1, "str", "text").Info("info log")
+	LogInfo("num", 1, "str", "text", "info log")
 
 	assert.Equal(t, 1, len(hook.LogEntries))
 	entry := hook.LogEntries[0]
 	assert.Equal(t, 2, len(entry.Data))
 	assert.Equal(t, 1, entry.Data["num"])
 	assert.Equal(t, "text", entry.Data["str"])
+	assert.Equal(t, "info log", entry.Message)
 }
 
-func TestOddArgsWarn(t *testing.T) {
+func TestEvenArgs(t *testing.T) {
 	assert.Nil(t, os.Setenv("LOG_STYLE", "json"))
 	ConfigureLogging()
 
 	var hook = NewTestLogHook()
 	log.AddHook(hook)
 
-	Log("num", 1, 2).Info("info log")
+	LogInfo("num", 1, "str", "text")
 
-	assert.Equal(t, 2, len(hook.LogEntries))
-	warnEntry := hook.LogEntries[0]
-	assert.Equal(t, "Unable to accept odd (3) arguments count in utils.DebugLog method.", warnEntry.Message)
-	infoEntry := hook.LogEntries[1]
-	assert.Equal(t, "info log", infoEntry.Message)
+	assert.Equal(t, 1, len(hook.LogEntries))
+	entry := hook.LogEntries[0]
+	assert.Equal(t, 2, len(entry.Data))
+	assert.Equal(t, 1, entry.Data["num"])
+	assert.Equal(t, "text", entry.Data["str"])
 }
