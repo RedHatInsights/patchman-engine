fad0be81c85513c5bc9f9348d8e2c22b56a5e24a
Michael Mraka <michael.mraka@redhat.com>
2023-06-12 14:23:20 +0200

    SPM-2093: mark functions as immutable

---
+++ database_admin/migrations/111_immutable.down.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION empty(t TEXT)
+    RETURNS BOOLEAN as
+$empty$
+BEGIN
+    RETURN t ~ '^[[:space:]]*$';
+END;
+$empty$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION update_status(update_data jsonb)
+    RETURNS TEXT as
+$$
+DECLARE
+    len int;
+BEGIN
+    len = jsonb_array_length(update_data);
+    IF len IS NULL or len = 0 THEN
+        RETURN 'None';
+    END IF;
+    len = jsonb_array_length(jsonb_path_query_array(update_data, '$ ? (@.status == "Installable")'));
+    IF len > 0 THEN
+        RETURN 'Installable';
+    END IF;
+    RETURN 'Applicable';
+END;
+$$ LANGUAGE 'plpgsql';
