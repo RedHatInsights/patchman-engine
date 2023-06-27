f9dba9a955570eff89e60c5c62e00caf01a17c40
Patrik Segedy <psegedy@redhat.com>
2023-06-27 17:16:17 +0200

    SPM-2126: add metrics for package_refresh job

--- tasks/caches/caches.go
+++ tasks/caches/caches.go
@@ -28,8 +28,12 @@ func RunPackageRefresh() {
 	tasks.HandleContextCancel(tasks.WaitAndExit)
 	configure()
 	utils.LogInfo("Refreshing package cache")
-	if err := RefreshPackagesCaches(nil); err != nil {
-		utils.LogError("err", err.Error(), "Refresh account packages caches")
+	errRefresh := RefreshPackagesCaches(nil)
+	if err := Metrics().Add(); err != nil {
+		utils.LogInfo("err", err, "Could not push to pushgateway")
+	}
+	if errRefresh != nil {
+		utils.LogError("err", errRefresh.Error(), "Refresh account packages caches")
 		return
 	}
 	utils.LogInfo("Refreshed account packages caches")
