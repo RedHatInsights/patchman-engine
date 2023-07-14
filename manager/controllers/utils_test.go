7453eb97145337249d2585122ba21e8a4ecd2a35
Patrik Segedy <psegedy@redhat.com>
2023-07-14 18:58:53 +0200

    ESSNTL-4817: test group_name inventory filter

--- manager/controllers/utils_test.go
+++ manager/controllers/utils_test.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
+	"app/base/database"
+	"app/base/rbac"
+	"app/base/utils"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,4 +53,27 @@ func TestNestedQueryInvalidKey(t *testing.T) {
 		t.Fatal("Timeout exceeded - probably infinite loop in nested query")
 	case <-done:
 	}
+}
+
+func TestGroupNameFilter(t *testing.T) {
+	utils.SkipWithoutDB(t)
+	database.Configure()
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request, _ = http.NewRequest("GET", "/?filter[group_name][in]=group2", nil)
+
+	filters, err := ParseInventoryFilters(c)
+	assert.Nil(t, err)
+
+	var systems []SystemsID
+	groups := map[string]string{
+		rbac.KeyGrouped: `{"[{\"id\":\"inventory-group-1\"}]","[{\"id\":\"inventory-group-2\"}]"}`,
+	}
+	tx := database.Systems(database.Db, 1, groups)
+	tx, _ = ApplyInventoryFilter(filters, tx, "sp.inventory_id")
+	tx.Scan(&systems)
+
+	assert.Equal(t, 2, len(systems)) // 2 systems with `group2` in test_data
+	assert.Equal(t, "00000000-0000-0000-0000-000000000007", systems[0].ID)
+	assert.Equal(t, "00000000-0000-0000-0000-000000000008", systems[1].ID)
 }
