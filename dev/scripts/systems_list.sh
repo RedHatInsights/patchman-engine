e43cf63bed8f05441d50be150d28df646445469b
Michael Mraka <michael.mraka@redhat.com>
2023-07-21 13:40:26 +0200

    update default org in dev scripts

--- dev/scripts/systems_list.sh
+++ dev/scripts/systems_list.sh
@@ -2,4 +2,4 @@
 
 IDENTITY="$($(dirname "$0")/identity.sh)"
 
-curl -v -H "x-rh-identity: $IDENTITY" http://localhost:8080/api/patch/v2/systems/ | python3 -m json.tool
+curl -v -H "x-rh-identity: $IDENTITY" http://localhost:8080/api/patch/v2/systems | python3 -m json.tool
