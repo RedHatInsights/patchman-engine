61e12317719dd40e4f79f3ef011fb2508829e3d1
Michael Mraka <michael.mraka@redhat.com>
2023-08-11 15:07:49 +0200

    POC: migration script for system_package2

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (114, false);
+VALUES (115, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
