4b660cf45e301c337ffb0e5baf53d6414d038bdc
Patrik Segedy <psegedy@redhat.com>
2023-09-05 10:27:05 +0200

    RHINENG-1638: fix flaky unit tests

--- manager/controllers/advisories_test.go
+++ manager/controllers/advisories_test.go
@@ -274,7 +274,11 @@ func TestAdvisoriesTags(t *testing.T) {
 	assert.Equal(t, 8, len(output.Data))
 	assert.Equal(t, 1, output.Data[0].Attributes.InstallableSystems)
 	assert.Equal(t, 2, output.Data[0].Attributes.ApplicableSystems)
-	assert.Equal(t, "/?offset=0&limit=20&tags=ns1/k2=val2&filter[applicable_systems]=gt:0&sort=id", output.Links.First)
+	assert.Equal(t, 76, len(output.Links.First))
+	assert.Contains(t, output.Links.First, "?offset=0&limit=20")
+	assert.Contains(t, output.Links.First, "tags=ns1/k2=val2")
+	assert.Contains(t, output.Links.First, "filter[applicable_systems]=gt:0")
+	assert.Contains(t, output.Links.First, "sort=id")
 }
 
 func TestListAdvisoriesTagsInvalid(t *testing.T) {
