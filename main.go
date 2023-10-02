1875b8f45b2a624a72a230a8f157621edd5a32f0
Patrik Segedy <psegedy@redhat.com>
2023-10-02 14:03:31 +0200

    POC: add job to migrate data from system_package

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"app/platform"
 	"app/tasks/caches"
 	"app/tasks/cleaning"
+	"app/tasks/migration"
 	"app/tasks/system_culling"
 	"app/tasks/vmaas_sync"
 	"app/turnpike"
@@ -70,5 +71,7 @@ func runJob(name string) {
 		cleaning.RunDeleteUnusedData()
 	case "packages_cache_refresh":
 		caches.RunPackageRefresh()
+	case "system_package_data_migration":
+		migration.RunSystemPackageDataMigration()
 	}
 }
