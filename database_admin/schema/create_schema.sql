cd5dd657f022b95a49a07bce722625693359441f
Patrik Segedy <psegedy@redhat.com>
2024-01-25 13:37:37 +0100

    RHINENG-7796: check yum_updates in unchanged trigger

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (122, false);
+VALUES (123, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
@@ -70,7 +70,7 @@ BEGIN
         NEW.unchanged_since := CURRENT_TIMESTAMP;
     END IF;
     IF (TG_OP = 'UPDATE') AND
-       NEW.json_checksum <> OLD.json_checksum THEN
+       (NEW.json_checksum <> OLD.json_checksum OR NEW.yum_updates <> OLD.yum_updates) THEN
         NEW.unchanged_since := CURRENT_TIMESTAMP;
     END IF;
     RETURN NEW;
