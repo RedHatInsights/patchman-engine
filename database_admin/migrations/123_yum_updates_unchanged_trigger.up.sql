d1c683f121cf35dd0d857c93af422becd81d8df1
Patrik Segedy <psegedy@redhat.com>
2024-01-25 13:37:37 +0100

    RHINENG-7796: check yum_updates in unchanged trigger

---
+++ database_admin/migrations/123_yum_updates_unchanged_trigger.up.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION check_unchanged()
+    RETURNS TRIGGER AS
+$check_unchanged$
+BEGIN
+    IF (TG_OP = 'INSERT') AND
+       NEW.unchanged_since IS NULL THEN
+        NEW.unchanged_since := CURRENT_TIMESTAMP;
+    END IF;
+    IF (TG_OP = 'UPDATE') AND
+       (NEW.json_checksum <> OLD.json_checksum OR NEW.yum_updates <> OLD.yum_updates) THEN
+        NEW.unchanged_since := CURRENT_TIMESTAMP;
+    END IF;
+    RETURN NEW;
+END;
+$check_unchanged$
+    LANGUAGE 'plpgsql';
