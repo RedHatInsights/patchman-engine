8c77154e2881b9f1042fd6973b383fc72d01bd60
Jakub Dugovič <jdugovic@redhat.com>
2024-02-28 13:42:53 +0100

    RHINENG-8708: update dev scripts to use v3 API

--- dev/scripts/system_packages.sh
+++ dev/scripts/system_packages.sh
@@ -3,4 +3,4 @@
 IDENTITY="$($(dirname "$0")/identity.sh)"
 UUID=${1:-00000000-0000-0000-0000-000000000002}
 
-curl -v -H "x-rh-identity: $IDENTITY" http://localhost:8080/api/patch/v2/systems/$UUID/packages | python3 -m json.tool
+curl -v -H "x-rh-identity: $IDENTITY" http://localhost:8080/api/patch/v3/systems/$UUID/packages | python3 -m json.tool
