945418b673bdf3c03a0499bef7cbc3d067c6c018
Michael Mraka <michael.mraka@redhat.com>
2024-03-27 17:04:36 +0100

    RHINENG-8045: inside container server is not bind to localhost

--- dev/scripts/kafka-send-message.sh
+++ dev/scripts/kafka-send-message.sh
@@ -3,4 +3,4 @@
 # send message from stdin to kafka topic
 
 TOPIC=${1:-platform.inventory.events}
-podman exec -i kafka sh -c " tr '\n' ' ' | kafka-console-producer --topic $TOPIC --broker-list localhost:9092 "
+podman exec -i kafka sh -c " tr '\n' ' ' | kafka-console-producer --topic $TOPIC --broker-list kafka:9092 "
