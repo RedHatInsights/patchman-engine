20de9cd5ee7f2ccee530d25c578ee07995e5f0fb
Patrik Segedy <psegedy@redhat.com>
2024-10-01 13:21:43 +0200

    RHINENG-13371: set package_data=null for old errata

--- database_admin/migrations/131_remove_advisory_old_package_data.up.sql
+++ database_admin/migrations/131_remove_advisory_old_package_data.up.sql
@@ -1,2 +1,1 @@
-delete from advisory_metadata am where package_data is not json array and not exists (select 1 from advisory_account_data aad where aad.advisory_id = am.id);
 update advisory_metadata set package_data = null where package_data is not json array;
