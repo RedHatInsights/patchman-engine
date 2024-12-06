430dbbf63ce435f1fdf2fdd85870522bc6936608
Michael Mraka <michael.mraka@redhat.com>
2024-12-06 12:06:59 +0100

    RHINENG-14351: add test for SystemAuthenticator

--- manager/middlewares/authentication_test.go
+++ manager/middlewares/authentication_test.go
@@ -3,8 +3,12 @@ package middlewares
 import (
 	"app/base/database"
 	"app/base/utils"
+	"bytes"
+	"encoding/base64"
+	"net/http"
 	"net/http/httptest"
 	"testing"
+	"text/template"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -70,4 +74,121 @@ func TestFindDNoAccNr(t *testing.T) {
 	assert.Contains(t, AccountIDCache.Values, orgID)
 	newID := AccountIDCache.Values[orgID]
 	assert.NotEqual(t, newID, accID)
+}
+
+var (
+	identityHeaderTmpl, _ = template.New("x-rh-identity").Parse(`{
+    "identity": {
+        "org_id": "{{.OrgID}}",
+        "auth_type": "{{.AuthType}}",
+        "type": "{{.Type}}",
+		"user": {
+            "username": "jdoe@acme.com",
+            "email": "jdoe@acme.com",
+            "first_name": "john",
+            "last_name": "doe",
+            "is_active": true,
+            "is_org_admin": false,
+            "is_internal": false,
+            "locale": "en_US"
+        },
+        "system": {
+            "cert_type": "{{.CertType}}",
+            "cn": "{{.SystemCN}}"
+        },
+        "internal": {
+            "org_id": "{{.OrgID}}",
+            "auth_type": "{{.AuthType}}",
+            "auth_time": 6300
+        }
+    },
+    "entitlements": {
+        "insights": {
+            "is_entitled": true
+        }
+    }
+}`)
+	systemCN = "cccccccc-0000-0000-0001-000000000004"
+)
+
+func TestPublicAuthenticator(t *testing.T) {
+	c := testSetup(t)
+	var identityHeader bytes.Buffer
+	err := identityHeaderTmpl.Execute(&identityHeader,
+		map[string]string{"OrgID": "org_1", "Type": "User", "AuthType": "basic-auth"})
+	assert.Nil(t, err)
+	c.Request = &http.Request{Header: http.Header{}}
+	c.Request.Header.Add("x-rh-identity", base64.StdEncoding.EncodeToString(identityHeader.Bytes()))
+	PublicAuthenticator()(c)
+	assert.Equal(t, 1, c.GetInt(utils.KeyAccount))
+	assert.Equal(t, "john doe", c.GetString(utils.KeyUser))
+}
+
+func TestPublicAuthenticatorMissingHeader(t *testing.T) {
+	c := testSetup(t)
+	// no x-rh-identity header
+	c.Request = &http.Request{Header: http.Header{}}
+
+	PublicAuthenticator()(c)
+	assert.Nil(t, c.Keys)
+}
+
+func TestTurnpikeAuthenticator(t *testing.T) {
+	c := testSetup(t)
+	var identityHeader bytes.Buffer
+	err := identityHeaderTmpl.Execute(&identityHeader,
+		map[string]string{"OrgID": "org_1", "Type": "Associate", "AuthType": "basic-auth"})
+	assert.Nil(t, err)
+	c.Request = &http.Request{Header: http.Header{}}
+	c.Request.Header.Add("x-rh-identity", base64.StdEncoding.EncodeToString(identityHeader.Bytes()))
+	TurnpikeAuthenticator()(c)
+	assert.False(t, c.IsAborted())
+}
+
+func TestTurnpikeAuthenticatorAbort(t *testing.T) {
+	c := testSetup(t)
+	var identityHeader bytes.Buffer
+	err := identityHeaderTmpl.Execute(&identityHeader,
+		map[string]string{"OrgID": "org_1", "Type": "User", "AuthType": "basic-auth"})
+	assert.Nil(t, err)
+	c.Request = &http.Request{Header: http.Header{}}
+	c.Request.Header.Add("x-rh-identity", base64.StdEncoding.EncodeToString(identityHeader.Bytes()))
+	TurnpikeAuthenticator()(c)
+	assert.True(t, c.IsAborted())
+}
+
+func TestSystemCertAuthenticator(t *testing.T) {
+	c := testSetup(t)
+	var systemIdentityHeader bytes.Buffer
+	err := identityHeaderTmpl.Execute(&systemIdentityHeader,
+		map[string]string{"OrgID": "org_1", "SystemCN": systemCN, "Type": "System",
+			"CertType": "system", "AuthType": "cert-type"})
+	assert.Nil(t, err)
+	c.Request = &http.Request{Header: http.Header{}}
+	c.Request.Header.Add("x-rh-identity", base64.StdEncoding.EncodeToString(systemIdentityHeader.Bytes()))
+	SystemCertAuthenticator()(c)
+	assert.Equal(t, 1, c.GetInt(utils.KeyAccount))
+	assert.Equal(t, systemCN, c.GetString(utils.KeySystem))
+}
+
+func TestSystemCertAuthenticatorDeny(t *testing.T) {
+	c := testSetup(t)
+	c.Request = &http.Request{Header: http.Header{}}
+	var systemIdentityHeader bytes.Buffer
+
+	id1 := map[string]string{"OrgID": "org_1", "SystemCN": systemCN, "Type": "User",
+		"CertType": "system", "AuthType": "cert-type"}
+	id2 := map[string]string{"OrgID": "org_1", "SystemCN": systemCN, "Type": "System",
+		"CertType": "not-valid", "AuthType": "cert-type"}
+	id3 := map[string]string{"OrgID": "org_1", "SystemCN": "not-valid", "Type": "System",
+		"CertType": "system", "AuthType": "cert-type"}
+	identities := []map[string]string{id1, id2, id3}
+
+	for _, id := range identities {
+		err := identityHeaderTmpl.Execute(&systemIdentityHeader, id)
+		assert.Nil(t, err)
+		c.Request.Header.Add("x-rh-identity", base64.StdEncoding.EncodeToString(systemIdentityHeader.Bytes()))
+		SystemCertAuthenticator()(c)
+		assert.Nil(t, c.Keys)
+	}
 }
