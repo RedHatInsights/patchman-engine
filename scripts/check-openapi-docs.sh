743d078d48a11a24c20cfbbdfa61ce23275e1480
Patrik Segedy <psegedy@redhat.com>
2024-12-06 14:03:16 +0100

    check all openapi specs

--- scripts/check-openapi-docs.sh
+++ scripts/check-openapi-docs.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-APIVER=v3
-OPENAPI_COPY=$(mktemp -t openapi.json.XXX)
-cp docs/$APIVER/openapi.json $OPENAPI_COPY
+APIVERS="v3 admin"
+declare -A OPENAPI_COPY
+for APIVER in $APIVERS; do
+  OPENAPI_COPY["$APIVER"]=$(mktemp -t openapi.json.XXX)
+  cp docs/$APIVER/openapi.json ${OPENAPI_COPY["$APIVER"]}
+done
+
 ./scripts/generate_docs.sh
-diff docs/$APIVER/openapi.json $OPENAPI_COPY
-rc=$?
-if [ $rc -gt 0 ]; then
-  echo "docs/$APIVER/openapi.json different from file generated with './scripts/generate_docs.sh'!"
-else
-  echo "docs/$APIVER/openapi.json consistent with generated file."
-fi
 
-rm $OPENAPI_COPY
+for APIVER in ${!OPENAPI_COPY[@]}; do
+  diff docs/$APIVER/openapi.json ${OPENAPI_COPY["$APIVER"]}
+  rc+=$?
+  if [ $rc -gt 0 ]; then
+    echo "docs/$APIVER/openapi.json different from file generated with './scripts/generate_docs.sh'!"
+  else
+    echo "docs/$APIVER/openapi.json consistent with generated file."
+  fi
+
+  rm ${OPENAPI_COPY["$APIVER"]}
+done
 exit $rc
