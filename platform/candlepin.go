78ead79c9f3d40206b8a058b639fb2290a73b0bf
Michael Mraka <michael.mraka@redhat.com>
2025-02-11 17:00:57 +0100

    Revert "RHINENG-15506: fix test for candlepin API"
    This reverts commit b454942e30178cc4965e6458f39ba6f42b6ba23d.
    
    revert until candlepin v4.6.0 is available

--- platform/candlepin.go
+++ platform/candlepin.go
@@ -2,11 +2,29 @@ package platform
 
 import (
 	"app/base/utils"
+	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+func candlepinEnvHandler(c *gin.Context) {
+	envID := c.Param("envid")
+	/*
+		jsonData, _ := io.ReadAll(c.Request.Body)
+		json.Unmarshal(jsonData, &body) // nolint:errcheck
+		if body.ReturnStatus > 200 {
+			c.AbortWithStatus(body.ReturnStatus)
+			return
+		}
+	*/
+	data := fmt.Sprintf(`{
+        "environment": "%s"
+    }`, envID)
+	utils.LogInfo(data)
+	c.Data(http.StatusOK, gin.MIMEJSON, []byte(data))
+}
 
 func candlepinConsumersHandler(c *gin.Context) {
 	consumer := c.Param("consumer")
@@ -15,14 +33,7 @@ func candlepinConsumersHandler(c *gin.Context) {
 	c.Data(http.StatusOK, gin.MIMEJSON, []byte{})
 }
 
-func candlepinConsumersEnvironmentsHandler(c *gin.Context) {
-	owner := c.Param("owner")
-	jsonData, _ := io.ReadAll(c.Request.Body)
-	utils.LogInfo("owner", owner, "body", string(jsonData))
-	c.Data(http.StatusOK, gin.MIMEJSON, []byte{})
-}
-
 func initCandlepin(app *gin.Engine) {
+	app.POST("/candlepin/environments/:envid/consumers", candlepinEnvHandler)
 	app.PUT("/candlepin/consumers/:consumer", candlepinConsumersHandler)
-	app.PUT("/candlepin/owner/:owner/consumers/environments", candlepinConsumersEnvironmentsHandler)
 }
