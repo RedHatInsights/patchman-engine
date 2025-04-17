1d6a2b3ce410b281bf1dfbd49fcebc7c75045081
Michael Mraka <michael.mraka@redhat.com>
2025-04-17 17:45:17 +0200

    RHINENG-17345: add tests for candlepin env check

--- platform/candlepin.go
+++ platform/candlepin.go
@@ -1,10 +1,12 @@
 package platform
 
 import (
+	"app/base/candlepin"
 	"app/base/utils"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,10 +32,10 @@ func candlepinEnvHandler(c *gin.Context) {
 	c.Data(http.StatusOK, gin.MIMEJSON, []byte(data))
 }
 
-func candlepinConsumersHandler(c *gin.Context) {
+func candlepinConsumersPutHandler(c *gin.Context) {
 	consumer := c.Param("consumer")
 	jsonData, _ := io.ReadAll(c.Request.Body)
-	utils.LogInfo("consumer", consumer, "body", string(jsonData))
+	utils.LogInfo("PUT consumer", consumer, "body", string(jsonData))
 	if consumer == "return_404" {
 		c.Data(http.StatusNotFound, gin.MIMEJSON, []byte{})
 		return
@@ -41,7 +43,21 @@ func candlepinConsumersHandler(c *gin.Context) {
 	c.Data(http.StatusOK, gin.MIMEJSON, []byte{})
 }
 
+func candlepinConsumersGetHandler(c *gin.Context) {
+	consumer := c.Param("consumer")
+	utils.LogInfo("GET consumer", consumer, "body")
+	env := strings.ReplaceAll(consumer, "-", "")
+	env = strings.Replace(env, "000", "999", 1)
+	response := candlepin.ConsumersDetailResponse{
+		Environments: []candlepin.ConsumersEnvironment{
+			{ID: env},
+		},
+	}
+	c.JSON(http.StatusOK, response)
+}
+
 func initCandlepin(app *gin.Engine) {
 	app.POST("/candlepin/environments/:envid/consumers", candlepinEnvHandler)
-	app.PUT("/candlepin/consumers/:consumer", candlepinConsumersHandler)
+	app.PUT("/candlepin/consumers/:consumer", candlepinConsumersPutHandler)
+	app.GET("/candlepin/consumers/:consumer", candlepinConsumersGetHandler)
 }
