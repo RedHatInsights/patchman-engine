9a89688811d5000c06b9f702e587ec39a7b7b8c4
Michael Mraka <michael.mraka@redhat.com>
2025-04-17 14:52:18 +0200

    RHINENG-17345: assign template according to system's environment in candlepin

--- manager/controllers/template_systems_update.go
+++ manager/controllers/template_systems_update.go
@@ -21,7 +21,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var errCandlepin = errors.New("candlepin error")
 var candlepinClient = candlepin.CreateCandlepinClient()
 
 type TemplateSystemsUpdateRequest struct {
@@ -189,7 +188,7 @@ func callCandlepin(ctx context.Context, consumer string, request *candlepin.Cons
 		utils.LogDebug("request", *request, "candlepin_url", candlepinEnvConsumersURL,
 			"status_code", statusCode, "err", err)
 		if err != nil {
-			err = errors.Wrap(errCandlepin, err.Error())
+			err = errors.Wrap(candlepin.ErrCandlepin, err.Error())
 		} else if statusCode != http.StatusOK && statusCode != http.StatusNoContent {
 			err = errors.Errorf("candlepin API status %d", statusCode)
 		}
