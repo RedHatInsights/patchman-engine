b67ec95f10f8a60825a878cb4178fc18ba0154e1
Michael Mraka <michael.mraka@redhat.com>
2025-07-23 17:03:41 +0200

    RHINENG-19571: use upstream kafka image
    apache/kafka is build both for amd64 and arm64

--- dev/kafka/entrypoint.sh
+++ dev/kafka/entrypoint.sh
@@ -2,5 +2,5 @@
 
 /app/setup.sh 2>&1 | grep -v '^WARNING: Due to limitations in metric names' &
 
-exec /etc/confluent/docker/run 2>&1 \
+exec /etc/kafka/docker/run 2>&1 \
     | grep -v -E ' (TRACE|DEBUG|INFO) '
