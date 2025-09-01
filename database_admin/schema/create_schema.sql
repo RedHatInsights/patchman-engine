9397ec560a1f3a1bde6077bd55fd02a15c393dee
Michael Mraka <michael.mraka@redhat.com>
2025-09-01 16:25:52 +0200

    RHINENG-20474: simplify trigger on system_platform
    the trigger creates lock on advisory_account_table
    faster trigger means less time spent in lock

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (133, false);
+VALUES (134, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
@@ -107,44 +107,17 @@ BEGIN
         RETURN NEW;
     END IF;
 
-    -- find advisories linked to the server
-    WITH to_update_advisories AS (
-        SELECT aad.advisory_id,
-               aad.rh_account_id,
-               case when sa.status_id = 0 then change else 0 end as systems_installable_change,
-               change as systems_applicable_change
-          FROM advisory_account_data aad
-          JOIN system_advisories sa ON aad.advisory_id = sa.advisory_id
-          -- Filter advisory_account_data only for advisories affectign this system & belonging to system account
-         WHERE aad.rh_account_id =  NEW.rh_account_id
-           AND sa.system_id = NEW.id AND sa.rh_account_id = NEW.rh_account_id
-         ORDER BY aad.advisory_id),
-         -- update existing rows
-         update AS (
-            UPDATE advisory_account_data aad
-               SET systems_installable = aad.systems_installable + ta.systems_installable_change,
-                   systems_applicable = aad.systems_applicable + ta.systems_applicable_change
-              FROM to_update_advisories ta
-             WHERE aad.advisory_id = ta.advisory_id
-               AND aad.rh_account_id = NEW.rh_account_id
-         )
-    -- If we have system affected && no exisiting advisory_account_data entry, we insert new rows
+    -- insert/update advisories linked to the server
     INSERT
       INTO advisory_account_data (advisory_id, rh_account_id, systems_installable, systems_applicable)
     SELECT sa.advisory_id, NEW.rh_account_id,
-           case when sa.status_id = 0 then 1 else 0 end as systems_installable,
-           1 as systems_applicable
-    FROM system_advisories sa
-    WHERE sa.system_id = NEW.id AND sa.rh_account_id = NEW.rh_account_id
-      AND change > 0
-      -- create only rows which are not already in to_update_advisories
-      AND (NEW.rh_account_id, sa.advisory_id) NOT IN (
-            SELECT ta.rh_account_id, ta.advisory_id
-              FROM to_update_advisories ta
-    )
-    ON CONFLICT (advisory_id, rh_account_id) DO UPDATE
-        SET systems_installable = advisory_account_data.systems_installable + EXCLUDED.systems_installable,
-            systems_applicable = advisory_account_data.systems_applicable + EXCLUDED.systems_applicable;
+           case when sa.status_id = 0 then change else 0 end as systems_installable,
+           change as systems_applicable
+      FROM system_advisories sa
+     WHERE sa.system_id = NEW.id AND sa.rh_account_id = NEW.rh_account_id
+        ON CONFLICT (advisory_id, rh_account_id) DO UPDATE
+           SET systems_installable = advisory_account_data.systems_installable + EXCLUDED.systems_installable,
+               systems_applicable = advisory_account_data.systems_applicable + EXCLUDED.systems_applicable;
     RETURN NEW;
 END;
 $system_update$ LANGUAGE plpgsql;
