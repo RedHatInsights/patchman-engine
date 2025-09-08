78f725cac7688702cd79c678de45d1be2b10d5dd
Jakub Dugovič <jdugovic@redhat.com>
2025-09-08 15:40:21 +0200

    RHINENG-19163: refactor kessel middleware
    This was done in preparetion for the addition of templates support.

--- manager/middlewares/kessel.go
+++ manager/middlewares/kessel.go
@@ -3,21 +3,19 @@ package middlewares
 import (
 	"app/base/utils"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/redhatinsights/platform-go-middlewares/identity"
 	"google.golang.org/grpc"
 
 	"github.com/project-kessel/kessel-sdk-go/kessel/auth"
 	kesselv2 "github.com/project-kessel/kessel-sdk-go/kessel/inventory/v1beta2"
 )
-
-type ListObjectStreamingClient = grpc.ServerStreamingClient[kesselv2.StreamedListObjectsResponse]
 
 var credentials = auth.NewOAuth2ClientCredentials(
 	utils.CoreCfg.KesselAuthClientID,
@@ -36,48 +34,16 @@ func setupClient() (kesselv2.KesselInventoryServiceClient, *grpc.ClientConn, err
 	return clientBuilder.Build()
 }
 
-func buildRequest(c *gin.Context) (*kesselv2.StreamedListObjectsRequest, error) {
-	xrhid, err := utils.ParseXRHID(c.GetHeader("x-rh-identity"))
-	if err != nil {
-		return nil, err
-	}
-
-	reporterType := "rbac"
-	req := &kesselv2.StreamedListObjectsRequest{
-		ObjectType: &kesselv2.RepresentationType{
-			ResourceType: "workspace",
-			ReporterType: &reporterType,
-		},
-		Subject: &kesselv2.SubjectReference{
-			Resource: &kesselv2.ResourceReference{
-				ResourceType: "principal",
-				ResourceId:   fmt.Sprintf("redhat/%s", xrhid.Identity.User.UserID),
-				Reporter: &kesselv2.ReporterReference{
-					Type: "rbac",
-				},
+func buildSubject(xrhid *identity.XRHID) *kesselv2.SubjectReference {
+	return &kesselv2.SubjectReference{
+		Resource: &kesselv2.ResourceReference{
+			ResourceType: "principal",
+			ResourceId:   fmt.Sprintf("redhat/%s", xrhid.Identity.User.UserID),
+			Reporter: &kesselv2.ReporterReference{
+				Type: "rbac",
 			},
 		},
 	}
-
-	switch c.Request.Method {
-	case http.MethodGet, http.MethodPost:
-		req.Relation = patchReadPerm
-	case http.MethodPut, http.MethodDelete:
-		req.Relation = patchWritePerm
-	}
-
-	return req, nil
-}
-
-func receiveAll(stream ListObjectStreamingClient) ([]*kesselv2.StreamedListObjectsResponse, error) {
-	responses := make([]*kesselv2.StreamedListObjectsResponse, 0)
-	for res, err := stream.Recv(); err != io.EOF; res, err = stream.Recv() {
-		if err != nil {
-			return nil, err
-		}
-		responses = append(responses, res)
-	}
-	return responses, nil
 }
 
 func processWorkspaces(workspaces []*kesselv2.StreamedListObjectsResponse) (map[string]string, error) {
@@ -97,6 +63,50 @@ func processWorkspaces(workspaces []*kesselv2.StreamedListObjectsResponse) (map[
 	return map[string]string{utils.KeyGrouped: fmt.Sprintf("{%s}", strings.Join(groups, ","))}, nil
 }
 
+func useStreamedListObjects(c *gin.Context, client kesselv2.KesselInventoryServiceClient, xrhid *identity.XRHID) error {
+	sloReqContext, sloContextCancel := context.WithCancel(c)
+	defer sloContextCancel()
+
+	var permission string
+	switch c.Request.Method {
+	case http.MethodGet, http.MethodPost:
+		permission = "patch_all_view"
+	case http.MethodPut, http.MethodDelete:
+		permission = "patch_all_edit"
+	}
+
+	resourceType := "rbac"
+	stream, err := client.StreamedListObjects(sloReqContext, &kesselv2.StreamedListObjectsRequest{
+		ObjectType: &kesselv2.RepresentationType{
+			ResourceType: "workspace",
+			ReporterType: &resourceType,
+		},
+		Relation: permission,
+		Subject:  buildSubject(xrhid),
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to establish a gRPC stream with Kessel")
+	}
+
+	workspaces := make([]*kesselv2.StreamedListObjectsResponse, 0)
+	for res, err := stream.Recv(); err != io.EOF; res, err = stream.Recv() {
+		if err != nil {
+			return errors.Wrap(err, "failed to receive all from Kessel")
+		}
+		workspaces = append(workspaces, res)
+	}
+
+	inventoryGroups, err := processWorkspaces(workspaces)
+	if err != nil {
+		utils.LogError("err", err.Error(), "processWorkspaces")
+		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.ErrorResponse{
+			Error: "You don't have access to this application",
+		})
+	}
+	c.Set(utils.KeyInventoryGroups, inventoryGroups)
+	return nil
+}
+
 func hasPermissionKessel(c *gin.Context) {
 	client, conn, err := setupClient()
 	if err != nil {
@@ -108,43 +118,20 @@ func hasPermissionKessel(c *gin.Context) {
 	}
 	defer conn.Close()
 
-	req, err := buildRequest(c)
+	xrhid, err := utils.ParseXRHID(c.GetHeader("x-rh-identity"))
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.ErrorResponse{Error: "Invalid x-rh-identity header"})
-		return
 	}
 
-	// TODO: how long should the timeout be
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	stream, err := client.StreamedListObjects(ctx, req)
+	// Require granular permission if set for handler
+	// TODO: useCheckForUpdate
+
+	// Require method-derived permission otherwise
+	err = useStreamedListObjects(c, client, xrhid)
 	if err != nil {
-		utils.LogError("err", err.Error(), "failed to establish a gRPC stream with Kessel")
-		c.AbortWithStatusJSON(http.StatusInternalServerError, utils.ErrorResponse{
-			Error: "Failed to communicate with Kessel RBAC service",
-		})
-		return
+		utils.LogError("err", err.Error(), "useStreamedListObjects failed")
+		c.AbortWithStatus(http.StatusInternalServerError)
 	}
-
-	workspaces, err := receiveAll(stream)
-	if err != nil {
-		utils.LogError("err", err.Error(), "failed to receive from Kessel")
-		c.AbortWithStatusJSON(http.StatusInternalServerError, utils.ErrorResponse{
-			Error: "Failed to communicate with Kessel RBAC service",
-		})
-		return
-	}
-
-	inventoryGroups, err := processWorkspaces(workspaces)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, utils.ErrorResponse{
-			Error: "You don't have access to this application",
-		})
-		return
-	}
-
-	c.Set(utils.KeyInventoryGroups, inventoryGroups)
-	utils.LogDebug("Kessel check successful")
 }
 
 func Kessel() gin.HandlerFunc {
