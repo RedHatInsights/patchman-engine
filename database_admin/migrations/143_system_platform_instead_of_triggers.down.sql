b296184d8d67fca2e4c62b090d399088460817ee
Michael Mraka <michael.mraka@redhat.com>
2026-01-21 15:58:30 +0100

    RHINENG-21214: handle inserts/updates to system_platform view

---
+++ database_admin/migrations/143_system_platform_instead_of_triggers.down.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER IF EXISTS system_platform_delete_trigger ON system_platform;
+DROP TRIGGER IF EXISTS system_platform_update_trigger ON system_platform;
+DROP TRIGGER IF EXISTS system_platform_insert_trigger ON system_platform;
+
+DROP FUNCTION IF EXISTS system_platform_delete();
+DROP FUNCTION IF EXISTS system_platform_update();
+DROP FUNCTION IF EXISTS system_platform_insert();
