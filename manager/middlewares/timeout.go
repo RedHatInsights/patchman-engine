eef7df5b4616348984b13ffe898c3ecbca802c98
TenSt <smaksymc@redhat.com>
2026-03-05 13:07:09 +0100

    build: upgrade dependencies

--- manager/middlewares/timeout.go
+++ manager/middlewares/timeout.go
@@ -12,9 +12,6 @@ import (
 func WithTimeout(seconds time.Duration) gin.HandlerFunc {
 	return timeout.New(
 		timeout.WithTimeout(seconds*time.Second),
-		timeout.WithHandler(func(c *gin.Context) {
-			c.Next()
-		}),
 		timeout.WithResponse(func(c *gin.Context) {
 			c.AbortWithStatusJSON(http.StatusRequestTimeout, utils.ErrorResponse{Error: "Request timeout"})
 			c.Done()
