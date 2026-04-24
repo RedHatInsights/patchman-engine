cedda00cdc937163b6a6b150c66f8f676b3ddb64
Michael Mraka <michael.mraka@redhat.com>
2026-04-24 18:29:19 +0200

    small speedup to search filter
    removed LOWER() and CONCAT() which prevent to use indexes

--- manager/controllers/utils_test.go
+++ manager/controllers/utils_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"app/base/core"
 	"app/base/database"
 	"app/base/utils"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestGroupNameFilter(t *testing.T) {
@@ -53,4 +55,45 @@ func TestGroupNameFilter2(t *testing.T) {
 	tx.Scan(&systems)
 
 	assert.Equal(t, 9, len(systems)) // 2 systems with `group2`, 6 with `group1` in test_data
+}
+
+func TestApplySearchEmpty(t *testing.T) {
+	var baseTx gorm.DB
+
+	var cases = []struct {
+		url     string
+		columns []string
+		comment string
+	}{
+		{"/", []string{""}, "no search query returns original tx and empty meta"},
+		{"/?search=", []string{"some_col"}, "empty search value returns original tx and empty meta"},
+		{"/?search=foo", []string{}, "search present but no columns returns original tx and empty meta"},
+	}
+	for _, c := range cases {
+		t.Run(c.comment, func(t *testing.T) {
+			ctx, _ := gin.CreateTestContext(httptest.NewRecorder())
+			ctx.Request, _ = http.NewRequest("GET", c.url, nil)
+			outTx, q := ApplySearch(ctx, &baseTx, c.columns...)
+			assert.Same(t, &baseTx, outTx)
+			assert.Equal(t, "", q)
+		})
+	}
+}
+
+func TestApplySearchHello(t *testing.T) {
+	core.SetupTest(t)
+
+	c, _ := gin.CreateTestContext(httptest.NewRecorder())
+	c.Request, _ = http.NewRequest("GET", "/?search=hello", nil)
+
+	tx, searchQ := ApplySearch(c, database.DB, "a", "b")
+	assert.Equal(t, "search=hello", searchQ)
+
+	tx = tx.Where("1 = 0")
+	var n string
+	sql := tx.ToSQL(func(g *gorm.DB) *gorm.DB {
+		return g.Table("(select 'a' as a, 'b' as b) as t").Select("a").Scan(&n)
+	})
+
+	assert.Contains(t, sql, "WHERE (a::text ILIKE '%hello%' OR b::text ILIKE '%hello%')")
 }
