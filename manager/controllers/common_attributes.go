cce74df3d0bb0ffa734b1e1f99e0d15030efd87d
Jakub Dugovič <jdugovic@redhat.com>
2026-05-06 11:01:25 +0200

    RHINENG-25147: refactot SystemGroups

--- manager/controllers/common_attributes.go
+++ manager/controllers/common_attributes.go
@@ -29,7 +29,7 @@ type SystemTags struct {
 }
 
 type SystemGroups struct {
-	Groups SystemGroupsList `json:"groups" csv:"groups" query:"si.workspaces" gorm:"column:groups" order_query:"si.workspaces->0->>'name'"`
+	Groups SystemGroupsList `json:"groups" csv:"groups" query:"CASE WHEN si.workspace_id IS NOT NULL THEN jsonb_build_array(jsonb_build_object('id', si.workspace_id, 'name', si.workspace_name)) ELSE '[]'::jsonb END" order_query:"si.workspace_name"`
 }
 
 type SystemWorkspace struct {
