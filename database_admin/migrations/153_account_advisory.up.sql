c0c60f1ca43deaa9afd3c68d236affaf241ca339
rverdile <ryanverdile@gmail.com>
2026-05-12 11:50:20 -0400

    review

--- database_admin/migrations/153_account_advisory.up.sql
+++ database_admin/migrations/153_account_advisory.up.sql
@@ -2,14 +2,13 @@ CREATE TABLE IF NOT EXISTS account_advisory
 (
     advisory_id              BIGINT NOT NULL,
     rh_account_id            INT    NOT NULL,
-    workspace_id             TEXT   NOT NULL,
+    workspace_id             UUID   NOT NULL,
     systems_applicable       INT    NOT NULL DEFAULT 0,
     systems_installable      INT    NOT NULL DEFAULT 0,
     notified                 TIMESTAMP WITH TIME ZONE NULL,
     CONSTRAINT account_advisory_advisory_id
         FOREIGN KEY (advisory_id)
             REFERENCES advisory_metadata (id),
-    UNIQUE (advisory_id, rh_account_id, workspace_id),
     PRIMARY KEY (rh_account_id, workspace_id, advisory_id)
 ) PARTITION BY HASH (rh_account_id);
 
@@ -21,5 +20,3 @@ SELECT grant_table_partitions('SELECT, INSERT, UPDATE, DELETE', 'account_advisor
 SELECT grant_table_partitions('SELECT, INSERT, UPDATE, DELETE', 'account_advisory', 'evaluator');
 SELECT grant_table_partitions('SELECT, INSERT, UPDATE, DELETE', 'account_advisory', 'listener');
 SELECT grant_table_partitions('SELECT, INSERT, UPDATE, DELETE', 'account_advisory', 'vmaas_sync');
-
-CREATE INDEX ON account_advisory (rh_account_id, workspace_id);
