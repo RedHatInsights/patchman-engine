81054a16d3df5419c818b681a73199a9324edd0e
rverdile <ryanverdile@gmail.com>
2026-05-12 11:57:59 -0400

    update migration number

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (153, false);
+VALUES (154, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
