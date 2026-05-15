3a0955c9040d689403ea4dfb40b8c7c4d961874c
rverdile <ryanverdile@gmail.com>
2026-05-15 10:47:22 -0400

    remove trigger

--- database_admin/migrations/155_account_advisory_queries.up.sql
+++ database_admin/migrations/155_account_advisory_queries.up.sql
@@ -1,68 +1,6 @@
 CREATE INDEX ON account_advisory (systems_applicable);
 CREATE INDEX ON account_advisory (systems_installable);
 
-CREATE OR REPLACE FUNCTION on_system_update_account_advisory()
-    RETURNS TRIGGER
-AS
-$system_update_account_advisory$
-DECLARE
-    was_counted       BOOLEAN;
-    should_count      BOOLEAN;
-    workspace_changed BOOLEAN;
-BEGIN
-    IF TG_OP != 'UPDATE' OR NOT EXISTS (
-        SELECT 1
-        FROM system_patch
-        WHERE system_id = NEW.id
-          AND rh_account_id = NEW.rh_account_id
-          AND last_evaluation IS NOT NULL
-    ) THEN
-        RETURN NEW;
-    END IF;
-
-    was_counted := OLD.stale = FALSE AND OLD.workspace_id IS NOT NULL;
-    should_count := NEW.stale = FALSE AND NEW.workspace_id IS NOT NULL;
-    workspace_changed := OLD.workspace_id IS DISTINCT FROM NEW.workspace_id;
-
-    -- Decrement from old workspace
-    IF was_counted AND (NOT should_count OR workspace_changed) THEN
-        INSERT
-          INTO account_advisory (advisory_id, rh_account_id, workspace_id, systems_installable, systems_applicable)
-        SELECT sa.advisory_id, OLD.rh_account_id, OLD.workspace_id,
-               CASE WHEN sa.status_id = 0 THEN -1 ELSE 0 END,
-               -1
-          FROM system_advisories sa
-         WHERE sa.system_id = OLD.id AND sa.rh_account_id = OLD.rh_account_id
-         ORDER BY sa.advisory_id
-            ON CONFLICT (rh_account_id, workspace_id, advisory_id) DO UPDATE
-               SET systems_installable = account_advisory.systems_installable + EXCLUDED.systems_installable,
-                   systems_applicable = account_advisory.systems_applicable + EXCLUDED.systems_applicable;
-    END IF;
-
-    -- Increment in new workspace
-    IF should_count AND (NOT was_counted OR workspace_changed) THEN
-        INSERT
-          INTO account_advisory (advisory_id, rh_account_id, workspace_id, systems_installable, systems_applicable)
-        SELECT sa.advisory_id, NEW.rh_account_id, NEW.workspace_id,
-               CASE WHEN sa.status_id = 0 THEN 1 ELSE 0 END,
-               1
-          FROM system_advisories sa
-         WHERE sa.system_id = NEW.id AND sa.rh_account_id = NEW.rh_account_id
-         ORDER BY sa.advisory_id
-            ON CONFLICT (rh_account_id, workspace_id, advisory_id) DO UPDATE
-               SET systems_installable = account_advisory.systems_installable + EXCLUDED.systems_installable,
-                   systems_applicable = account_advisory.systems_applicable + EXCLUDED.systems_applicable;
-    END IF;
-
-    RETURN NEW;
-END;
-$system_update_account_advisory$ LANGUAGE plpgsql;
-
-SELECT create_table_partition_triggers('system_inventory_on_update_account_advisory',
-                                       $$AFTER UPDATE$$,
-                                       'system_inventory',
-                                       $$FOR EACH ROW EXECUTE PROCEDURE on_system_update_account_advisory()$$);
-
 CREATE OR REPLACE FUNCTION refresh_account_advisory_caches_multi(advisory_ids_in INTEGER[] DEFAULT NULL,
                                                                   rh_account_id_in INTEGER DEFAULT NULL)
     RETURNS VOID AS
--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -116,63 +116,6 @@ BEGIN
 END;
 $system_update$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION on_system_update_account_advisory()
-    RETURNS TRIGGER
-AS
-$system_update_account_advisory$
-DECLARE
-    was_counted       BOOLEAN;
-    should_count      BOOLEAN;
-    workspace_changed BOOLEAN;
-BEGIN
-    IF TG_OP != 'UPDATE' OR NOT EXISTS (
-        SELECT 1
-        FROM system_patch
-        WHERE system_id = NEW.id
-          AND rh_account_id = NEW.rh_account_id
-          AND last_evaluation IS NOT NULL
-    ) THEN
-        RETURN NEW;
-    END IF;
-
-    was_counted := OLD.stale = FALSE AND OLD.workspace_id IS NOT NULL;
-    should_count := NEW.stale = FALSE AND NEW.workspace_id IS NOT NULL;
-    workspace_changed := OLD.workspace_id IS DISTINCT FROM NEW.workspace_id;
-
-    -- Decrement from old workspace
-    IF was_counted AND (NOT should_count OR workspace_changed) THEN
-        INSERT
-          INTO account_advisory (advisory_id, rh_account_id, workspace_id, systems_installable, systems_applicable)
-        SELECT sa.advisory_id, OLD.rh_account_id, OLD.workspace_id,
-               CASE WHEN sa.status_id = 0 THEN -1 ELSE 0 END,
-               -1
-          FROM system_advisories sa
-         WHERE sa.system_id = OLD.id AND sa.rh_account_id = OLD.rh_account_id
-         ORDER BY sa.advisory_id
-            ON CONFLICT (rh_account_id, workspace_id, advisory_id) DO UPDATE
-               SET systems_installable = account_advisory.systems_installable + EXCLUDED.systems_installable,
-                   systems_applicable = account_advisory.systems_applicable + EXCLUDED.systems_applicable;
-    END IF;
-
-    -- Increment in new workspace
-    IF should_count AND (NOT was_counted OR workspace_changed) THEN
-        INSERT
-          INTO account_advisory (advisory_id, rh_account_id, workspace_id, systems_installable, systems_applicable)
-        SELECT sa.advisory_id, NEW.rh_account_id, NEW.workspace_id,
-               CASE WHEN sa.status_id = 0 THEN 1 ELSE 0 END,
-               1
-          FROM system_advisories sa
-         WHERE sa.system_id = NEW.id AND sa.rh_account_id = NEW.rh_account_id
-         ORDER BY sa.advisory_id
-            ON CONFLICT (rh_account_id, workspace_id, advisory_id) DO UPDATE
-               SET systems_installable = account_advisory.systems_installable + EXCLUDED.systems_installable,
-                   systems_applicable = account_advisory.systems_applicable + EXCLUDED.systems_applicable;
-    END IF;
-
-    RETURN NEW;
-END;
-$system_update_account_advisory$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION refresh_advisory_caches_multi(advisory_ids_in INTEGER[] DEFAULT NULL,
                                                          rh_account_id_in INTEGER DEFAULT NULL)
     RETURNS VOID AS
@@ -741,11 +684,6 @@ SELECT create_table_partition_triggers('system_inventory_on_update',
                                        $$AFTER UPDATE$$,
                                        'system_inventory',
                                        $$FOR EACH ROW EXECUTE PROCEDURE on_system_update()$$);
-
-SELECT create_table_partition_triggers('system_inventory_on_update_account_advisory',
-                                       $$AFTER UPDATE$$,
-                                       'system_inventory',
-                                       $$FOR EACH ROW EXECUTE PROCEDURE on_system_update_account_advisory()$$);
 
 CREATE INDEX IF NOT EXISTS system_inventory_inventory_id_idx ON system_inventory (inventory_id);
 CREATE INDEX IF NOT EXISTS system_inventory_tags_index ON system_inventory USING GIN (tags JSONB_PATH_OPS);
