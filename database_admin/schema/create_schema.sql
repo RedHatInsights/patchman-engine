740135cfd0ee5861bedfd66f249ebf972ce7cf64
Stepan <stepan.maks@gmail.com>
2026-06-09 13:20:55 +0200

    database: drop old delete_system function (#2229)

--- database_admin/schema/create_schema.sql
+++ database_admin/schema/create_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS schema_migrations
 
 
 INSERT INTO schema_migrations
-VALUES (157, false);
+VALUES (158, false);
 
 -- ---------------------------------------------------------------------------
 -- Functions
