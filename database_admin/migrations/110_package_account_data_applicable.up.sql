5889f2dda81fa75a13b32d9baecf9a99893d7345
Michael Mraka <michael.mraka@redhat.com>
2026-07-14 11:01:47 +0200

    RHINENG-25502: update older migrations to fix schema upgrade test
    in Postgresql 18

--- database_admin/migrations/110_package_account_data_applicable.up.sql
+++ database_admin/migrations/110_package_account_data_applicable.up.sql
@@ -1,4 +1,6 @@
 TRUNCATE package_account_data;
 UPDATE rh_account SET valid_package_cache = FALSE;
 ALTER TABLE package_account_data RENAME COLUMN systems_updatable TO systems_installable;
+ALTER TABLE package_account_data RENAME CONSTRAINT package_account_data_systems_updatable_not_null
+                                                TO package_account_data_systems_installable_not_null;
 ALTER TABLE package_account_data ADD COLUMN systems_applicable INT NOT NULL DEFAULT 0;
